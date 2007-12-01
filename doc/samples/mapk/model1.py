f1168de4a8c7bdaae4141203b6fa2743b41e1649
shafi <none@none>
2007-12-01 04:17:11 +0000

    --HG-- branch : brown extra : convert_revision : svn%3A15b6bd1d-d91c-0410-8971-a6cc2a4f8591/brown%40479

--- doc/samples/mapk/model1.py
+++ doc/samples/mapk/model1.py
@@ -12,35 +12,35 @@ box1 = CuboidalSurface( [0,0,0],[1e-5,1e-5,1e-5] )
 # not supported yet
 #s.addSurface( box1 )
 
-Fus3 = Species( 'Fus3', 2e-11, 5e-8 )
-s.addSpecies( Fus3 )
-Ste7 = Species( 'Ste7', 2e-11, 5e-8 )
-s.addSpecies( Ste7 )
-Msg5 = Species( 'Msg5', 2e-11, 5e-8 )
-s.addSpecies( Msg5 )
-Fus3p = Species( 'Fus3p', 2e-11, 5e-8 )
-s.addSpecies( Fus3p )
-Fus3pp = Species( 'Fus3pp', 2e-11, 5e-8 )
-s.addSpecies( Fus3pp )
-Fus3Ste7 = Species( 'Fus3Ste7', 2e-11, 5e-8 )
-s.addSpecies( Fus3Ste7 )
-Fus3pSte7 = Species( 'Fus3pSte7', 2e-11, 5e-8 )
-s.addSpecies( Fus3pSte7 )
-Fus3ppMsg5 = Species( 'Fus3ppMsg5', 2e-11, 5e-8 )
-s.addSpecies( Fus3ppMsg5 )
-Fus3pMsg5 = Species( 'Fus3pMsg5', 2e-11, 5e-8 )
-s.addSpecies( Fus3pMsg5 )
+K = Species( 'K', 2e-11, 5e-8 )
+s.addSpecies( K )
+KK = Species( 'KK', 2e-11, 5e-8 )
+s.addSpecies( KK )
+P = Species( 'P', 2e-11, 5e-8 )
+s.addSpecies( P )
+Kp = Species( 'Kp', 2e-11, 5e-8 )
+s.addSpecies( Kp )
+Kpp = Species( 'Kpp', 2e-11, 5e-8 )
+s.addSpecies( Kpp )
+KKK = Species( 'KKK', 2e-11, 5e-8 )
+s.addSpecies( KKK )
+KpKK = Species( 'KpKK', 2e-11, 5e-8 )
+s.addSpecies( KpKK )
+KppP = Species( 'KppP', 2e-11, 5e-8 )
+s.addSpecies( KppP )
+KpP = Species( 'KpP', 2e-11, 5e-8 )
+s.addSpecies( KpP )
 
-#  1 2   Fus3 + Ste7   <-> Fus3Ste7
-#  3     Fus3Ste7       -> Fus3p + Ste7
-#  4 5   Fus3p + Ste7  <-> Fus3pSte7
-#  6     Fus3pSte7      -> Fus3pp + Ste7 
-#  7 8   Fus3pp + Msg5 <-> Fus3ppMsg5
-#  9     Fus3ppMsg5     -> Fus3p + Msg5
-# 10 11  Fus3p + Msg5  <-> Fus3pMsg5
-# 12     Fus3pMsg5      -> Fus3 + Msg5
+#  1 2   K + KK   <-> KKK
+#  3     KKK       -> Kp + KK
+#  4 5   Kp + KK  <-> KpKK
+#  6     KpKK      -> Kpp + KK 
+#  7 8   Kpp + P <-> KppP
+#  9     KppP     -> Kp + P
+# 10 11  Kp + P  <-> KpP
+# 12     KpP      -> K + P
 
-r1 = BindingReactionType( Ea, X, EaX, 1e9 / N_A )
+r1 = BindingReactionType( , X, EaX, 1e9 / N_A )
 s.addReactionType( r1 )
 r2 = UnbindingReactionType( EaX, Ea, X, 1e3 )
 s.addReactionType( r2 )
