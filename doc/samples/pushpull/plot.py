32e2d3cc40062435f91c6f97452afa3208639de5
shafi <none@none>
2008-03-17 05:43:39 +0000

    --HG-- branch : brown extra : convert_revision : svn%3A15b6bd1d-d91c-0410-8971-a6cc2a4f8591/brown%40596

--- doc/samples/pushpull/plot.py
+++ doc/samples/pushpull/plot.py
@@ -23,15 +23,83 @@ def plot_theory( K ):
 
     farray = [ fraction_Sp( E1, E2, K ) for E1 in e1array ]
     farray = numpy.array( farray )
-    print farray
+    #print farray
 
     semilogx( e1array/E2, farray, label='K = %f' % K )
 
+def file_mean( filename, skip ):
+    ycolumns = [2,]
+    #ycolumns = [2,6]
+    #ycolumns = [3,5]
+    #ycolumns = [2,6,3,5]
 
-plot_theory( 0.01 )
-plot_theory( 0.05 )
-plot_theory( 0.1 )
-plot_theory( 1 )
-plot_theory( 10 )
-legend()
-show()
+    data = load( filename )
+    x = data[:,0]
+    y = data[:,ycolumns[0]]
+
+    start = x.searchsorted( skip )
+
+    x = x[start:]
+    y = y[start:]
+
+    xdiff = x[1:] - x[:-1] 
+    yscaled = y[:-1] * xdiff
+    yscaledmean = yscaled.sum() / ( x[-1] - x[0] )
+    print yscaledmean, y.mean()
+    #return y.mean()
+    return yscaledmean
+
+
+
+import glob
+import os
+
+S_tot = 300.0
+
+model = 'pushpull'
+#Keq_str = '0.05'
+Keq_str = '5'
+koff_ratio_str = '0.5'
+#koff_ratio_str = '0'
+N_P = 5
+V = '1e-14'
+T = '100'
+mode = 'normal'
+#mode = 'localized'
+
+skip = float(T) / 2
+
+dir = sys.argv[1]
+#pattern = sys.argv[2]
+#globpattern = pattern.replace('ALL','*') + '_*.dat'
+
+
+for N_K in ( 1, 2, 3, 4, 5 ):
+    globpattern = \
+        string.join( ( model, Keq_str, koff_ratio_str, str(N_K), 
+                       str(N_P), V, mode, T, '*' ), '_' )
+    print globpattern
+    filelist = glob.glob( dir + os.sep + globpattern )
+    data = numpy.zeros(len(filelist))
+
+    for i, file in enumerate( filelist ):
+        print file
+        data[i] = file_mean( file, skip )
+    print data
+    data /= S_tot
+    mean = data.mean()
+    std_err = data.std()/math.sqrt(len(data))
+    print mean, std_err
+
+    errorbar( float(N_K)/N_P, mean, yerr=std_err )
+
+plot_theory( float( Keq_str ) )
+
+figtitle = string.join( ( model, Keq_str, koff_ratio_str, 'ALL', 
+                          str(N_P), V, mode ), 
+                        '_' )
+title( figtitle )
+
+#show()
+savefig( 'figs2/' + figtitle + '.png', dpi=80 )
+
