200543fb4853f9e82b7d56af6d40def9020c5242
shafi <none@none>
2008-06-16 06:53:28 +0000

    --HG-- branch : brown extra : convert_revision : svn%3A15b6bd1d-d91c-0410-8971-a6cc2a4f8591/brown%40838

---
+++ test/SphericalBesselGenerator_test.cpp
@@ -0,0 +1,89 @@
+#define BOOST_AUTO_TEST_MAIN
+
+#define BOOST_TEST_MODULE SphericalBesselGenerator
+
+#include <boost/mpl/list.hpp>
+#include <boost/test/test_case_template.hpp>
+#include <boost/test/auto_unit_test.hpp>
+#include <boost/test/floating_point_comparison.hpp>
+
+#include "SphericalBesselGenerator.hpp"
+
+
+const unsigned int maxn( 50 );
+const unsigned int tableResolution( 100 );
+
+//SphericalBesselGenerator& generator( *sphericalBesselGenerator );
+SphericalBesselGenerator generator( maxn, tableResolution );
+
+const Real TOLERANCE( 1e-5 );
+
+BOOST_AUTO_TEST_CASE( testJ )
+{
+    const UnsignedInteger resolution( 100 );
+    const Real maxz( generator.maxz( maxn ) * 1.2 );
+
+    for( UnsignedInteger i( 0 ); i <= resolution; ++i )
+    {
+        const Real z( maxz * i / resolution );
+        
+        for( UnsignedInteger n( 0 ); n <= maxn; ++n )
+        {
+            const Real tj( generator.j( n, z ) );
+            const Real j( gsl_sf_bessel_jl( n, z ) );
+            
+            BOOST_CHECK_CLOSE( j, tj, TOLERANCE );
+
+            //printf("%d %g\n",n,z);
+        }
+    }
+
+}
+
+
+BOOST_AUTO_TEST_CASE( testY )
+{
+    const UnsignedInteger resolution( 100 );
+    const Real maxz( generator.maxz( maxn ) * 1.1 );
+
+    for( UnsignedInteger i( 1 ); i <= resolution; ++i )
+    {
+        const Real z( maxz * i / resolution );
+        
+        for( UnsignedInteger n( 0 ); n <= maxn; ++n )
+        {
+            const Real ty( generator.y( n, z ) );
+            const Real y( gsl_sf_bessel_yl( n, z ) );
+            
+            BOOST_CHECK_CLOSE( y, ty, TOLERANCE );
+
+            //printf("y %d %g\n",n,z);
+        }
+    }
+
+}
+
+/*
+BOOST_AUTO_TEST_CASE( testJ_large_x )
+{
+    const UnsignedInteger resolution( 100 );
+    const Real maxz( generator.maxz( maxn ) * 10000 );
+
+    for( UnsignedInteger i( 0 ); i <= resolution; ++i )
+    {
+        const Real z( maxz * i / resolution );
+        
+        for( UnsignedInteger n( 0 ); n <= maxn; ++n )
+        {
+            const Real tj( generator.j( n, z ) );
+            const Real j( gsl_sf_bessel_jl( n, z ) );
+            
+            BOOST_CHECK_CLOSE( j, tj, TOLERANCE );
+
+            //printf("j large %d %g\n",n,z);
+        }
+    }
+
+}
+*/
+
