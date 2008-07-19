372d8853650c592dd80f83280e4bd94a3088e80d
shafi <none@none>
2008-07-19 21:51:23 +0000

    --HG-- branch : brown extra : convert_revision : svn%3A15b6bd1d-d91c-0410-8971-a6cc2a4f8591/brown%40915

--- samples/hardbody/run.py
+++ samples/hardbody/run.py
@@ -7,58 +7,57 @@ from logger import *
 import sys
 import time
 
-# run
-T = float( sys.argv[1] )
-V = float( sys.argv[2] )
-N = int( sys.argv[3] )
 
-L = math.pow( V * 1e-3, 1.0 / 3.0 )
+def run_single( T, V, N ):
+
+    L = math.pow( V * 1e-3, 1.0 / 3.0 )
+
+    
+    s = EGFRDSimulator()
+    #s = BDSimulator()
+    s.setWorldSize( L )
+
+    matrixSize = min( max( 3, int( (3 * N) ** (1.0/3.0) ) ), 100 )
+    print 'matrixSize=', matrixSize
+    s.setMatrixSize( matrixSize )
+    
+    box1 = CuboidalSurface( [0,0,0],[L,L,L] )
+
+    A = Species( 'A', 1e-12, 2.5e-9 )
+    s.addSpecies( A )
+    
+    s.throwInParticles( A, N, box1 )
+    print 'stir'
+
+    stirTime = T * .1
+    while 1:
+        s.step()
+        nextTime = s.getNextTime()
+        if nextTime > stirTime:
+            s.stop( stirTime )
+            break
+    print 'reset'
+    s.reset()
+    print 'reset finish'
+
+    print 'run'
+    start = time.time()
+    while s.t < T:
+        s.step()
+        #l.log()
+
+    end = time.time()
+    print 'T =', T, '; V= ', V, '; N=', N
+    print 'TIMING:\n', end - start
+
+    return end - start
 
 
-s = EGFRDSimulator()
-#s = BDSimulator()
-s.setWorldSize( L )
+if __name__ == '__main__':
+    
+    T = float( sys.argv[1] )
+    V = float( sys.argv[2] )
+    N = int( sys.argv[3] )
 
-matrixSize = min( max( 3, int( (3 * N) ** (1.0/3.0) ) ), 100 )
-print 'matrixSize=', matrixSize
-s.setMatrixSize( matrixSize )
+    run_single( T, V, N )
 
-#print int( N ** (1.0/3.0) )
-#sys.exit(0)
-#s.setMatrixSize(10)
-#s.setMatrixSize( 30 )
-
-box1 = CuboidalSurface( [0,0,0],[L,L,L] )
-
-A = Species( 'A', 1e-12, 2.5e-9 )
-s.addSpecies( A )
-
-# no reaction
-
-s.throwInParticles( A, N, box1 )
-print 'stir'
-#stirTime = 0
-stirTime = T * .1
-while 1:
-    s.step()
-    nextTime = s.getNextTime()
-    if nextTime > stirTime:
-        s.stop( stirTime )
-        break
-print 'reset'
-s.reset()
-print 'reset finish'
-#l = Logger( s, 'hardbody' )
-#l.setParticleOutput( ('A',) )
-#l.setInterval( 1e-1 )
-#l.log()
-
-print 'run'
-start = time.time()
-while s.t < T:
-    s.step()
-    #l.log()
-
-end = time.time()
-print 'T =', T, '; V= ', V, '; N=', N
-print 'TIMING:\n', end - start
