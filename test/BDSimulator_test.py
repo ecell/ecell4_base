fbb232ad8cafe9f0392f112ccc09059d112e07d5
moriyoshi <none@none>
2009-04-27 12:39:39 +0000

    * Set up the fixture in setUp() just in the same way as EGFRDSimulator_test.py
    --HG--
    extra : convert_revision : svn%3A15b6bd1d-d91c-0410-8971-a6cc2a4f8591/epdp/trunk%401112

--- test/BDSimulator_test.py
+++ test/BDSimulator_test.py
@@ -9,74 +9,61 @@ from bd import *
 class BDSimulatorTestCase( unittest.TestCase ):
 
     def setUp(self):
-        pass
+        self.s = BDSimulator()
+        self.s.setWorldSize( 1e-5 )
+        self.S = Species( 'S', 2e-11, 5e-8 )
+        self.s.addSpecies( self.S )
+        self.A = Species( 'A', 0, 1e-8 )
+        self.s.addSpecies( self.A )
+        self.B = Species( 'B', 2e-11, 5e-9 )
+        self.s.addSpecies( self.B )
 
     def tearDown(self):
         pass
     
     def test_instantiation( self ):
-        s = BDSimulator()
-        self.failIf( s == None )
+        self.failIf( self.s == None )
 
     
     def test_OneParticle( self ):
-        s = BDSimulator()
-        s.setWorldSize( 1e-5 )
-        S = Species( 'S', 2e-11, 5e-8 )
-        s.addSpecies( S )
-        s.placeParticle( S, [0.0,0.0,0.0] )
+        self.s.placeParticle( self.S, [0.0,0.0,0.0] )
 
-        t = s.t
+        t = self.s.t
         for i in range( 5 ):
-            s.step()
-        self.failIf( t == s.t )
+            self.s.step()
+        self.failIf( t == self.s.t )
 
     def test_TwoParticles( self ):
-        s = BDSimulator()
-        s.setWorldSize( 1e-5 )
-        S = Species( 'S', 2e-11, 5e-8 )
-        s.addSpecies( S )
-        s.placeParticle( S, [0.0,0.0,0.0] )
-        s.placeParticle( S, [5e-6,5e-6,5e-6] )
+        self.s.placeParticle( self.S, [0.0,0.0,0.0] )
+        self.s.placeParticle( self.S, [5e-6,5e-6,5e-6] )
 
-        t = s.t
+        t = self.s.t
         for i in range( 5 ):
-            s.step()
-        self.failIf( t == s.t )
+            self.s.step()
+        self.failIf( t == self.s.t )
 
     def test_ThreeParticles( self ):
-        s = BDSimulator()
-        s.setWorldSize( 1e-5 )
-        S = Species( 'S', 2e-11, 5e-8 )
-        s.addSpecies( S )
-        s.placeParticle( S, [0.0,0.0,0.0] )
-        s.placeParticle( S, [5e-6,5e-6,5e-6] )
-        s.placeParticle( S, [1e-7,1e-7,1e-7] )
+        self.s.placeParticle( self.S, [0.0,0.0,0.0] )
+        self.s.placeParticle( self.S, [5e-6,5e-6,5e-6] )
+        self.s.placeParticle( self.S, [1e-7,1e-7,1e-7] )
 
-        t = s.t
+        t = self.s.t
         for i in range( 5 ):
-            s.step()
-        self.failIf( t == s.t )
+            self.s.step()
+        self.failIf( t == self.s.t )
 
     def test_immobile_is_immobile( self ):
-        s = BDSimulator()
-        s.setWorldSize( 1e-5 )
-        A = Species( 'A', 0, 1e-8 )
-        s.addSpecies( A )
-        B = Species( 'B', 2e-11, 5e-9 )
-        s.addSpecies( B )
-
-        particleA = s.placeParticle( A, [0.0,0.0,0.0] )
-        s.placeParticle( B, [1.5000001e-8,0.0,0.0] )
+        particleA = self.s.placeParticle( self.A, [0.0,0.0,0.0] )
+        self.s.placeParticle( self.B, [1.5000001e-8,0.0,0.0] )
 
         initialPosition = particleA.getPos().copy()
 
         for i in range( 10 ):
-            s.step()
+            self.s.step()
             print particleA.getPos()
         
         newPosition = particleA.getPos().copy()
-        dist = s.distance( initialPosition, newPosition )
+        dist = self.s.distance( initialPosition, newPosition )
 
         self.failIf( dist != 0, 'initial pos: %s,\tnew pos: %s' %
                      ( initialPosition, newPosition ) )
