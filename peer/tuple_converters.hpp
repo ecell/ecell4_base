0d02f7921a09e2a58b8b37b9f016276d49f6c252
moriyoshi <none@none>
2009-07-08 07:14:37 +0000

    * Merge from object_matrix_standalone
    --HG--
    extra : convert_revision : svn%3A15b6bd1d-d91c-0410-8971-a6cc2a4f8591/epdp/trunk%401255

--- peer/tuple_converters.hpp
+++ peer/tuple_converters.hpp
@@ -10,6 +10,13 @@
 #include <boost/range/const_iterator.hpp>
 #include <boost/range/begin.hpp>
 #include <boost/range/end.hpp>
+#include <boost/preprocessor/cat.hpp>
+#include <boost/preprocessor/arithmetic/sub.hpp>
+#include <boost/preprocessor/repetition/repeat_from_to.hpp>
+#include <boost/preprocessor/repetition/enum.hpp>
+#include <boost/preprocessor/repetition/enum_params.hpp>
+
+#include "utils.hpp"
 
 namespace peer {
 
@@ -32,7 +39,7 @@ namespace util
                 PyObject*, const boost::tuples::null_type&, Py_ssize_t) {}
 
         template<typename Ttuple_>
-        struct tuple_to_tuple_converter
+        struct tuple_to_pytuple_converter
         {
             typedef Ttuple_ argument_value_type;
             typedef const argument_value_type& argument_type;
@@ -46,7 +53,7 @@ namespace util
         };
 
         template<typename Tfirst_, typename Tsecond_>
-        struct tuple_to_tuple_converter<std::pair<Tfirst_, Tsecond_> >
+        struct tuple_to_pytuple_converter<std::pair<Tfirst_, Tsecond_> >
         {
             typedef std::pair<Tfirst_, Tsecond_> argument_value_type;
             typedef const argument_value_type& argument_type;
@@ -60,14 +67,98 @@ namespace util
         };
 
         template<typename Ttuple_>
-        struct tuple_to_tuple_converter<boost::shared_ptr<Ttuple_> >
+        struct tuple_to_pytuple_converter<boost::shared_ptr<Ttuple_> >
         {
             typedef Ttuple_ argument_value_type;
             typedef boost::shared_ptr<argument_value_type> argument_type;
             static PyObject* convert(argument_type val)
             {
-                return tuple_to_tuple_converter<argument_value_type>::convert(
+                return tuple_to_pytuple_converter<argument_value_type>::convert(
                         *val);
+            }
+        };
+
+        template<typename Ttuple_>
+        struct pytuple_to_tuple_converter
+        {
+            static void* convertible(PyObject* pyo)
+            {
+                return 0;
+            }
+
+            static void construct(PyObject* pyo,
+                                  boost::python::converter::rvalue_from_python_stage1_data* data)
+            {
+            }
+        };
+
+#define TUPLE_CONVERTERS_PYTUPLE_TO_TUPLE_TEMPLATE_EXTRACT(__z__, __n__, __v__) \
+        boost::python::extract<BOOST_PP_CAT(__v__,__n__)>(PySequence_GetItem(pyo, __n__))
+
+#define TUPLE_CONVERTERS_PYTUPLE_TO_TUPLE_TEMPLATE(__z__, __n__, __v__) \
+        template<BOOST_PP_ENUM_PARAMS(__n__, typename T)> \
+        struct pytuple_to_tuple_converter<boost::tuples::tuple<BOOST_PP_ENUM_PARAMS(__n__, T)> > \
+        { \
+            typedef boost::tuples::tuple<BOOST_PP_ENUM_PARAMS(__n__, T)> native_type; \
+\
+            static void* convertible(PyObject* pyo) \
+            { \
+                if (!PySequence_Check(pyo)) \
+                { \
+                    return 0; \
+                } \
+\
+                if (PySequence_Size(pyo) != __n__) \
+                { \
+                    return 0; \
+                } \
+                return pyo; \
+            } \
+ \
+            static void construct(PyObject* pyo, \
+                                  boost::python::converter::rvalue_from_python_stage1_data* data) \
+            { \
+                void* storage(reinterpret_cast< \
+                    boost::python::converter::rvalue_from_python_storage<native_type>*>(data)->storage.bytes); \
+                data->convertible = new (storage) native_type( \
+                    BOOST_PP_ENUM(__n__, TUPLE_CONVERTERS_PYTUPLE_TO_TUPLE_TEMPLATE_EXTRACT, T)); \
+            } \
+        };
+
+        BOOST_PP_REPEAT_FROM_TO(1, 10, TUPLE_CONVERTERS_PYTUPLE_TO_TUPLE_TEMPLATE, );
+
+#undef TUPLE_CONVERTERS_PYTUPLE_TO_TUPLE_TEMPLATE_NULLTYPE
+#undef TUPLE_CONVERTERS_PYTUPLE_TO_TUPLE_TEMPLATE_EXTRACT
+#undef TUPLE_CONVERTERS_PYTUPLE_TO_TUPLE_TEMPLATE
+
+
+        template<typename Tfirst_, typename Tsecond_>
+        struct pytuple_to_tuple_converter<std::pair<Tfirst_, Tsecond_> >
+        {
+            typedef std::pair<Tfirst_, Tsecond_> native_type;
+ 
+            static void* convertible(PyObject* pyo)
+            {
+                if (!PySequence_Check(pyo))
+                {
+                    return 0;
+                }
+
+                if (PySequence_Size(pyo) != 2)
+                {
+                    return 0;
+                }
+                return pyo;
+            }
+
+            static void construct(PyObject* pyo,
+                                  boost::python::converter::rvalue_from_python_stage1_data* data)
+            {
+                void* storage(reinterpret_cast<
+                    boost::python::converter::rvalue_from_python_storage<native_type>*>(data)->storage.bytes);
+                data->convertible = new (storage) native_type(
+                    boost::python::extract<Tfirst_>(PySequence_GetItem(pyo, 0)),
+                    boost::python::extract<Tsecond_>(PySequence_GetItem(pyo, 1)));
             }
         };
 
@@ -97,10 +188,11 @@ namespace util
         if (!registered)
         {
             boost::python::to_python_converter<
-                Ttuple_, detail::tuple_to_tuple_converter<Ttuple_> >();
+                Ttuple_, detail::tuple_to_pytuple_converter<Ttuple_> >();
             boost::python::to_python_converter<
                 boost::shared_ptr<Ttuple_>,
-                detail::tuple_to_tuple_converter<boost::shared_ptr<Ttuple_> > >();
+                detail::tuple_to_pytuple_converter<boost::shared_ptr<Ttuple_> > >();
+            to_native_converter<Ttuple_, detail::pytuple_to_tuple_converter<Ttuple_> >();
             registered = true;
         }
     }
