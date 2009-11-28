38898a2a0b63fa89d8c2fe1296dc796404e7dfea
shafi <none@none>
2009-11-28 08:28:03 +0000

    * fix: from numpy.random import uniform
    --HG--
    extra : convert_revision : svn%3A15b6bd1d-d91c-0410-8971-a6cc2a4f8591/epdp/trunk%401318

--- bd.py
+++ bd.py
@@ -6,7 +6,7 @@ import math
 
 import random
 import numpy
-
+from numpy.random import uniform
 
 from utils import *
 from surface import *
@@ -154,7 +154,7 @@ class BDSimulatorCoreBase( object ):
 
                     p = self.getP_acct( rt, D12, radius12 )
 
-                    rnd = numpy.uniform()
+                    rnd = uniform()
 
                     if p > rnd:
                         if __debug__:
@@ -184,7 +184,7 @@ class BDSimulatorCoreBase( object ):
         if not reactionTypes:
             return None  # no reaction
 
-        rnd = numpy.uniform() / self.dt
+        rnd = uniform() / self.dt
 
         # handle the most common case efficiently.
         if len( reactionTypes ) == 1:  
@@ -246,7 +246,7 @@ class BDSimulatorCoreBase( object ):
 
             for i in range( 100 ):
 
-                rnd = numpy.uniform()
+                rnd = uniform()
                 pairDistance = drawR_gbd( rnd, radius12, self.dt, D12 )
 
                 unitVector = randomUnitVector()
