f87209080511757eb6aa46a9b3d35f4ad3b3c84d
moriyoshi <none@none>
2009-12-14 04:53:17 +0000

    * Add missing file.
    --HG--
    extra : convert_revision : svn%3A15b6bd1d-d91c-0410-8971-a6cc2a4f8591/epdp/trunk%401397

---
+++ myrandom.py
@@ -0,0 +1,30 @@
+from _gfrd import RandomNumberGenerator
+import numpy
+
+__all__ = (
+    'shuffle',
+    'uniform',
+    'normal',
+    'seed',
+    )
+
+rng = RandomNumberGenerator()
+
+def uniform(min=0.0, max=1.0, size=None):
+    global rng
+    if size is None:
+        return rng.uniform(min, max)
+    retval = numpy.ndarray(dtype=numpy.float64, shape=size)
+    f = retval.flat
+    for i in range(len(f)):
+        f[i] = uniform(min, max)
+    return retval
+
+normal = rng.normal
+seed = rng.seed
+
+def shuffle(seq):
+    for i in reversed(range(0, len(seq))):
+        j = rng.uniform_int(0, i)
+        seq[i], seq[j] = seq[j], seq[i]
+
