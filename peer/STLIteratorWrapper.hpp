60ef2722e4b8f6fd971a2f17db4697aa61259fb6
Moriyoshi Koizumi <mozo@riken.jp>
2010-02-18 11:29:25 +0900

    Fix :)
    --HG--
    branch : world-integration

--- peer/STLIteratorWrapper.hpp
+++ peer/STLIteratorWrapper.hpp
@@ -12,28 +12,29 @@ class STLIteratorWrapper
 {
 protected:
     PyObject_VAR_HEAD
-    Titer_ theIdx;
-    Titer_ theEnd; 
+    Titer_ i_;
+    Titer_ end_; 
 
 public:
     static PyTypeObject __class__;
 
 public:
-    void* operator new( size_t )
+    void* operator new(size_t)
     {
-        return PyObject_New( STLIteratorWrapper, &__class__ );
+        return PyObject_New(STLIteratorWrapper, &__class__);
     }
 
-    void operator delete( void* ptr )
+    void operator delete(void* ptr)
     {
-        reinterpret_cast< PyObject* >( ptr )->ob_type->tp_free( reinterpret_cast< PyObject* >( ptr ) );
+        reinterpret_cast<PyObject*>(ptr)->ob_type->tp_free(reinterpret_cast< PyObject*>(ptr));
     }
 
     template< typename Trange_ >
-    STLIteratorWrapper( Trange_ const& range )
-        : theIdx( boost::begin( range ) ), theEnd( boost::end( range ) )
-    {
-    }
+    STLIteratorWrapper(Trange_ const& range)
+        : i_(boost::begin(range )), end_(boost::end(range)) {}
+
+    STLIteratorWrapper(Titer_ const& begin, Titer_ const& end)
+        : i_(begin), end_(end) {}
 
     ~STLIteratorWrapper()
     {
@@ -42,73 +43,73 @@ public:
 public:
     static PyTypeObject* __class_init__()
     {
-        PyType_Ready( &__class__ );
+        PyType_Ready(&__class__);
         return &__class__;
     }
 
-    template< typename Trange_ >
-    static STLIteratorWrapper* create( Trange_ const& range )
+    template<typename Trange_>
+    static STLIteratorWrapper* create(Trange_ const& range)
     {
-        return new STLIteratorWrapper( range );
+        return new STLIteratorWrapper(range);
     }
 
-    static void __dealloc__( STLIteratorWrapper* self )
+    static void __dealloc__(STLIteratorWrapper* self)
     {
         delete self;
     }
 
-    static PyObject* __next__( STLIteratorWrapper* self )
+    static PyObject* __next__(STLIteratorWrapper* self)
     {
-        if ( self->theIdx == self->theEnd )
+        if ( self->i_ == self->end_ )
             return NULL;
 
-        return boost::python::incref( boost::python::object( *self->theIdx ).ptr() );
+        return boost::python::incref(boost::python::object(*self->i_ ++).ptr());
     }
 };
 
 template< typename Titer_ >
 PyTypeObject STLIteratorWrapper< Titer_ >::__class__ = {
-	PyObject_HEAD_INIT( &PyType_Type )
-	0,					/* ob_size */
-	"ecell._ecs.STLIteratorWrapper", /* tp_name */
-	sizeof( STLIteratorWrapper ), /* tp_basicsize */
-	0,					/* tp_itemsize */
-	/* methods */
-	(destructor)&STLIteratorWrapper::__dealloc__, /* tp_dealloc */
-	0,					/* tp_print */
-	0,					/* tp_getattr */
-	0,					/* tp_setattr */
-	0,					/* tp_compare */
-	0,					/* tp_repr */
-	0,					/* tp_as_number */
-	0,					/* tp_as_sequence */
-	0,					/* tp_as_mapping */
-	0,					/* tp_hash */
-	0,					/* tp_call */
-	0,					/* tp_str */
-	PyObject_GenericGetAttr,		/* tp_getattro */
-	0,					/* tp_setattro */
-	0,					/* tp_as_buffer */
-	Py_TPFLAGS_HAVE_CLASS | Py_TPFLAGS_HAVE_WEAKREFS | Py_TPFLAGS_HAVE_ITER,/* tp_flags */
-	0,					/* tp_doc */
-	0,	/* tp_traverse */
-	0,					/* tp_clear */
-	0,					/* tp_richcompare */
-	0,					/* tp_weaklistoffset */
-	PyObject_SelfIter,  /* tp_iter */
-	(iternextfunc)&STLIteratorWrapper::__next__,		/* tp_iternext */
-	0,		        	/* tp_methods */
-	0,					/* tp_members */
-	0,                  /* tp_getset */
-	0,					/* tp_base */
-	0,					/* tp_dict */
-	0,					/* tp_descr_get */
-	0,					/* tp_descr_set */
-	0,					/* tp_dictoffset */
-	0,			        /* tp_init */
-	PyType_GenericAlloc,			/* tp_alloc */
-	PyType_GenericNew,			/* tp_new */
-	PyObject_Del,			/* tp_free */
+    PyObject_HEAD_INIT( &PyType_Type )
+    0,                    /* ob_size */
+    "ecell._ecs.STLIteratorWrapper", /* tp_name */
+    sizeof( STLIteratorWrapper ), /* tp_basicsize */
+    0,                    /* tp_itemsize */
+    /* methods */
+    (destructor)&STLIteratorWrapper::__dealloc__, /* tp_dealloc */
+    0,                    /* tp_print */
+    0,                    /* tp_getattr */
+    0,                    /* tp_setattr */
+    0,                    /* tp_compare */
+    0,                    /* tp_repr */
+    0,                    /* tp_as_number */
+    0,                    /* tp_as_sequence */
+    0,                    /* tp_as_mapping */
+    0,                    /* tp_hash */
+    0,                    /* tp_call */
+    0,                    /* tp_str */
+    PyObject_GenericGetAttr,        /* tp_getattro */
+    0,                    /* tp_setattro */
+    0,                    /* tp_as_buffer */
+    Py_TPFLAGS_HAVE_CLASS | Py_TPFLAGS_HAVE_WEAKREFS | Py_TPFLAGS_HAVE_ITER,/* tp_flags */
+    0,                    /* tp_doc */
+    0,                    /* tp_traverse */
+    0,                    /* tp_clear */
+    0,                    /* tp_richcompare */
+    0,                    /* tp_weaklistoffset */
+    PyObject_SelfIter,  /* tp_iter */
+    (iternextfunc)&STLIteratorWrapper::__next__,        /* tp_iternext */
+    0,                    /* tp_methods */
+    0,                    /* tp_members */
+    0,                    /* tp_getset */
+    0,                    /* tp_base */
+    0,                    /* tp_dict */
+    0,                    /* tp_descr_get */
+    0,                    /* tp_descr_set */
+    0,                    /* tp_dictoffset */
+    0,                    /* tp_init */
+    PyType_GenericAlloc,  /* tp_alloc */
+    PyType_GenericNew,    /* tp_new */
+    PyObject_Del,         /* tp_free */
 };
 
 namespace util {
