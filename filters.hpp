df46d6fa4c979e7ff6c29425fdedf8de6520c4be
Moriyoshi Koizumi <mozo@riken.jp>
2010-03-01 15:52:07 +0900

    They should be spheres, not shapes.

--- filters.hpp
+++ filters.hpp
@@ -8,17 +8,17 @@
 #include <boost/type_traits/is_const.hpp>
 #include "Shape.hpp"
 
-template<typename Toc_, typename Tfun_, typename Tshape_>
+template<typename Toc_, typename Tfun_, typename Tsphere_>
 class neighbor_filter
         : public std::unary_function<
             typename boost::range_iterator<Toc_>::type, void>
 {
     typedef typename boost::range_iterator<Toc_>::type argument_type;
     typedef void result_type;
-    typedef Tshape_ shape_type;
+    typedef Tsphere_ sphere_type;
 
 public:
-    inline neighbor_filter(Tfun_& next, const shape_type& cmp)
+    inline neighbor_filter(Tfun_& next, const sphere_type& cmp)
         : next_(next), cmp_(cmp) {}
 
     inline result_type operator()(argument_type i) const
@@ -31,7 +31,7 @@ public:
             return;
         }
 */
-        const typename shape_type::length_type dist(
+        const typename sphere_type::length_type dist(
             distance(shape(item.second), cmp_.position()));
         if (dist < cmp_.radius())
         {
@@ -41,24 +41,24 @@ public:
 
 private:
     Tfun_& next_;
-    const shape_type cmp_;
+    const sphere_type cmp_;
 };
 
-template<typename Toc_, typename Tfun_, typename Tshape_>
-inline void take_neighbor(Toc_& oc, Tfun_& fun, const Tshape_& cmp)
+template<typename Toc_, typename Tfun_, typename Tsphere_>
+inline void take_neighbor(Toc_& oc, Tfun_& fun, const Tsphere_& cmp)
 {
     oc.each_neighbor(oc.index(cmp.position()),
-                     neighbor_filter<Toc_, Tfun_, Tshape_>(fun, cmp));
+                     neighbor_filter<Toc_, Tfun_, Tsphere_>(fun, cmp));
 }
 
-template<typename Toc_, typename Tfun_, typename Tshape_>
-inline void take_neighbor(Toc_ const& oc, Tfun_& fun, const Tshape_& cmp)
+template<typename Toc_, typename Tfun_, typename Tsphere_>
+inline void take_neighbor(Toc_ const& oc, Tfun_& fun, const Tsphere_& cmp)
 {
     oc.each_neighbor(oc.index(cmp.position()),
-                     neighbor_filter<Toc_ const, Tfun_, Tshape_>(fun, cmp));
+                     neighbor_filter<Toc_ const, Tfun_, Tsphere_>(fun, cmp));
 }
 
-template<typename Toc_, typename Tfun_, typename Tshape_>
+template<typename Toc_, typename Tfun_, typename Tsphere_>
 class cyclic_neighbor_filter
         : public std::binary_function<
             typename boost::range_iterator<Toc_>::type,
@@ -68,11 +68,11 @@ class cyclic_neighbor_filter
     typedef typename boost::range_iterator<Toc_>::type first_argument_type;
     typedef const typename Toc_::position_type& second_argument_type;
     typedef void result_type;
-    typedef Tshape_ shape_type;
+    typedef Tsphere_ sphere_type;
 
 public:
     inline cyclic_neighbor_filter(Tfun_& next,
-            const shape_type& cmp)
+            const sphere_type& cmp)
         : next_(next), cmp_(cmp) {}
 
     inline result_type operator()(first_argument_type i,
@@ -85,7 +85,7 @@ public:
             return;
         }
 */
-        const typename shape_type::length_type dist(
+        const typename sphere_type::length_type dist(
             distance(shape(offset(item.second, p)), cmp_.position()));
         if (dist < cmp_.radius())
         {
@@ -95,21 +95,21 @@ public:
 
 private:
     Tfun_& next_;
-    const shape_type cmp_;
+    const sphere_type cmp_;
 };
 
-template<typename Toc_, typename Tfun_, typename Tshape_>
-inline void take_neighbor_cyclic(Toc_& oc, Tfun_& fun, const Tshape_& cmp)
+template<typename Toc_, typename Tfun_, typename Tsphere_>
+inline void take_neighbor_cyclic(Toc_& oc, Tfun_& fun, const Tsphere_& cmp)
 {
     oc.each_neighbor_cyclic(oc.index(cmp.position()),
-            cyclic_neighbor_filter<Toc_, Tfun_, Tshape_>(fun, cmp));
+            cyclic_neighbor_filter<Toc_, Tfun_, Tsphere_>(fun, cmp));
 }
 
-template<typename Toc_, typename Tfun_, typename Tshape_>
-inline void take_neighbor_cyclic(Toc_ const& oc, Tfun_& fun, const Tshape_& cmp)
+template<typename Toc_, typename Tfun_, typename Tsphere_>
+inline void take_neighbor_cyclic(Toc_ const& oc, Tfun_& fun, const Tsphere_& cmp)
 {
     oc.each_neighbor_cyclic(oc.index(cmp.position()),
-            cyclic_neighbor_filter<Toc_ const, Tfun_, Tshape_>(fun, cmp));
+            cyclic_neighbor_filter<Toc_ const, Tfun_, Tsphere_>(fun, cmp));
 }
 
 #endif /* ALGORITHM_HPP */
