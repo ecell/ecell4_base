b69446fa048ae0cad7c5cc7b194fd07410ebf816
Moriyoshi Koizumi <mozo@riken.jp>
2010-03-15 11:33:43 +0900

    Remove debug print
    --HG--
    branch : world-integration

--- pyGFRD.cpp
+++ pyGFRD.cpp
@@ -32,6 +32,7 @@
 
 #include "utils/array_traits.hpp"
 #include "utils.hpp"
+#include "gsl_rng_base.hpp"
 #include "geometry.hpp"
 #include "MatrixSpace.hpp"
 #include "Vector3.hpp"
@@ -657,6 +658,52 @@ typename T_::particle_id_pair_and_distance_list* World_check_overlap(
 {
     return world.check_overlap(s, ignore);
 }
+
+struct static_gsl_rng: public gsl_rng_base<static_gsl_rng>
+{
+    typedef unsigned int result_type;
+
+    static const char name[];
+    static const unsigned long int min = 0;
+    static const unsigned long int max = static_cast<result_type>(-1);
+
+    void set(unsigned long int seed)
+    {
+        idx_ = std::min(seed, PyObject_Size(seq_.ptr()));
+    }
+
+    unsigned long int get()
+    {
+        Py_ssize_t nelems(PyObject_Size(seq_.ptr()));
+        if (idx_ >= nelems)
+        {
+            return min; 
+        }
+        boost::python::handle<> i(
+                boost::python::allow_null(
+                    PySequence_GetItem(seq_.ptr(), idx_)));
+        if (!i)
+        {
+            return min;
+        }
+        ++idx_;
+        return PyLong_AsUnsignedLong(i.get());
+    }
+
+    double get_double()
+    {
+        return static_cast<double>(get()) / (static_cast<double>(max) + 1);
+    }
+
+    static_gsl_rng(boost::python::object seq)
+        : seq(seq_), idx_(0) {}
+
+private:
+    boost::python::object seq_;
+    Py_ssize_t idx_;
+};
+
+const char[] static_gsl_rng::name = "static_gsl_rng";
 
 BOOST_PYTHON_MODULE( _gfrd )
 {
@@ -1286,6 +1333,7 @@ BOOST_PYTHON_MODULE( _gfrd )
 
     peer::RandomNumberGenerator<GSLRandomNumberGenerator>::__register_class("RandomNumberGenerator");
     def("create_gsl_rng", &create_gsl_rng<gsl_rng_mt19937>);
+    def("create_static_gsl_rng", &create_gsl_rng<static_gsl_rng>);
 
     peer::util::register_scalar_to_native_converters();
 }
