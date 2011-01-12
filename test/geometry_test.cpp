8e3d9b555ea6ba834e4e2c3470607df163aaa7bf
thomie <miedema@amolf.nl>
2011-01-12 11:38:20 +0100

    Fix geometry_test
    Or maybe it was supposed to fail?
     r[1]{1.2246063538223773e-16} is close enough to zero for me.

--- test/geometry_test.cpp
+++ test/geometry_test.cpp
@@ -22,8 +22,9 @@ BOOST_AUTO_TEST_CASE(test_rotate_vector)
     {
         boost::array<double, 3> const r(rotate_vector(array_gen(1., 0., 0.),
                                               array_gen(0., 0., 1.), M_PI));
-        BOOST_CHECK_CLOSE(r[0], -1., 1);
-        BOOST_CHECK_CLOSE(0., r[1], 100);
-        BOOST_CHECK_CLOSE(r[2], 0., 1);
+        BOOST_CHECK_CLOSE(r[0], -1., 0.000000001);
+        // r[1]{1.2246063538223773e-16}
+        BOOST_CHECK_SMALL(r[1], 0.000000001);
+        BOOST_CHECK_CLOSE(r[2], 0., 0.0000000001);
     }
 }
