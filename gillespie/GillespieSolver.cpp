bd12e7afd0d43a1842c5df9db47d69f5f9959e44
YukiSakamoto <tromso@YukiSakamoto-Air.local>
2012-11-10 14:29:31 +0900

    separate definitions of GillespieWorld class into another files.

--- gillespie/GillespieSolver.cpp
+++ gillespie/GillespieSolver.cpp
@@ -72,33 +72,6 @@ int combination(int n, int k) {
 	return permutation(n, kk) / factorial(kk);
 }
 
-//============================================================
-//	World	*Definitions
-//============================================================
-World::World(void)
-{
-	this->current_t = 0.0;
-}
-double World::get_current_time(void)
-{	return this->current_t;	}
-
-void World::set_current_time(double new_t)
-{	this->current_t = new_t;	}
-
-int World::get_current_state(int id)
-{	
-	return this->current_state[id];	
-}
-
-void World::set_current_state(int id, int number)
-{	
-	this->current_state[id] = number;	
-}
-
-void World::add_specie(int id, int number = 0)
-{
-	this->current_state.insert(std::map<int,int>::value_type(id, number));
-}
 
 //============================================================
 //	Model 	*Definitions
--- gillespie/GillespieSolver.hpp
+++ gillespie/GillespieSolver.hpp
@@ -6,23 +6,8 @@
 #include <gsl/gsl_sf_log.h>
 
 
-//============================================================
-//	World	
-//============================================================
-class World {
-private:
-public:	// Data members
-	double current_t;
-	std::map<int,int> current_state;	// [id] -> number of substrate	// id => 分子の個数　
+#include "./GillespieWorld.hpp"
 
-public:
-	World();
-	double get_current_time(void);
-	void set_current_time(double new_t);
-	int get_current_state(int id);
-	void set_current_state(int id, int number);
-	void add_specie(int id, int number);
-};
 
 //============================================================
 //	Model and supporting classes.
