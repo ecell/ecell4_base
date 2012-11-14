bb72ad0bedf3ae5c0d723ff3bd94e9c5a654feb8
Kazunari Kaizu <kaizu@tcs2.gsc.riken.jp>
2012-11-14 19:29:51 +0900

    added interfaces for Space and CompartmentSpace

---
+++ CompartmentSpace.hpp
@@ -0,0 +1,24 @@
+#ifndef __COMPARTMENT_SPACE_HPP
+#define __COMPARTMENT_SPACE_HPP
+
+#include "types.hpp"
+#include "Space.hpp"
+
+namespace ecell4
+{
+
+class CompartmentSpace
+    : public Space
+{
+    // not implemented yet
+};
+
+class CompartmentSpaceVectorImpl
+    : public CompartmentSpace
+{
+    // not implemented yet
+};
+
+} // ecell4
+
+#endif /* __COMPARTMENT_SPACE_HPP */
---
+++ Space.hpp
@@ -0,0 +1,17 @@
+#ifndef __SPACE_HPP
+#define __SPACE_HPP
+
+#include "types.hpp"
+
+
+namespace ecell4
+{
+
+class Space
+{
+    // not implemented yet
+};
+
+} // ecell4
+
+#endif /* __SPACE_HPP */
