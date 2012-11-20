c8dc07b4c4bb85186fe2aae52a529e9ee4b4c450
Kazunari Kaizu <kaizu@tcs2.gsc.riken.jp>
2012-11-20 19:33:51 +0900

    added the method to shuffle a vector in a random manner

--- core/RandomNumberGenerator.hpp
+++ core/RandomNumberGenerator.hpp
@@ -1,6 +1,7 @@
 #ifndef __RANDOM_NUMBER_GENERATOR_HPP
 #define __RANDOM_NUMBER_GENERATOR_HPP
 
+#include <vector>
 #include <boost/shared_ptr.hpp>
 #include <gsl/gsl_rng.h>
 #include <gsl/gsl_randist.h>
@@ -16,8 +17,21 @@ class RandomNumberGenerator
 public:
 
     virtual Real uniform(Real min, Real max) = 0;
+    virtual Integer uniform_int(Integer min, Integer max) = 0;
     virtual void seed(Integer val) = 0;
 };
+
+template <typename Telem_>
+inline void shuffle(RandomNumberGenerator& rng, std::vector<Telem_>& cont)
+{
+    typedef std::vector<Telem_> container_type;
+    for (typename container_type::size_type i(cont.size()); i > 0;)
+    {
+        --i;
+        typename container_type::size_type const j(rng.uniform_int(0, i));
+        std::swap(cont[i], cont[j]);
+    }
+}
 
 class GSLRandomNumberGenerator
     : public RandomNumberGenerator
@@ -29,6 +43,11 @@ public:
     Real uniform(Real min, Real max)
     {
         return gsl_rng_uniform(rng_.get()) * (max - min) + min;
+    }
+
+    Integer uniform_int(Integer min, Integer max)
+    {
+        return gsl_rng_uniform_int(rng_.get(), max - min + 1) + min;
     }
 
     void seed(Integer val)
