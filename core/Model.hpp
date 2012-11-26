9e1aa2ddcd3558dc5dbc49c196bde94ace12fa48
Kazunari Kaizu <kaizu@tcs2.gsc.riken.jp>
2012-11-26 16:57:21 +0900

    remodeled Model interface

--- core/Model.hpp
+++ core/Model.hpp
@@ -4,6 +4,7 @@
 #include "types.hpp"
 #include "Species.hpp"
 #include "ReactionRule.hpp"
+#include "exceptions.hpp"
 
 
 namespace ecell4
@@ -13,10 +14,47 @@ class Model
 {
 public:
 
+    /**
+     * a fundamental function to query unimolecular reaction rules from a reactant.
+     * this must be overloaded by any sub classes of Model.
+     * @param species Species of a reactant
+     * @return the vector of ReactionRule(s)
+     */
     virtual ReactionRuleVector query_reaction_rules(
         Species const& sp) const = 0;
+
+    /**
+     * a fundamental function to query bimolecular reaction rules from reactants.
+     * this must be overloaded by any sub classes of Model.
+     * @param species1 Species of the first reactant
+     * @param species2 Species of the second reactant
+     * @return the vector of ReactionRule(s)
+     */
     virtual ReactionRuleVector query_reaction_rules(
         Species const& sp1, Species const& sp2) const = 0;
+
+    /**
+     * add a concrete species to the model.
+     * this function is a part of the trait of NetworkModel.
+     * @param species a new Species
+     * @return if the species is not registered yet.
+     */
+    virtual bool add_species(Species const& sp)
+    {
+        throw NotSupported("add_species is not supported in this model class");
+    }
+
+    /**
+     * add a concrete reaction rule to the model.
+     * this function is a part of the trait of NetworkModel.
+     * @param rr a new ReactionRule
+     * @return if the reaction rule is not registered yet.
+     */
+    virtual bool add_reaction_rule(ReactionRule const& rr)
+    {
+        throw NotSupported(
+            "add_reaction_rule is not supported in this model class");
+    }
 };
 
 } // ecell4
--- core/exceptions.hpp
+++ core/exceptions.hpp
@@ -107,6 +107,32 @@ private:
     std::string str_;
 };
 
+class NotSupported
+    : public Exception
+{
+public:
+
+    NotSupported(std::string const& str)
+        : str_(str)
+    {
+        ;
+    }
+
+    virtual ~NotSupported() throw()
+    {
+        ;
+    }
+
+    virtual const char* what() const throw()
+    {
+        return str_.c_str();
+    }
+
+private:
+
+    std::string str_;
+};
+
 }
 
 #endif /* __EXCEPTIONS_HPP */
