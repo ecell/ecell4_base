cf1631a4650b225fa2cfc3fc60643f12211d8921
Kazunari Kaizu <kaizu@tcs2.gsc.riken.jp>
2012-11-28 14:44:39 +0900

    added brief descriptions

--- bd/samples/hardbody/main.cpp
+++ bd/samples/hardbody/main.cpp
@@ -10,36 +10,51 @@
 using namespace ecell4;
 using namespace ecell4::bd;
 
-
+/**
+ * a simple function to dump particle position(s)
+ */
 void print_particle_position(BDWorld const& world, ParticleID const& pid)
 {
     Position3 const pos(world.get_particle(pid).second.position());
     std::cout << std::setprecision(12) << world.t() << " : " << pos << std::endl;
 }
 
+/**
+ * main function
+ */
 int main(int argc, char** argv)
 {
+    /// simulation paramters
     const Real L(1e-6);
     std::string D("5e-12"), radius("5e-9");
     Position3 const edge_lengths(L, L, L);
 
+    /// instantiate RandomNumberGenerator
+    GSLRandomNumberGenerator rng;
+
+    /// instantiate NetworkModel
     boost::shared_ptr<Model> model(new NetworkModel());
+
+    /// create a Species, and set its attributes
     Species sp1("A");
     sp1.set_attribute("D", D);
     sp1.set_attribute("radius", radius);
     (*model).add_species(sp1);
 
+    /// instantiate BDWorld
     boost::shared_ptr<BDWorld> world(new BDWorld(edge_lengths));
+
+    /// create a Particle, and inject it into BDWorld
     SpeciesInfo info((*world).get_species_info(sp1));
     Particle const p1(
         sp1, Position3(0, 0, 0), info.radius, info.D);
     ParticleID const pid1((*world).new_particle(p1));
 
-    GSLRandomNumberGenerator rng;
-
+    /// instatiate BDSimulator
     BDSimulator sim(model, world, rng);
     sim.set_dt(1e-6);
 
+    /// run and log by the millisecond
     const Real upto(1e-3);
     for (unsigned int i(0); i <= 10; ++i)
     {
