78bad32e1d6bfbed4ccc68c78f1e5af2f7cb5d0f
Kazunari Kaizu <kaizu@tcs2.gsc.riken.jp>
2012-11-28 14:17:44 +0900

    trace a particle trajectory

--- bd/samples/hardbody/main.cpp
+++ bd/samples/hardbody/main.cpp
@@ -11,6 +11,12 @@ using namespace ecell4;
 using namespace ecell4::bd;
 
 
+void print_particle_position(BDWorld const& world, ParticleID const& pid)
+{
+    Position3 const pos(world.get_particle(pid).second.position());
+    std::cout << std::setprecision(12) << world.t() << " : " << pos << std::endl;
+}
+
 int main(int argc, char** argv)
 {
     const Real L(1e-6);
@@ -27,10 +33,21 @@ int main(int argc, char** argv)
     SpeciesInfo info((*world).get_species_info(sp1));
     Particle const p1(
         sp1, Position3(0, 0, 0), info.radius, info.D);
-    (*world).new_particle(p1);
+    ParticleID const pid1((*world).new_particle(p1));
 
     GSLRandomNumberGenerator rng;
 
-    BDSimulator target(model, world, rng);
-    target.step();
+    BDSimulator sim(model, world, rng);
+    sim.set_dt(1e-6);
+
+    const Real upto(1e-3);
+    for (unsigned int i(0); i <= 10; ++i)
+    {
+        while (sim.step(1e-3 * i))
+        {
+            ; // do nothing
+        }
+
+        print_particle_position(*world, pid1);
+    }
 }
