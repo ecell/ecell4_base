4846fa2e9bc14d5a3318b550d5f967b39e5b24fc
Kazunari Kaizu <kwaizu@gmail.com>
2012-12-05 14:28:07 +0900

    added a test for CompartmentSpace

--- core/tests/CompartmentSpace_test.cpp
+++ core/tests/CompartmentSpace_test.cpp
@@ -14,7 +14,9 @@ void CompartmentSpace_test_volume_template()
 {
     Real const volume(1e-18);
     Timpl_ target(volume);
-    target.set_volume(2 * target.volume());
+    Real const new_volume(2 * target.volume());
+    target.set_volume(new_volume);
+    BOOST_CHECK_EQUAL(target.volume(), new_volume);
 }
 
 BOOST_AUTO_TEST_CASE(CompartmentSpace_test_volume)
@@ -22,3 +24,29 @@ BOOST_AUTO_TEST_CASE(CompartmentSpace_test_volume)
     CompartmentSpace_test_volume_template<CompartmentSpaceVectorImpl>();
 }
 
+template<typename Timpl_>
+void CompartmentSpace_test_species_template()
+{
+    Real const volume(1e-18);
+    Timpl_ target(volume);
+
+    Species sp1("A"), sp2("B"), sp3("C");
+    target.add_species(sp1);
+    target.add_species(sp2);
+    BOOST_CHECK(target.has_species(sp1));
+    BOOST_CHECK(target.has_species(sp2));
+    BOOST_CHECK(!target.has_species(sp3));
+    target.add_species(sp3);
+    BOOST_CHECK(target.has_species(sp3));
+
+    target.remove_species(sp2);
+    BOOST_CHECK(!target.has_species(sp2));
+    BOOST_CHECK_THROW(target.remove_species(sp2), NotFound);
+    BOOST_CHECK(target.has_species(sp1));
+    BOOST_CHECK(target.has_species(sp3));
+}
+
+BOOST_AUTO_TEST_CASE(CompartmentSpace_test_species)
+{
+    CompartmentSpace_test_species_template<CompartmentSpaceVectorImpl>();
+}
