89d2f2291638bd24a20ca3511069fec1fc2befd8
Kazunari Kaizu <kwaizu@gmail.com>
2012-12-14 14:11:25 +0900

    added test cases for special keys

--- core/tests/get_mapper_mf_test.cpp
+++ core/tests/get_mapper_mf_test.cpp
@@ -5,16 +5,38 @@
 #include <boost/test/test_case_template.hpp>
 #include <boost/type_traits.hpp>
 
-#include "../get_mapper_mf.hpp"
 #include <map>
+
+#include "../get_mapper_mf.hpp"
+#include "../Particle.hpp"
+#include "../Species.hpp"
+
 
 using namespace ecell4;
 
 BOOST_AUTO_TEST_CASE(get_mapper_mf_test_type)
 {
-  typedef utils::get_mapper_mf<std::string, std::string>::type string_map_type;
-  // typedef std::map<std::string, std::string> expected_map_type;
-  typedef boost::unordered_map<std::string, std::string> expected_map_type;
+    typedef utils::get_mapper_mf<std::string, std::string>::type string_map_type;
+    // typedef std::map<std::string, std::string> expected_map_type;
+    typedef boost::unordered_map<std::string, std::string> expected_map_type;
 
-  BOOST_CHECK((boost::is_same<string_map_type, expected_map_type>::value));
+    BOOST_CHECK((boost::is_same<string_map_type, expected_map_type>::value));
+}
+
+BOOST_AUTO_TEST_CASE(get_mapper_mf_test_identifier_key)
+{
+    typedef utils::get_mapper_mf<ParticleID, std::string>::type map_type;
+    // typedef boost::unordered_map<ParticleID, std::string> map_type;
+    map_type target;
+    target[ParticleID()] = "hoge";
+    BOOST_CHECK_EQUAL(target[ParticleID()], "hoge");
+}
+
+BOOST_AUTO_TEST_CASE(get_mapper_mf_test_species_key)
+{
+    typedef utils::get_mapper_mf<Species, std::string>::type map_type;
+    // typedef boost::unordered_map<Species, std::string> map_type;
+    map_type target;
+    target[Species("foo")] = "bar";
+    BOOST_CHECK_EQUAL(target[Species("foo")], "bar");
 }
