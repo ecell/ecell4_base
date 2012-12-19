496140905b90bc16111e41768571a309b484be82
YukiSakamoto <tromso@s211-37.pubnet.titech.ac.jp>
2012-12-19 13:35:47 +0900

    add boost test file for GillespieSimulator

---
+++ gillespie/tests/GillespieSimulator_test.cpp
@@ -0,0 +1,50 @@
+#define BOOST_TEST_MODULE "GillespieSimulator_test"
+#define BOOST_TEST_NO_LIB
+
+#include <boost/test/included/unit_test.hpp>
+
+#include <core/RandomNumberGenerator.hpp>
+#include <core/Model.hpp>
+#include <core/NetworkModel.hpp>
+
+#include "../GillespieWorld.cpp"
+#include "../GillespieSimulator.hpp"
+
+using namespace ecell4;
+using namespace ecell4::gillespie;
+
+BOOST_AUTO_TEST_CASE(GillespieSimulator_test_step)
+{
+	Species sp1("A");
+	Species sp2("B");
+
+	ReactionRule rr1;
+	rr1.set_k(5.0);
+	rr1.add_reactant(sp1);
+	rr1.add_product(sp2);
+
+	boost::shared_ptr<NetworkModel> model(new NetworkModel());
+	model->add_reaction_rule(rr1);
+	Real vol(1.0);
+
+	boost::shared_ptr<GillespieWorld> world(new GillespieWorld(vol));
+	world->add_species(sp1);
+	world->add_species(sp2);
+
+	world->add_molecules(sp1, 10);
+	world->add_molecules(sp2, 10);
+
+	model->add_species(sp1);
+	model->add_species(sp2);
+
+	GSLRandomNumberGenerator rng;
+	rng.seed(time(NULL));
+	GillespieSimulator sim(model, world, rng);
+
+	sim.set_t(0.0);
+	sim.step();
+
+	BOOST_CHECK(0 < sim.t());
+	BOOST_CHECK(world->num_molecules(sp1) == 9);
+
+}
