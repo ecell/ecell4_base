53f57be5254ef9c9c03b75a0eb479d628cecc63a
Kazunari Kaizu <kwaizu@gmail.com>
2013-02-04 17:32:17 +0900

    small fixes

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # PREFIX=/usr/local
-PREFIX=${HOME}/local
-# PREFIX=
+# PREFIX=${HOME}/local
+PREFIX=
 SUBMODS=("bd" "gillespie")
 
 if [ "$PREFIX" == "" ]; then
-   echo "\${PREFIX} is undefined."
-   exit 1
+    echo "\${PREFIX} is undefined."
+    exit 1
 fi
 
 # install ecell4-core
@@ -20,9 +20,11 @@ fi
 for SUBMOD in ${SUBMODS[@]}
 do
     cd $SUBMOD
-    LD_LIBRARY_PATH=${PREFIX}/lib LIBRARY_PATH=${PREFIX}/lib CPLUS_INCLUDE_PATH=${PREFIX}/include ../waf distclean configure --prefix=${PREFIX} build install
+    LD_LIBRARY_PATH=${PREFIX}/lib LIBRARY_PATH=${PREFIX}/lib \
+        CPLUS_INCLUDE_PATH=${PREFIX}/include \
+        ../waf distclean configure --prefix=${PREFIX} build install
     cd ..
     if [ $? != 0 ]; then
-	exit 1
-    fi
+        exit 1
+  fi
 done
