2c90eb268f73ef1992c40fc724fe0e86f519dcc4
Kazunari Kaizu <kwaizu@gmail.com>
2013-02-07 18:46:20 +0900

    avoid a step with no particles

--- egfrd/EGFRDSimulatorWrapper.cpp
+++ egfrd/EGFRDSimulatorWrapper.cpp
@@ -9,12 +9,24 @@ namespace egfrd
 
 void EGFRDSimulatorWrapper::step()
 {
+    if ((*world_).num_particles() == 0)
+    {
+        ; // increment time
+        return;
+    }
+
     (*sim_).step();
     (*world_).set_t((*sim_).t());
 }
 
 bool EGFRDSimulatorWrapper::step(Real const& upto)
 {
+    if ((*world_).num_particles() == 0)
+    {
+        ; // increment time
+        return true; // this should be false
+    }
+
     bool retval((*sim_).step(upto));
     (*world_).set_t((*sim_).t());
     return retval;
