c12e95d20e1751650f59a8216e45ea03ad3d3715
Kazunari Kaizu <kwaizu@gmail.com>
2013-02-18 18:19:26 +0900

    fixed the wrong order of arguments when initializing Species.

--- spatiocyte/samples/hardbody/main.cpp
+++ spatiocyte/samples/hardbody/main.cpp
@@ -23,9 +23,9 @@ int main(int argc, char** argv)
 
     const std::string D("1e-12"), radius("2.5e-9");
 
-    const Real k2(1000), U(0.5);
+    const Real k2(0.1), U(0.5);
     const Real k1(k2 * volume * (1 - U) / (U * U * N));
-    ecell4::Species sp1("A", D, radius), sp2("B", D, radius), sp3("C", D, radius);
+    ecell4::Species sp1("A", radius, D), sp2("B", radius, D), sp3("C", radius, D);
     ReactionRule rr1(create_binding_reaction_rule(sp1, sp2, sp3, k1)),
         rr2(create_unbinding_reaction_rule(sp3, sp1, sp2, k2));
 
@@ -38,8 +38,6 @@ int main(int argc, char** argv)
 
     boost::shared_ptr<SpatiocyteWorld> world(
         new SpatiocyteWorld(edge_lengths, voxel_radius));
-    // std::cout << "volume = " << volume << std::endl;
-    // std::cout << "actual volume = " << world->volume() << std::endl;
 
     world->add_species(sp1);
     world->add_species(sp2);
@@ -48,15 +46,26 @@ int main(int argc, char** argv)
     world->add_molecules(sp2, N / 2);
 
     SpatiocyteSimulator sim(model, world);
+    // sim.initialize();
 
-    for (unsigned int i(0); i < 1; ++i)
+    Real next_time(0.0), dt(0.02);
+    std::cout << sim.t()
+              << "\t" << world->num_molecules(sp1)
+              << "\t" << world->num_molecules(sp2)
+              << "\t" << world->num_molecules(sp3)
+              << std::endl;
+    for (unsigned int i(0); i < 1000; ++i)
     {
-        while (sim.step(1e-3)) {}
+        next_time += dt;
+        while (sim.step(next_time)) {}
 
-        break;
+        std::cout << sim.t()
+                  << "\t" << world->num_molecules(sp1)
+                  << "\t" << world->num_molecules(sp2)
+                  << "\t" << world->num_molecules(sp3)
+                  << std::endl;
     }
 
-    std::cout << sim.t() << "\t" << world->num_molecules(sp1) << "\t"
-              << world->num_molecules(sp2) << "\t"
-              << world->num_molecules(sp3) << "\t" << std::endl;
+    std::cout << "volume = " << volume << std::endl;
+    std::cout << "actual volume = " << world->volume() << std::endl;
 }
