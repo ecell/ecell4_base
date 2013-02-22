cdc6d35ddcca611593490554e0fdc3e976d9a767
Kozo Nishida <knishida@riken.jp>
2013-02-23 03:19:40 +0900

    no need to install

--- bd/install.sh
+++ bd/install.sh
@@ -1,1 +1,1 @@
-sudo CPLUS_INCLUDE_PATH=/usr/local/hdf5/include LD_LIBRARY_PATH=/usr/local/hdf5/lib ../waf configure build install
+CPLUS_INCLUDE_PATH=/usr/local/hdf5/include LD_LIBRARY_PATH=/usr/local/hdf5/lib ../waf configure build
