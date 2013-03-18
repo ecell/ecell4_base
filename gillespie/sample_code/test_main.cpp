1211ed0f10bae858f695e117035a00143945cc09
YukiSakamoto <tromso@YukiSakamoto-Air.local>
2013-03-18 09:02:09 +0900

    merged H5Save.hpp

--- gillespie/sample_code/test_main.cpp
+++ gillespie/sample_code/test_main.cpp
@@ -1,13 +1,23 @@
 #include <string>
+#include <sstream>
 
 #include <ecell4/core/types.hpp>
 #include <ecell4/core/Species.hpp>
 #include <ecell4/core/NetworkModel.hpp>
 
+#include <hdf5.h>
+#include <H5Cpp.h>
+
 #include "../GillespieSimulator.hpp"
+
+#include "../../H5Save.hpp"
 
 using namespace ecell4;
 using namespace ecell4::gillespie;
+
+
+using namespace H5;
+
 
 int main(int argc, char **argv)
 {
@@ -40,10 +50,16 @@ int main(int argc, char **argv)
 	model->add_species(sp2);
 	
 	GillespieSimulator sim(model, world, rng);
+	ecell4_hdf5_manager<GillespieWorld, int>	hdf("GillespieWorld_test.hdf5", model, world, "CompartmentSpace");
 
+	sim.save_hdf5_init("test_main.h5");
 	for(int i = 0; i < 10; i++) {
 		sim.step();
+
+		std::ostringstream ost;
+		ost << sim.t();
 		printf("t = %f A: %llu, B: %llu \n", sim.t(), world->num_molecules(sp1), world->num_molecules(sp2));
+		hdf.save();
 	}
 
 }
