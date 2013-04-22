a5627cdbabe5cd2771db404e029d7025c97b7a92
Yuki Sakamoto <yukisakamoto06@gmail.com>
2013-04-23 00:42:48 +0900

    add ReactionRule's factorial methods

--- cython_wrapper/PyReactionRule.pyx
+++ cython_wrapper/PyReactionRule.pyx
@@ -34,3 +34,36 @@ cdef class PyReactionRule:
     def add_product(self, PySpecies sp):
         self.thisptr.add_product( deref(sp.thisptr) )
 
+def create_unimolecular_reaction_rule(
+        PySpecies reactant1, PySpecies product1, Real k):
+    rr = PyReactionRule()
+    rr.add_reactant(reactant1)
+    rr.add_product(product1)
+    rr.set_k(k)
+    return rr
+
+def create_binding_reaction_rule(
+        PySpecies reactant1, PySpecies reactant2, PySpecies product1, Real k):
+    rr = PyReactionRule()
+    rr.add_reactant(reactant1)
+    rr.add_reactant(reactant2)
+    rr.add_product(product1)
+    rr.set_k(k)
+    return rr
+
+def create_unbinding_reaction_rue(
+        PySpecies reactant1, PySpecies product1, PySpecies product2, Real k):
+    rr = PyReactionRule()
+    rr.add_reactant(reactant1)
+    rr.add_product(product1)
+    rr.add_product(product2)
+    rr.set_k(k)
+    return rr
+
+def create_repulsive_reaction_rule(
+        PySpecies reactant1, PySpecies reactant2):
+    rr = PyReactionRule()
+    rr.set_k(0.0)
+    rr.add_reactant(reactant1)
+    rr.add_product(reactant2)
+    return rr
