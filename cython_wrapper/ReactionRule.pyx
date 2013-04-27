c31bb4e88b2de3d8da2a391e353ddaa3beb67116
Yuki Sakamoto <yukisakamoto06@gmail.com>
2013-04-27 22:40:14 +0900

    add multiset

--- cython_wrapper/ReactionRule.pyx
+++ cython_wrapper/ReactionRule.pyx
@@ -1,5 +1,6 @@
 
-from cython.operator cimport dereference as deref
+from cython.operator cimport dereference as deref, preincrement as inc
+from cython cimport address 
 from libcpp.vector cimport vector
 from libcpp cimport set
 from libcpp cimport bool
@@ -16,6 +17,24 @@ cdef class ReactionRule:
 
     def set_k(self, Real k):
         self.thisptr.set_k(k)
+
+    def reactants(self):
+        cdef multiset[Cpp_Species] rawObj_Reactants = self.thisptr.reactants()
+        pyObj_Reactants = []
+        cdef multiset[Cpp_Species].iterator it = rawObj_Reactants.begin() 
+        while it != rawObj_Reactants.end():
+            pyObj_Reactants.append( to_PyObject_Species( <Cpp_Species*>address(deref(it)) ) )
+            inc(it)
+        return pyObj_Reactants
+
+    def products(self):
+        cdef multiset[Cpp_Species] rawObj_Products = self.thisptr.products()
+        pyObj_Products = []
+        cdef multiset[Cpp_Species].iterator it = rawObj_Products.begin() 
+        while it != rawObj_Products.end():
+            pyObj_Products.append( to_PyObject_Species( <Cpp_Species*>address(deref(it)) ) )
+            inc(it)
+        return pyObj_Products
 
     def add_reactant(self, Species sp):
         self.thisptr.add_reactant( deref(sp.thisptr) )
