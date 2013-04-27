9e4fdc045c95afd6a70282899b21e702259c8202
Yuki Sakamoto <yukisakamoto06@gmail.com>
2013-04-27 23:10:01 +0900

    small fix

--- cython_wrapper/setup.py
+++ cython_wrapper/setup.py
@@ -2,18 +2,16 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-ext_modules = [
-        Extension("Ecell4",
-            ["Ecell4.pyx"],
-            language = 'c++',
-            libraries = ['ecell4-core'],
-            )]
-
 setup(
         name = "Ecell4",
         cmdclass = {'build_ext' : build_ext},
-        ext_modules = ext_modules
-        )
+        ext_modules = [
+            Extension("Ecell4",
+                ["Ecell4.pyx"],
+                language = 'c++',
+                libraries = ['ecell4-core'],
+                )]
+            )
 
 #Gillespie
 setup(
