b5339dd332b9b830b5f9c1237292051d8e52a0ff
Kazunari Kaizu <kwaizu@gmail.com>
2013-05-16 16:27:32 +0900

    divided into pyx and pxd files

---
+++ gillespie/ecell4/gillespie.pxd
@@ -0,0 +1,49 @@
+from libcpp.string cimport string
+from libcpp cimport bool
+
+from ecell4.types cimport *
+from ecell4.shared_ptr cimport shared_ptr
+from ecell4.core cimport *
+
+
+## Cpp_GillespieWorld
+#  ecell4::gillespie::GillespieWorld
+cdef extern from "ecell4/gillespie/GillespieWorld.hpp" namespace "ecell4::gillespie":
+    cdef cppclass Cpp_GillespieWorld "ecell4::gillespie::GillespieWorld":
+        Cpp_GillespieWorld(Real, shared_ptr[Cpp_GSLRandomNumberGenerator]) except +
+        void set_t(Real)
+        Real t()
+        Real volume()
+        Integer num_species()
+        bool has_species(Cpp_Species &)
+        Integer num_molecules(Cpp_Species &)
+        void add_species(Cpp_Species &)
+        void remove_species(Cpp_Species &)
+        void add_molecules(Cpp_Species &sp, Integer &num)
+        void remove_molecules(Cpp_Species &sp, Integer &num)
+        void save(string)
+
+## GillespieWorld
+#  a python wrapper for Cpp_GillespieWorld
+cdef class GillespieWorld:
+    cdef shared_ptr[Cpp_GillespieWorld]* thisptr
+
+## Cpp_GillespieSimulator
+#  ecell4::gillespie::GillespieSimulator
+cdef extern from "ecell4/gillespie/GillespieSimulator.hpp" namespace "ecell4::gillespie":
+    cdef cppclass Cpp_GillespieSimulator "ecell4::gillespie::GillespieSimulator":
+        Cpp_GillespieSimulator(
+            shared_ptr[Cpp_NetworkModel], shared_ptr[Cpp_GillespieWorld]) except +
+        Integer num_steps()
+        void step()
+        bool step(Real)
+        Real t()
+        void set_t(Real)
+        Real dt()
+        void initialize()
+        Cpp_GSLRandomNumberGenerator& rng()
+
+## GillespieSimulator
+#  a python wrapper for Cpp_GillespieSimulator
+cdef class GillespieSimulator:
+    cdef Cpp_GillespieSimulator* thisptr
--- gillespie/ecell4/gillespie.pyx
+++ gillespie/ecell4/gillespie.pyx
@@ -1,33 +1,14 @@
 from cython.operator cimport dereference as deref
 from libcpp.string cimport string
-from libcpp cimport bool
 
 from ecell4.types cimport *
 from ecell4.shared_ptr cimport shared_ptr
 from ecell4.core cimport *
 
 
-## Cpp_GillespieWorld
-#  ecell4::gillespie::GillespieWorld
-cdef extern from "ecell4/gillespie/GillespieWorld.hpp" namespace "ecell4::gillespie":
-    cdef cppclass Cpp_GillespieWorld "ecell4::gillespie::GillespieWorld":
-        Cpp_GillespieWorld(Real, shared_ptr[Cpp_GSLRandomNumberGenerator]) except +
-        void set_t(Real)
-        Real t()
-        Real volume()
-        Integer num_species()
-        bool has_species(Cpp_Species &)
-        Integer num_molecules(Cpp_Species &)
-        void add_species(Cpp_Species &)
-        void remove_species(Cpp_Species &)
-        void add_molecules(Cpp_Species &sp, Integer &num)
-        void remove_molecules(Cpp_Species &sp, Integer &num)
-        void save(string)
-
 ## GillespieWorld
 #  a python wrapper for Cpp_GillespieWorld
 cdef class GillespieWorld:
-    cdef shared_ptr[Cpp_GillespieWorld]* thisptr
 
     def __cinit__(self, Real vol, GSLRandomNumberGenerator rng):
         # XXX: GSLRandomNumberGenerator -> RandomNumberGenerator
@@ -73,25 +54,9 @@ cdef class GillespieWorld:
     def save(self, string filename):
         self.thisptr.get().save(filename)
 
-## Cpp_GillespieSimulator
-#  ecell4::gillespie::GillespieSimulator
-cdef extern from "ecell4/gillespie/GillespieSimulator.hpp" namespace "ecell4::gillespie":
-    cdef cppclass Cpp_GillespieSimulator "ecell4::gillespie::GillespieSimulator":
-        Cpp_GillespieSimulator(
-            shared_ptr[Cpp_NetworkModel], shared_ptr[Cpp_GillespieWorld]) except +
-        Integer num_steps()
-        void step()
-        bool step(Real)
-        Real t()
-        void set_t(Real)
-        Real dt()
-        void initialize()
-        Cpp_GSLRandomNumberGenerator& rng()
-
 ## GillespieSimulator
 #  a python wrapper for Cpp_GillespieSimulator
 cdef class GillespieSimulator:
-    cdef Cpp_GillespieSimulator* thisptr
 
     def __cinit__(self, NetworkModel m, GillespieWorld w):
         self.thisptr = new Cpp_GillespieSimulator(
