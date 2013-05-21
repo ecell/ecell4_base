0bd19f0e2f6236549740f06dff2d68f8dfd33ef1
Kazunari Kaizu <kwaizu@gmail.com>
2013-05-21 18:38:36 +0900

    updated a sample

--- reaction_reader/samples/mapk/mapk.py
+++ reaction_reader/samples/mapk/mapk.py
@@ -2,11 +2,16 @@ from ecell4.reaction_reader.decorators import reaction_rules
 
 
 @reaction_rules
-def reactions(kf, kr):
-    mapk(phos=YT) + kk(bs=on) > mapk(phos=YT[1]).kk(bs=on[1]) | kf
-    mapk(phos=YT[1]).kk(bs=on[1]) > mapk(phos=pYT) + kk(bs=on) | kr
+def reactions(kon, koff, kcat):
+    mapk(phos=YT) + kk(bs=on) > mapk(phos=YT[1]).kk(bs=on[1]) | kon
+    mapk(phos=YT[1]).kk(bs=on[1]) > mapk(phos=YT) + kk(bs=on) | koff
+    mapk(phos=YT[1]).kk(bs=on[1]) > mapk(phos=pYT) + kk(bs=on) | kcat
+
+    mapk(phos=pYT) + pp(bs=on) > mapk(phos=pYT[1]).pp(bs=on[1]) | kon
+    mapk(phos=pYT[1]).pp(bs=on[1]) > mapk(phos=pYT) + pp(bs=on) | koff
+    mapk(phos=pYT[1]).pp(bs=on[1]) > mapk(phos=YT) + pp(bs=on) | kcat
 
 
 if __name__ == "__main__":
-    rules = reactions(1, 2)
+    rules = reactions(1, 2, 3)
     print rules
