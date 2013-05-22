932245432325a93839461e3bcccce174d9a3fedb
Kazunari Kaizu <kwaizu@gmail.com>
2013-05-23 05:19:07 +0900

    modified the sample named 'simple'

--- reaction_reader/samples/simple/simple.py
+++ reaction_reader/samples/simple/simple.py
@@ -3,28 +3,32 @@ from ecell4.reaction_reader.decorator import reaction_rules
 
 @reaction_rules
 def reactions(kon, koff, kcat):
-    (A + B == C | (kon, koff)
-        > D | kcat)
+    (K + KK == K_KK | (kon, koff)
+        > Kp + KK | kcat
+        == Kp_KK | (kon, koff)
+        > Kpp + KK | kcat)
 
-    D > C | kcat
+    (Kpp + PP == Kpp_PP | (kon, koff)
+        > Kp + PP | kcat
+        == Kp_PP | (kon, koff)
+        > K + PP | kcat)
 
 
 if __name__ == "__main__":
     rules = reactions(1, 2, 3)
 
-    # species = []
-    # for rr in rules:
-    #     species.extend(rr.reactants())
-    #     species.extend(rr.products())
-
-    # print [sp.name() for sp in species]
-    # species = sorted(set(species))
-    # print [sp.name() for sp in species]
-
+    species_list = []
     for i, rr in enumerate(rules):
         print i + 1, rr
-        # reactants, products = rr.reactants(), rr.products()
-        # print (i + 1,
-        #     [sp.name() for sp in reactants],
-        #     [sp.name() for sp in products],
-        #     rr.k())
+
+    #     reactants, products = rr.reactants(), rr.products()
+    #     species_list.extend(reactants)
+    #     species_list.extend(products)
+
+    #     print i + 1, (
+    #         [sp.name() for sp in reactants],
+    #         [sp.name() for sp in products],
+    #         rr.k())
+
+    # species_list = sorted(set(species_list))
+    # print tuple(sp.name() for sp in species_list)
