3a18fa0af223e14da3f211c632bc885bcf0e477d
Yuki Sakamoto <yukisakamoto06@gmail.com>
2013-05-26 10:18:42 +0900

    add new sample-set for ode_python

---
+++ ode_python/samples/equilibrium/main.py
@@ -0,0 +1,55 @@
+from ecell4.core import *
+from ecell4.ode import *
+
+def run():
+    volume = float(1e-18)
+    N = float(60)
+    ka, U = float(0.1), float(0.5)
+
+    sp1, sp2, sp3 = Species("A"), Species("B"), Species("C")
+    rr1 = ReactionRule()
+    rr1.set_k(ka)
+    rr1.add_reactant(sp1)
+    rr1.add_product(sp2)
+    rr1.add_product(sp3)
+
+    kd = (float)(ka * volume * (1 - U) / (U * U * N))
+    rr2 = ReactionRule()
+    rr2.set_k(kd)
+    rr2.add_reactant(sp2)
+    rr2.add_reactant(sp3)
+    rr2.add_product(sp1)
+    
+    model = NetworkModel()
+    model.add_species(sp1)
+    model.add_species(sp2)
+    model.add_species(sp3)
+    model.add_reaction_rule(rr1)
+    model.add_reaction_rule(rr2)
+
+    world = ODEWorld(volume)
+    world.add_species(sp1)
+    world.add_species(sp2)
+    world.add_species(sp3)
+    world.add_molecules(sp1, N) 
+
+    next_time = float(0.0)
+    dt = float(0.01)
+    target = ODESimulator(model, world)
+
+    print "t = %g\t A = %g\t B = %g\t C = %g" % (
+        target.t(), world.num_molecules(sp1), 
+        world.num_molecules(sp2),
+        world.num_molecules(sp3))
+    for i in range(1000):
+        next_time += dt
+        target.step(next_time)
+        print "t = %g\t A = %g\t B = %g\t C = %g" % (
+                target.t(), 
+                world.num_molecules(sp1), 
+                world.num_molecules(sp2),
+                world.num_molecules(sp3) )
+
+
+if __name__ == "__main__":
+    run()
