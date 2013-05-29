f3635fe89680bc57d4beb93ac28a385ea8fb4b60
Kazunari Kaizu <kwaizu@gmail.com>
2013-05-29 13:38:21 +0900

    wrapped egfrd

---
+++ egfrd_python/ecell4/egfrd.pxd
@@ -0,0 +1,67 @@
+from libcpp.string cimport string
+from libcpp cimport bool
+from libcpp.vector cimport vector
+
+from ecell4.types cimport *
+from ecell4.shared_ptr cimport shared_ptr
+from ecell4.core cimport *
+
+
+## Cpp_EGFRDWorld
+#  ecell4::egfrd::EGFRDWorld
+cdef extern from "ecell4/egfrd/EGFRDWorld.hpp" namespace "ecell4::egfrd":
+    cdef cppclass Cpp_EGFRDWorld "ecell4::egfrd::EGFRDWorld":
+        Cpp_EGFRDWorld(
+            Real& world_size, Integer matrix_size,
+            shared_ptr[Cpp_GSLRandomNumberGenerator] rng) except +
+        Cpp_ParticleID new_particle(Cpp_Particle& p)
+        void set_t(Real t)
+        Real t()
+        Cpp_Position3 edge_lengths()
+        Integer num_particles()
+        Integer num_particles(Cpp_Species& sp)
+        vector[pair[Cpp_ParticleID, Cpp_Particle]] list_particles()
+        vector[pair[Cpp_ParticleID, Cpp_Particle]] list_particles(Cpp_Species& sp)
+        bool has_particle(Cpp_ParticleID& pid)
+        bool update_particle(Cpp_ParticleID& pid, Cpp_Particle& p)
+        pair[Cpp_ParticleID, Cpp_Particle] get_particle(Cpp_ParticleID& pid)
+        void remove_particle(Cpp_ParticleID& pid)
+        vector[pair[pair[Cpp_ParticleID, Cpp_Particle], Real]] list_particles_within_radius(Cpp_Position3& pos, Real& radius)
+        vector[pair[pair[Cpp_ParticleID, Cpp_Particle], Real]] list_particles_within_radius(Cpp_Position3& pos, Real& radius, Cpp_ParticleID& ignore)
+        vector[pair[pair[Cpp_ParticleID, Cpp_Particle], Real]] list_particles_within_radius(Cpp_Position3& pos, Real& radius, Cpp_ParticleID& ignore1, Cpp_ParticleID& ignore2)
+        Cpp_Position3 periodic_transpose(Cpp_Position3& pos1, Cpp_Position3& pos2)
+        Cpp_Position3 apply_boundary(Cpp_Position3& pos)
+        Real distance_sq(Cpp_Position3& pos1, Cpp_Position3& pos2)
+        Real distance(Cpp_Position3& pos1, Cpp_Position3& pos2)
+        Real volume()
+        bool has_species(Cpp_Species& sp)
+        Integer num_molecules(Cpp_Species& sp)
+        void add_molecules(Cpp_Species& sp, Integer num)
+        shared_ptr[GSLRandomNumberGenerator] rng()
+        void save(string filename)
+
+## EGFRDWorld
+#  a python wrapper for Cpp_EGFRDWorld
+cdef class EGFRDWorld:
+    cdef shared_ptr[Cpp_EGFRDWorld]* thisptr
+
+## Cpp_EGFRDSimulator
+#  ecell4::egfrd::EGFRDSimulator
+cdef extern from "ecell4/egfrd/EGFRDSimulatorWrapper.hpp" namespace "ecell4::egfrd":
+    cdef cppclass Cpp_EGFRDSimulatorWrapper "ecell4::egfrd::EGFRDSimulatorWrapper":
+        Cpp_EGFRDSimulatorWrapper(
+            shared_ptr[Cpp_NetworkModel], shared_ptr[Cpp_EGFRDWorld],
+            Integer dissociation_retry_moves) except +
+        Cpp_EGFRDSimulatorWrapper(
+            shared_ptr[Cpp_NetworkModel], shared_ptr[Cpp_EGFRDWorld]) except +
+        Integer num_steps()
+        void step()
+        bool step(Real& upto)
+        Real t()
+        Real dt()
+        void initialize()
+
+## EGFRDSimulator
+#  a python wrapper for Cpp_EGFRDSimulatorWrapper
+cdef class EGFRDSimulatorWrapper:
+    cdef Cpp_EGFRDSimulatorWrapper* thisptr
---
+++ egfrd_python/ecell4/egfrd.pyx
@@ -0,0 +1,173 @@
+from cython.operator cimport dereference as deref, preincrement as inc
+from cython cimport address
+from libcpp.string cimport string
+from libcpp.vector cimport vector
+
+from ecell4.types cimport *
+from ecell4.shared_ptr cimport shared_ptr
+from ecell4.core cimport *
+
+
+## EGFRDWorld
+#  a python wrapper for Cpp_EGFRDWorld
+cdef class EGFRDWorld:
+
+    def __cinit__(
+        self, Real world_size, Integer matrix_size,
+        GSLRandomNumberGenerator rng):
+        self.thisptr = new shared_ptr[Cpp_EGFRDWorld](
+            new Cpp_EGFRDWorld(world_size, matrix_size, deref(rng.thisptr)))
+
+    def __dealloc__(self):
+        # XXX: Here, we release shared pointer,
+        #      and if reference count to the EGFRDWorld object,
+        #      it will be released automatically.
+        del self.thisptr
+
+    def new_particle(self, Particle p):
+        cdef Cpp_ParticleID pid = self.thisptr.get().new_particle(deref(p.thisptr))
+        return ParticleID_from_Cpp_ParticleID(address(pid))
+
+    def set_t(self, Real t):
+        self.thisptr.get().set_t(t)
+
+    def t(self):
+        return self.thisptr.get().t()
+
+    def edge_lengths(self):
+        cdef Cpp_Position3 lengths = self.thisptr.get().edge_lengths()
+        return Position3_from_Cpp_Position3(address(lengths))
+
+    def num_particles(self, Species sp):
+        if sp is None:
+            return self.thisptr.get().num_particles()
+        else:
+            return self.thisptr.get().num_particles(deref(sp.thisptr))
+
+    def list_particles(self, Species sp):
+        cdef vector[pair[Cpp_ParticleID, Cpp_Particle]] particles
+        if sp is None:
+            particles = self.thisptr.get().list_particles(deref(sp.thisptr))
+        else:
+            particles = self.thisptr.get().list_particles(deref(sp.thisptr))
+
+        retval = []
+        cdef vector[pair[Cpp_ParticleID, Cpp_Particle]].iterator \
+            it = particles.begin()
+        while it != particles.end():
+            retval.append(
+                (ParticleID_from_Cpp_ParticleID(
+                     <Cpp_ParticleID*>(address(deref(it).first))),
+                 Particle_from_Cpp_Particle(
+                     <Cpp_Particle*>(address(deref(it).second)))))
+            inc(it)
+        return retval
+
+    def has_particle(self, ParticleID pid):
+        return self.thisptr.get().has_particle(deref(pid.thisptr))
+
+    def update_particle(self, ParticleID pid, Particle p):
+        return self.thisptr.get().update_particle(deref(pid.thisptr), deref(p.thisptr))
+
+    def get_particle(self, ParticleID pid):
+        cdef pair[Cpp_ParticleID, Cpp_Particle] \
+            pid_particle_pair = self.thisptr.get().get_particle(deref(pid.thisptr))
+        return (ParticleID_from_Cpp_ParticleID(address(pid_particle_pair.first)),
+                Particle_from_Cpp_Particle(address(pid_particle_pair.second)))
+
+    def remove_particle(self, ParticleID pid):
+        self.thisptr.get().remove_particle(deref(pid.thisptr))
+
+    def list_particles_within_radius(
+        self, Position3 pos, Real radius,
+        ParticleID ignore1, ParticleID ignore2):
+        cdef vector[pair[pair[Cpp_ParticleID, Cpp_Particle], Real]] particles
+        if ignore1 is None and ignore2 is None:
+            particles = self.thisptr.get().list_particles_within_radius(
+                deref(pos.thisptr), radius)
+        elif ignore2 is None:
+            particles = self.thisptr.get().list_particles_within_radius(
+                deref(pos.thisptr), radius, deref(ignore1.thisptr))
+        else:
+            particles = self.thisptr.get().list_particles_within_radius(
+                deref(pos.thisptr), radius,
+                deref(ignore1.thisptr), deref(ignore2.thisptr))
+
+        retval = []
+        cdef vector[pair[pair[Cpp_ParticleID, Cpp_Particle], Real]].iterator \
+            it = particles.begin()
+        while it != particles.end():
+            retval.append(
+                ((ParticleID_from_Cpp_ParticleID(
+                      <Cpp_ParticleID*>(address(deref(it).first.first))),
+                  Particle_from_Cpp_Particle(
+                      <Cpp_Particle*>(address(deref(it).first.second)))),
+                 deref(it).second))
+            inc(it)
+        return retval
+
+    def periodic_transpose(self, Position3 pos1, Position3 pos2):
+        cdef Cpp_Position3 newpos = self.thisptr.get().periodic_transpose(
+            deref(pos1.thisptr), deref(pos2.thisptr))
+        return Position3_from_Cpp_Position3(address(newpos))
+
+    def apply_boundary(self, Position3 pos):
+        cdef Cpp_Position3 newpos = self.thisptr.get().apply_boundary(deref(pos.thisptr))
+        return Position3_from_Cpp_Position3(address(newpos))
+
+    def distance_sq(self, Position3 pos1, Position3 pos2):
+        return self.thisptr.get().distance_sq(deref(pos1.thisptr), deref(pos2.thisptr))
+
+    def distance(self, Position3 pos1, Position3 pos2):
+        return self.thisptr.get().distance(deref(pos1.thisptr), deref(pos2.thisptr))
+
+    def volume(self):
+        return self.thisptr.get().volume()
+
+    def has_species(self, Species sp):
+        return self.thisptr.get().has_species(deref(sp.thisptr))
+
+    def num_molecules(self, Species sp):
+        return self.thisptr.get().num_molecules(deref(sp.thisptr))
+
+    # def add_species(self, Species sp):
+    #     self.thisptr.get().add_species(deref(sp.thisptr))
+
+    def add_molecules(self, Species sp, Integer num):
+        self.thisptr.get().add_molecules(deref(sp.thisptr), num)
+
+    def save(self, string filename):
+        self.thisptr.get().save(filename)
+
+## EGFRDSimulatorWrapper
+#  a python wrapper for Cpp_EGFRDSimulatorWrapper
+cdef class EGFRDSimulatorWrapper:
+
+    def __cinit__(self, NetworkModel m, EGFRDWorld w, Integer dissociation_retry_moves):
+        if dissociation_retry_moves is None:
+            self.thisptr = new Cpp_EGFRDSimulatorWrapper(
+                deref(m.thisptr), deref(w.thisptr))
+        else:
+            self.thisptr = new Cpp_EGFRDSimulatorWrapper(
+                deref(m.thisptr), deref(w.thisptr), dissociation_retry_moves)
+
+    def __dealloc__(self):
+        del self.thisptr
+
+    def num_steps(self):
+        return self.thisptr.num_steps()
+
+    def step(self, Real upto):
+        if upto is None:
+            self.thisptr.step()
+        else:
+            return self.thisptr.step(upto)
+
+    def t(self):
+        return self.thisptr.t()
+
+    def dt(self):
+        return self.thisptr.dt()
+
+    def initialize(self):
+        self.thisptr.initialize()
