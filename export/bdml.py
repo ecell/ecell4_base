31c5b181fdddc8eb7c0f04b774369c322d4ce6db
Kozo Nishida <knishida@riken.jp>
2013-07-30 20:53:31 +0900

    add skeleton for BDML exporter

---
+++ export/bdml.py
@@ -0,0 +1,19 @@
+from ecell4.export import Exporter
+import xml.etree.ElementTree as ET
+
+class BdmlExporter(Exporter):
+    def export(self):
+        pass
+        
+        
+
+
+
+
+
+
+
+
+
+
+
