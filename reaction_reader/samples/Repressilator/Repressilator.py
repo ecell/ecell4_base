73e3dd1c37f24918cee419a727b8507a7bd63a98
Kozo Nishida <knishida@riken.jp>
2013-08-09 18:47:21 +0900

    add kinetic rate

--- reaction_reader/samples/Repressilator/Repressilator.py
+++ reaction_reader/samples/Repressilator/Repressilator.py
@@ -1,44 +1,45 @@
 from ecell4.reaction_reader.decorator2 import species_attributes, reaction_rules
 from ecell4.reaction_reader.species import generate_reactions
+import math
 
 @species_attributes
 def attributegen():
-    Null()                                       | 1
-    gTetR(lac!1,lac!2).pLacI(tet!1).pLacI(tet!2) | 1
-    gCI(tet!1,tet!2).pTetR(cI!1).pTetR(cI!2)     | 1
-    gLacI(cI!1,cI!2).pCI(lac!1).pCI(lac!2)       | 1
-    mTetR()             |          3163
-    mCI()               |          6819
-    mLacI()             |          129
+    Null                                       | 1
+    gTetR(lac^1,lac^2).pLacI(tet^1).pLacI(tet^2) | 1
+    gCI(tet^1,tet^2).pTetR(cI^1).pTetR(cI^2)     | 1
+    gLacI(cI^1,cI^2).pCI(lac^1).pCI(lac^2)       | 1
+    mTetR             |          3163
+    mCI               |          6819
+    mLacI             |          129
     pTetR(cI)           |          183453
     pCI(lac)            |          2006198
     pLacI(tet)          |          165670
 
 @reaction_rules
-def rulegen():
+def rulegen( Na ,V  ,c0 ,c1 ,c2, c3 ,c4 ,c5 ,c6 ,c7 ,tF ,rF ,pF ):
     gTetR(lac,lac) + pLacI(tet) == gTetR(lac^1,lac).pLacI(tet^1)     |  (c0/Na/V*tF/pF, c1*tF)
     gTetR(lac^_,lac) + pLacI(tet) == gTetR(lac^_,lac^1).pLacI(tet^1) |  (c0/Na/V*tF/pF, c2*tF)
-    gTetR(lac,lac) > gTetR(lac,lac) + mTetR()                        |  c3*rF
-    gTetR(lac^_) > gTetR(lac^_) + mTetR()                            |  c4*rF
-    mTetR() > mTetR() + pTetR(cI)                                    |  c5/rF*pF
-    mTetR() + Null() > Null()                                        |  c6
-    pTetR(cI) + Null() > Null()                                      |  c7
+    gTetR(lac,lac) > gTetR(lac,lac) + mTetR                        |  c3*rF
+    gTetR(lac^_) > gTetR(lac^_) + mTetR                            |  c4*rF
+    mTetR > mTetR + pTetR(cI)                                    |  c5/rF*pF
+    mTetR + Null > Null                                        |  c6
+    pTetR(cI) + Null > Null                                      |  c7
     #
     gCI(tet,tet) + pTetR(cI) == gCI(tet^1,tet).pTetR(cI^1)           |  (c0/Na/V*tF/pF, c1*tF)
     gCI(tet^_,tet) + pTetR(cI) == gCI(tet^_,tet^1).pTetR(cI^1)       |  (c0/Na/V*tF/pF, c2*tF)
-    gCI(tet,tet) > gCI(tet,tet) + mCI()                              |  c3*rF
-    gCI(tet^_) > gCI(tet^_) + mCI()                                  |  c4*rF
-    mCI() > mCI() + pCI(lac)                                         |  c5/rF*pF
-    mCI() + Null() > Null()                                          |  c6
-    pCI(lac) + Null() > Null()                                       |  c7
+    gCI(tet,tet) > gCI(tet,tet) + mCI                              |  c3*rF
+    gCI(tet^_) > gCI(tet^_) + mCI                                  |  c4*rF
+    mCI > mCI + pCI(lac)                                         |  c5/rF*pF
+    mCI + Null > Null                                          |  c6
+    pCI(lac) + Null > Null                                       |  c7
     #
     gLacI(cI,cI) + pCI(lac) == gLacI(cI^1,cI).pCI(lac^1)             |  (c0/Na/V*tF/pF, c1*tF)
     gLacI(cI^_,cI) + pCI(lac) == gLacI(cI^_,cI^1).pCI(lac^1)         |  (c0/Na/V*tF/pF, c2*tF)
-    gLacI(cI,cI) > gLacI(cI,cI) + mLacI()                            |  c3*rF
-    gLacI(cI^_) > gLacI(cI^_) + mLacI()                              |  c4*rF
-    mLacI() > mLacI() + pLacI(tet)                                   |  c5/rF*pF
-    mLacI() + Null() > Null()                                        |  c6
-    pLacI(tet) + Null() > Null()                                     |  c7
+    gLacI(cI,cI) > gLacI(cI,cI) + mLacI                            |  c3*rF
+    gLacI(cI^_) > gLacI(cI^_) + mLacI                              |  c4*rF
+    mLacI > mLacI + pLacI(tet)                                   |  c5/rF*pF
+    mLacI + Null > Null                                        |  c6
+    pLacI(tet) + Null > Null                                     |  c7
 
 if __name__ == "__main__":
     newseeds = []
@@ -47,7 +48,8 @@ if __name__ == "__main__":
         newseeds.append(sp)
     print ''
 
-    rules = rulegen()
+    rules = rulegen(6.022e23 ,1.4e-15, 1e9, 224, 9, 0.5, 5e-4, 0.167, math.log(2)/120, math.log(2)/600, 1e-4, 1000, 1000)    
+
     for i, rr in enumerate(rules):
         print i, rr
     print ''
