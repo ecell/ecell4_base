13fa4cbb0b4c0fb433d98055f7d0222aa5cbff77
Kozo Nishida <knishida@riken.jp>
2013-08-22 20:37:36 +0900

    hardcode species value

--- reaction_reader/samples/SHP2_base_model/convert.py
+++ reaction_reader/samples/SHP2_base_model/convert.py
@@ -15,7 +15,7 @@ with open("too_old_export.bngl", "w") as fd:
 # convert2bng_reaction_rules(fd, rulegen() )
 
 with open("new_export.bngl", "w") as f:
-    bng_mng = Convert2BNGManager(attributegen(), rulegen())
+    bng_mng = Convert2BNGManager(attributegen(), rulegen(1000, 10, 500, 1, 1, 1, 1, 0.1, 1, 10, 1, 1000, 100, 1000, 1000, 100, 100, 100, 1000, 100, 100, 1000, 0.025, 0.05))
     bng_mng.write_section_molecule_types(f)
     bng_mng.write_section_seed_species(f)
     bng_mng.write_section_reaction_rules(f)
