f101c327f2955db33f98fc43c7be4e7a612f802c
Kazunari Kaizu <kwaizu@gmail.com>
2013-09-09 11:59:44 +0900

    just formatted

--- reaction_reader/ecell4/reaction_reader/network.py
+++ reaction_reader/ecell4/reaction_reader/network.py
@@ -1,24 +1,33 @@
 import species
 import ecell4.core as core
 
-def generateNetworkModel(seeds, rules):
+
+def generate_NetworkModel(seeds, rules):
     model = core.NetworkModel()
-    
+
     for sp in seeds:
-        model.add_species_attribute( core.Species(str(sp)) )
+        model.add_species_attribute(core.Species(str(sp)))
+
     for r_tuple in rules:
         rr = core.ReactionRule()
         for reactant in r_tuple[0]:
-            rr.add_reactant(core.Species( str(reactant) ))
+            rr.add_reactant(core.Species(str(reactant)))
         for product in r_tuple[1]:
-            rr.add_product(core.Species( str(product) ))
-        opts = r_tuple[2]
+            rr.add_product(core.Species(str(product)))
+
         # kinetic parameter
-        if isinstance(opts, list) and 0 < len(opts):
-            rr.set_k(opts[0])
+        for opt in r_tuple[2]:
+            if (not isinstance(opt, bool)
+                and isinstance(opt, (int, long, float, complex))):
+                rr.set_k(opt)
+                break
         else:
-            rr.set_k(opts)
+            raise RuntimeError, "No kinetic rate is defined."
+
         model.add_reaction_rule(rr)
+
     return model
 
-        
+
+if __name__ == '__main__':
+    pass
