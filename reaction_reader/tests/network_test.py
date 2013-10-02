43c513d74ada8eeb1b4353827cc251e2e2c3dec7
devel <devel@dhammapada.(none)>
2013-10-02 17:43:36 +0900

    added a simple test

--- reaction_reader/tests/network_test.py
+++ reaction_reader/tests/network_test.py
@@ -10,7 +10,17 @@ class NetworkTestCase(unittest.TestCase):
         pass
 
     def test_check_stoichiometry(self):
-        self.assertTrue(True)
+        sp1 = create_species(
+            "A(l,r^1).A(l^1,r^2).B(l^2,r^3).A(l^3,r^4)"
+            + ".B(l^4,r^5).A(l^5,r^6).B(l^6,r)")
+        self.assertTrue(
+            network.check_stoichiometry(sp1, dict(A=4, B=3)))
+        self.assertTrue(
+            network.check_stoichiometry(sp1, dict(C=0)))
+        self.assertFalse(
+            network.check_stoichiometry(sp1, dict(A=3, B=3)))
+        self.assertFalse(
+            network.check_stoichiometry(sp1, dict(A=4, B=2)))
 
 
 if __name__ == '__main__':
