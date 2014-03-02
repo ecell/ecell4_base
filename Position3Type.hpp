76029ce2736679d983ebb3307588d11013e1f509
Yuki Sakamoto <j.c.ferrero.esp@gmail.com>
2014-03-02 15:12:28 +0900

    removed unnecessary comments

--- Position3Type.hpp
+++ Position3Type.hpp
@@ -27,63 +27,14 @@
 #include "utils/array_traits.hpp"
 #include "linear_algebra.hpp"
 
-/*
-#include <algorithm>
-#include <cmath>
-#include <gsl/gsl_pow_int.h>
-#include <boost/preprocessor/tuple/elem.hpp>
-#include <boost/preprocessor/repetition/repeat.hpp>
-#include <boost/preprocessor/repetition/enum_params.hpp>
-#include <boost/preprocessor/repetition/enum_trailing_params.hpp>
-#include <boost/mpl/bool.hpp>
-#include <boost/mpl/and.hpp>
-#include <boost/type_traits/is_arithmetic.hpp>
-#include <boost/utility/enable_if.hpp>
-#include <boost/array.hpp>
-#include <boost/multi_array.hpp>
-#include <boost/preprocessor/repetition/repeat_from_to.hpp>
-*/
-
-
 template <std::size_t N_>
 struct is_vector<ecell4::Position3, N_>: public boost::mpl::true_ {};
-
-
-/*
-template <>
-struct is_vector3<ecell4::Position3>: public boost::mpl::true_ {};
-*/
-
-
 
 template <>
 struct element_type_of<ecell4::Position3>
 {
     typedef ecell4::Position3::value_type type;
 };
-
-
-/*
-template <typename T>
-ecell4::Position3 translate_to_Position3(Vector3<T, 3> src)
-{
-    ecell4::Position3 retval( 
-            boost::lexical_cast<double> src[0],
-            boost::lexical_cast<double> src[1],
-            boost::lexical_cast<double> src[2] );
-    return retval;
-}
-
-template <>
-ecell4::Position3 translate_to_Position3<double> (Vector3<double, 3> src)
-{
-    ecell4::Position3 retval( 
-            boost::lexical_cast<double> src[0],
-            boost::lexical_cast<double> src[1],
-            boost::lexical_cast<double> src[2] );
-    return retval;
-}
-*/
 
 #if defined(HAVE_TR1_FUNCTIONAL)
 namespace std { namespace tr1 {
