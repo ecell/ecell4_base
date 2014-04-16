3675d56a0d088198cf5221027d030eaa69706272
Kazunari Kaizu <kwaizu@gmail.com>
2014-04-16 16:51:27 +0900

    donot forget to add a file

---
+++ core_python/ecell4/UnitSpecies.pyx
@@ -0,0 +1,38 @@
+from cython.operator cimport dereference as deref
+from libcpp.string cimport string
+cimport util
+
+
+cdef class UnitSpecies:
+
+    def __cinit__(self, string name):
+        self.thisptr = new Cpp_UnitSpecies(name)
+
+    def __dealloc__(self):
+        del self.thisptr
+
+    def __richcmp__(UnitSpecies self, UnitSpecies rhs, int op):
+        cdef int compare
+        if deref(self.thisptr) > deref(rhs.thisptr):
+            compare = 1
+        elif deref(self.thisptr) < deref(rhs.thisptr):
+            compare = -1
+        else: # self == rhs
+            compare = 0
+        return util.richcmp_helper(compare, op)
+
+    def __hash__(self):
+        return hash(self.thisptr.serial())
+
+    def serial(self):
+        return self.thisptr.serial()
+
+    def name(self):
+        return self.thisptr.name()
+
+cdef UnitSpecies UnitSpecies_from_Cpp_UnitSpecies(Cpp_UnitSpecies *sp):
+    cdef Cpp_UnitSpecies *new_obj = new Cpp_UnitSpecies(deref(sp))
+    r = UnitSpecies("")
+    del r.thisptr
+    r.thisptr = new_obj
+    return r
