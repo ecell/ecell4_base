f92cba0811b91beff6d0a6180a49c8bf0e2cb720
Kazunari Kaizu <kwaizu@gmail.com>
2014-07-02 15:27:08 +0900

    rollbacked

--- ecell4/core/Species.cpp
+++ ecell4/core/Species.cpp
@@ -24,7 +24,30 @@ bool Species::operator>(const Species& rhs) const
 
 bool Species::match(const Species& target) const
 {
-    return spmatch(*this, target);
+    // return spmatch(*this, target);
+    container_type::const_iterator i(units_.begin()), j(target.begin());
+    while (i != units_.end())
+    {
+        const UnitSpecies& usp(*i);
+
+        j = std::lower_bound(j, target.end(), usp);
+        if (j == target.end())
+        {
+            return false;
+        }
+
+        const container_type::const_iterator
+            nexti(std::upper_bound(i, units_.end(), usp)),
+            nextj(std::upper_bound(j, target.end(), usp));
+        if (nextj - j < nexti - i)
+        {
+            return false;
+        }
+
+        i = nexti;
+        j = nextj;
+     }
+     return true;
 }
 
 class unit_species_comparerator
