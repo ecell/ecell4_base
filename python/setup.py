82269312c07e339c0cf40866fe759e085a69002c
Kazunari Kaizu <kwaizu@gmail.com>
2014-07-07 17:02:04 +0900

    added the way for the pure cython install

--- python/setup.py
+++ python/setup.py
@@ -1,4 +1,5 @@
 import sys
+import glob
 from distutils.core import setup, Command
 from distutils.extension import Extension
 import unittest
@@ -36,12 +37,8 @@ class run_tests(Command):
         test_runner = unittest.TextTestRunner()
         test_runner.run(suite)
 
-setup(
-    name = "ecell4",
-    package_dir = {"": "lib"},
-    package_data = {"ecell4.util": ["templates/*"]},
-    packages = ["ecell4", "ecell4.util", "ecell4.reaction_reader"],
-    cmdclass = {'build_ext': build_ext, 'test': run_tests},
+with_cpp_shared_libraries = True
+if with_cpp_shared_libraries:
     ext_modules = [
         Extension("ecell4.core", sources=["lib/ecell4/core.pyx"],
             include_dirs=["."], libraries=["ecell4-core"], language="c++"),
@@ -57,4 +54,37 @@ setup(
         Extension("ecell4.lattice", sources=["lib/ecell4/lattice.pyx"],
             include_dirs=["."], libraries=["ecell4-core", "ecell4-lattice"],
             language="c++")
-        ])
+        ]
+else:
+    dependent_libs = [
+        'gsl', 'gslcblas', 'm', 'hdf5_hl_cpp', 'hdf5_cpp', 'hdf5_hl', 'hdf5']
+    core_src = glob.glob("../ecell4/core/*.cpp")
+    ext_modules = [
+        Extension("ecell4.core", sources=["lib/ecell4/core.pyx"] + core_src,
+            include_dirs=[".", ".."], libraries=dependent_libs, language="c++"),
+        Extension("ecell4.gillespie",
+            sources=["lib/ecell4/gillespie.pyx"]
+                + glob.glob("../ecell4/gillespie/*.cpp") + core_src,
+            libraries=dependent_libs, include_dirs=[".", ".."], language="c++"),
+        Extension("ecell4.bd",
+            sources=["lib/ecell4/bd.pyx"]
+                + glob.glob("../ecell4/bd/*.cpp") + core_src,
+            libraries=dependent_libs, include_dirs=[".", ".."], language="c++"),
+        Extension("ecell4.ode",
+            sources=["lib/ecell4/ode.pyx"]
+                + glob.glob("../ecell4/ode/*.cpp") + core_src,
+            libraries=dependent_libs, include_dirs=[".", ".."], language="c++"),
+        Extension("ecell4.lattice",
+            sources=["lib/ecell4/lattice.pyx"]
+                + glob.glob("../ecell4/lattice/*.cpp") + core_src,
+            libraries=dependent_libs, include_dirs=[".", ".."], language="c++"),
+        ]
+
+setup(
+    name = "ecell4",
+    package_dir = {"": "lib"},
+    package_data = {"ecell4.util": ["templates/*"]},
+    packages = ["ecell4", "ecell4.util", "ecell4.reaction_reader"],
+    cmdclass = {'build_ext': build_ext, 'test': run_tests},
+    ext_modules = ext_modules
+    )
