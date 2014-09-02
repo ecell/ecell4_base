be51da88ed20dfd101e0c4d2c5b6c059829a651b
Yuki Sakamoto <sakamotoyuki.jpn@gmail.com>
2014-09-02 10:44:47 +0000

    begin to implement ode-ratelow

---
+++ ecell4/ode/ODESimulator_ratelow.hpp
@@ -0,0 +1,205 @@
+#ifndef __ECELL4_ODE_ODE_SIMULATOR_HPP
+#define __ECELL4_ODE_ODE_SIMULATOR_HPP
+
+#include <cstring>
+#include <vector>
+#include <boost/shared_ptr.hpp>
+
+#include <ecell4/core/exceptions.hpp>
+#include <ecell4/core/types.hpp>
+#include <ecell4/core/get_mapper_mf.hpp>
+#include <ecell4/core/NetworkModel.hpp>
+#include <ecell4/core/Simulator.hpp>
+#include <ecell4/core/ModelWrapper.hpp>
+
+#include "ODEWorld.hpp"
+
+#include <boost/scoped_ptr.hpp>
+#include <boost/scoped_array.hpp>
+
+namespace ecell4
+{
+
+namespace ode
+{
+
+class ODESystem
+{
+public:
+
+    typedef std::vector<double> state_type;
+    typedef Model::reaction_rule_container_type reaction_rule_container_type;
+
+protected:
+
+    typedef utils::get_mapper_mf<
+        Species, state_type::size_type>::type species_map_type;
+
+public:
+
+    ODESystem(const std::vector<Species>& species,
+        const reaction_rule_container_type& reaction_rules, const Real& volume)
+        : species_(species), reaction_rules_(reaction_rules), volume_(volume)
+    {
+        initialize();
+    }
+
+    void initialize()
+    {
+        state_type::size_type i(0);
+        for (std::vector<Species>::const_iterator
+                 it(species_.begin()); it != species_.end(); ++it)
+        {
+            index_map_[*it] = i;
+            ++i;
+        }
+    }
+
+    void operator()(const state_type& x, state_type& dxdt, const double& t)
+    {
+        for (state_type::iterator i(dxdt.begin()); i != dxdt.end(); ++i)
+        {
+            *i = 0.0;
+        }
+
+        for (reaction_rule_container_type::const_iterator
+            i(reaction_rules_.begin()); i != reaction_rules_.end(); ++i)
+        {
+            double flux((*i).k() * volume_);
+
+            const ReactionRule::reactant_container_type&
+                reactants((*i).reactants());
+            const ReactionRule::product_container_type&
+                products((*i).products());
+            for (ReactionRule::reactant_container_type::const_iterator
+                     j(reactants.begin()); j != reactants.end(); ++j)
+            {
+                flux *= x[index_map_[*j]] / volume_;
+            }
+
+            for (ReactionRule::reactant_container_type::const_iterator
+                     j(reactants.begin()); j != reactants.end(); ++j)
+            {
+                dxdt[index_map_[*j]] -= flux;
+            }
+
+            for (ReactionRule::product_container_type::const_iterator
+                     j(products.begin()); j != products.end(); ++j)
+            {
+                dxdt[index_map_[*j]] += flux;
+            }
+        }
+    }
+
+protected:
+
+    const std::vector<Species>& species_;
+    const reaction_rule_container_type& reaction_rules_;
+    Real volume_;
+
+    species_map_type index_map_;
+};
+
+struct StateAndTimeBackInserter
+{
+    typedef std::vector<ODESystem::state_type> state_container_type;
+    typedef std::vector<double> time_container_type;
+
+    state_container_type& m_states;
+    time_container_type& m_times;
+
+    StateAndTimeBackInserter(
+        state_container_type& states, time_container_type& times)
+        : m_states(states), m_times(times)
+    {
+        ;
+    }
+
+    void operator()(const ODESystem::state_type&x, double t)
+    {
+        m_states.push_back(x);
+        m_times.push_back(t);
+    }
+};
+
+class ODESimulatorRatelow
+    : public Simulator<NetworkModel, ODEWorld>
+{
+public:
+
+    typedef Simulator<NetworkModel, ODEWorld> base_type;
+
+public:
+
+    ODESimulatorRatelow(
+        boost::shared_ptr<NetworkModel> model,
+        boost::shared_ptr<ODEWorld> world)
+        : base_type(model, world), dt_(inf)
+    {
+        initialize();
+    }
+
+    ODESimulatorRatelow(boost::shared_ptr<ODEWorld> world)
+        : base_type(world), dt_(inf)
+    {
+        initialize();
+    }
+
+    void initialize()
+    {
+        const std::vector<Species> species(model_->list_species());
+        for (std::vector<Species>::const_iterator
+                 i(species.begin()); i != species.end(); ++i)
+        {
+            if (!(*world_).has_species(*i))
+            {
+                (*world_).reserve_species(*i);
+            }
+        }
+    }
+
+    // SimulatorTraits
+
+    Real t(void) const
+    {
+        return (*world_).t();
+    }
+
+    Real dt() const
+    {
+        return dt_;
+    }
+
+    void step(void)
+    {
+        step(next_time());
+    }
+
+    bool step(const Real& upto);
+
+    // Optional members
+
+    void set_t(const Real& t)
+    {
+        (*world_).set_t(t);
+    }
+
+    void set_dt(const Real& dt)
+    {
+        if (dt <= 0)
+        {
+            throw std::invalid_argument("The step size must be positive.");
+        }
+        dt_ = dt;
+    }
+
+protected:
+
+    Real dt_;
+};
+
+} // ode
+
+} // ecell4
+
+#endif /* __ECELL4_ODE_ODE_SIMULATOR_HPP */
---
+++ ecell4/ode/tests/ODESimulator_test2.cpp
@@ -0,0 +1,73 @@
+#define BOOST_TEST_MODULE "ODESimulator_test"
+
+#ifdef UNITTEST_FRAMEWORK_LIBRARY_EXIST
+#   include <boost/test/unit_test.hpp>
+#else
+#   define BOOST_TEST_NO_LIB
+#   include <boost/test/included/unit_test.hpp>
+#endif
+
+#include <ecell4/core/Species.hpp>
+#include <ecell4/core/ReactionRule.hpp>
+#include <ecell4/core/NetworkModel.hpp>
+#include "../ODESimulator_ratelow.hpp"
+
+using namespace ecell4;
+using namespace ecell4::ode;
+
+
+BOOST_AUTO_TEST_CASE(ODESimulator_test_constructor)
+{
+    const Real L(1e-6);
+    const Position3 edge_lengths(L, L, L);
+
+    boost::shared_ptr<NetworkModel> model(new NetworkModel());
+    boost::shared_ptr<ODEWorld> world(new ODEWorld(edge_lengths));
+
+    ODESimulatorRatelow target(model, world);
+}
+
+BOOST_AUTO_TEST_CASE(ODESimulator_test_step1)
+{
+    const Real L(1e-6);
+    const Position3 edge_lengths(L, L, L);
+
+    boost::shared_ptr<NetworkModel> model(new NetworkModel());
+    boost::shared_ptr<ODEWorld> world(new ODEWorld(edge_lengths));
+
+    ODESimulatorRatelow target(model, world);
+    target.step(1.0);
+}
+
+BOOST_AUTO_TEST_CASE(ODESimulator_test_step2)
+{
+    const Real L(1e-6);
+    const Position3 edge_lengths(L, L, L);
+
+    Species sp1("A"), sp2("B"), sp3("C");
+    ReactionRule rr1;
+    rr1.set_k(1.0);
+    rr1.add_reactant(sp1);
+    rr1.add_product(sp2);
+    rr1.add_product(sp3);
+
+    boost::shared_ptr<NetworkModel> model(new NetworkModel());
+    model->add_species_attribute(sp1);
+    model->add_species_attribute(sp2);
+    model->add_species_attribute(sp3);
+    model->add_reaction_rule(rr1);
+
+    boost::shared_ptr<ODEWorld> world(new ODEWorld(edge_lengths));
+    world->reserve_species(sp1);
+    world->set_value(sp1, 60);
+
+    ODESimulatorRatelow target(model, world);
+
+    // std::cout << target.t() << ":" << world->num_molecules(sp1)
+    //           << ":" << world->num_molecules(sp2) << std::endl;
+    target.step(1.0);
+    // std::cout << target.t() << ":" << world->num_molecules(sp1)
+    //           << ":" << world->num_molecules(sp2) << std::endl;
+
+    // BOOST_ASSERT(false);
+}
