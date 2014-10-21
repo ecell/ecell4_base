81d35c50dd458c9e13e6e1690036e3c5a4948d05
Kazunari Kaizu <kwaizu@gmail.com>
2014-10-21 17:26:04 +0900

    forgot to add new files

---
+++ ecell4/ode/ODEFactory.hpp
@@ -0,0 +1,67 @@
+#ifndef __ECELL4_ODE_ODE_FACTORY_HPP
+#define __ECELL4_ODE_ODE_FACTORY_HPP
+
+#include <ecell4/core/SimulatorFactory.hpp>
+#include "ODEWorld.hpp"
+#include "ODESimulator.hpp"
+
+
+namespace ecell4
+{
+
+namespace ode
+{
+
+class ODEFactory:
+    public SimulatorFactory<ODEWorld, ODESimulator>
+{
+public:
+
+    typedef SimulatorFactory<ODEWorld, ODESimulator> base_type;
+
+public:
+
+    ODEFactory()
+        : base_type()
+    {
+        ; // do nothing
+    }
+
+    virtual ~ODEFactory()
+    {
+        ; // do nothing
+    }
+
+    virtual ODEWorld* create_world(const std::string filename) const
+    {
+        return new ODEWorld(filename);
+    }
+
+    virtual ODEWorld* create_world(
+        const Position3& edge_lengths = Position3(1, 1, 1)) const
+    {
+        return new ODEWorld(edge_lengths);
+    }
+
+    virtual ODESimulator* create_simulator(
+        const boost::shared_ptr<Model>& model,
+        const boost::shared_ptr<world_type>& world) const
+    {
+        return new ODESimulator(
+            boost::dynamic_pointer_cast<NetworkModel>(model), world); //XXX:
+    }
+
+    virtual ODESimulator* create_simulator(
+        const boost::shared_ptr<world_type>& world) const
+    {
+        return new ODESimulator(world);
+    }
+
+protected:
+};
+
+} // ode
+
+} // ecell4
+
+#endif /* __ECELL4_ODE_ODE_FACTORY_HPP */
