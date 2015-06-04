de17c11de3590f45a9af6b3e7fc839d626b68ec4
Yuki Sakamoto <j.c.ferrero.esp@gmail.com>
2015-06-04 14:27:02 +0900

    remove jacobi_func from ODERatelaw. Always using forward difference method.

--- ecell4/ode/ODERatelaw.cpp
+++ ecell4/ode/ODERatelaw.cpp
@@ -25,62 +25,6 @@ Real ODERatelawCppCallback::deriv_func(
     return this->func_(reactants_state_array, products_state_array, volume);
 }
 
-void ODERatelawCppCallback::jacobi_func(
-        matrix_type &jacobian,
-        state_container_type const &reactants_state_array,
-        state_container_type const &products_state_array,
-        Real const volume, Real const t, 
-        ODEReactionRule const &rr)
-{
-    Real h(this->h_); //XXX: 1.0e-8. Should be fixed
-
-    std::fill(jacobian.data().begin(), jacobian.data().end(), Real(0.0));
-    Real flux(this->deriv_func(reactants_state_array, products_state_array, volume, t, rr));
-    double num_reactants(reactants_state_array.size());
-    double num_products(products_state_array.size());
-
-    // Differentiates by Reactants.
-    for (int i(0); i < num_reactants; i++)
-    {
-        //XXX: For now, we are using FORWARD difference method.
-        state_container_type h_shift(reactants_state_array);
-        h_shift[i] += h;
-        double deriv = (
-            this->deriv_func(h_shift, products_state_array, volume, t, rr) - flux) / h;
-        for (matrix_type::size_type j(0); j < jacobian.size1(); j++)
-        {
-            if (j < num_reactants)
-            {
-                jacobian(j, i) -= deriv;
-            }
-            else
-            {
-                jacobian(j, i) += deriv;
-            }
-        }
-    }
-
-    // Differentiates by Products.
-    for (int i(0); i < num_products; i++)
-    {
-        state_container_type h_shift(products_state_array);
-        h_shift[i] += h;
-        double deriv = (
-            this->deriv_func(reactants_state_array, h_shift, volume, t, rr) - flux) / h;
-        for (matrix_type::size_type j(0); j < jacobian.size1(); j++)
-        {
-            if (j < num_reactants)
-            {
-                jacobian(j, i + num_reactants) -= deriv;
-            }
-            else
-            {
-                jacobian(j, i + num_reactants) += deriv;
-            }
-        }
-    }
-}
-
 Real ODERatelawMassAction::deriv_func(
         state_container_type const &reactants_state_array,
         state_container_type const &products_state_array, 
@@ -96,43 +40,6 @@ Real ODERatelawMassAction::deriv_func(
         flux *= std::pow( (*it) / volume, reactants_coefficients[i]);
     }
     return flux;
-}
-
-void ODERatelawMassAction::jacobi_func(
-        matrix_type &jacobian,
-        state_container_type const &reactants_state_array,
-        state_container_type const &products_state_array, 
-        Real const volume, Real const t, 
-        ODEReactionRule const &rr)
-{
-    // The size of the argument 'state_array' must be resized
-    // to the number of reactants.
-    // The size of the argument 'jacobian' must be resized
-    // to the number of (reactants + products)
-    std::fill(jacobian.data().begin(), jacobian.data().end(), Real(0.0));
-    Real flux(this->deriv_func(reactants_state_array, products_state_array, volume, t, rr));
-    if (flux == Real(0.0))
-    {
-        return;
-    }
-
-    matrix_type::size_type num_reactants(
-        static_cast<matrix_type::size_type>(reactants_state_array.size()));
-    for(matrix_type::size_type i(0); i < num_reactants; i++)
-    {
-        Real partial(flux / reactants_state_array[i]);
-        for(matrix_type::size_type j(0); j < jacobian.size1(); j++)
-        {
-            if (j < num_reactants)
-            {
-                jacobian(j, i) -= partial;
-            }
-            else
-            {
-                jacobian(j, i) += partial;
-            }
-        }
-    }
 }
 
 } // ode
--- ecell4/ode/ODERatelaw.hpp
+++ ecell4/ode/ODERatelaw.hpp
@@ -51,14 +51,6 @@ public:
         state_container_type const &products_state_array, 
         Real const volume, Real const t,
         ODEReactionRule const &reaction) = 0;
-
-    virtual void jacobi_func(
-        matrix_type &jacobian,
-        state_container_type const &reactants_state_array,
-        state_container_type const &products_state_array,
-        Real const volume, Real const t,
-        ODEReactionRule const &reaction) = 0;
-
 };
 
 class ODERatelawCppCallback
@@ -100,12 +92,6 @@ public:
     virtual Real deriv_func(
         state_container_type const &reactants_state_array,
         state_container_type const &products_state_array, 
-        Real const volume, Real const t, ODEReactionRule const &rr);
-
-    virtual void jacobi_func(
-        matrix_type &jacobian,
-        state_container_type const &reactants_state_array,
-        state_container_type const &products_state_array,
         Real const volume, Real const t, ODEReactionRule const &rr);
 
     ODERatelaw_Callback get_callback() const
@@ -177,63 +163,6 @@ public:
             this->python_func_, reactants_state_array, products_state_array, volume);
     }
 
-
-    virtual void jacobi_func(
-        matrix_type &jacobian,
-        state_container_type const &reactants_state_array,
-        state_container_type const &products_state_array,
-        Real const volume, Real const t, ODEReactionRule const &rr)
-    {
-        Real h(this->h_); //XXX: 1.0e-8. Should be fixed
-        std::fill(jacobian.data().begin(), jacobian.data().end(), Real(0.0));
-        Real flux(this->deriv_func(reactants_state_array, products_state_array, volume, t, rr));
-        double num_reactants(reactants_state_array.size());
-        double num_products(products_state_array.size());
-
-        // Differentiates by Reactants.
-        for (int i(0); i < num_reactants; i++)
-        {
-            //XXX: For now, we are using FORWARD difference method.
-            state_container_type h_shift(reactants_state_array);
-            h_shift[i] += h;
-            double deriv = (
-                this->deriv_func(h_shift, products_state_array, volume, t, rr) - flux) / h;
-            for (matrix_type::size_type j(0); j < jacobian.size1(); j++)
-            {
-                if (j < num_reactants)
-                {
-                    jacobian(j, i) -= deriv;
-                }
-                else
-                {
-                    jacobian(j, i) += deriv;
-                }
-            }
-        }
-
-        // Differentiates by Products.
-        for (int i(0); i < num_products; i++)
-        {
-            state_container_type h_shift(products_state_array);
-            h_shift[i] += h;
-            double deriv = (
-                this->deriv_func(reactants_state_array, h_shift, volume, t, rr) - flux) / h;
-            for (matrix_type::size_type j(0); j < jacobian.size1(); j++)
-            {
-                if (j < num_reactants)
-                {
-                    jacobian(j, i + num_reactants) -= deriv;
-                }
-                else
-                {
-                    jacobian(j, i + num_reactants) += deriv;
-                }
-            }
-        }
-
-        return; //XXX:
-    }
-
     void set_callback_pyfunc(Python_Functype new_func)
     {
         if (new_func == 0)
@@ -272,12 +201,6 @@ public:
     }
 
     virtual Real deriv_func(
-        state_container_type const &reactants_state_array,
-        state_container_type const &products_state_array, 
-        Real const volume, Real const t, ODEReactionRule const &rr);
-
-    virtual void jacobi_func(
-        matrix_type &jacobian,
         state_container_type const &reactants_state_array,
         state_container_type const &products_state_array, 
         Real const volume, Real const t, ODEReactionRule const &rr);
