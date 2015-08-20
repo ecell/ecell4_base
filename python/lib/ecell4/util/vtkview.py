2e0c080ad002290a664cada9cab921bdb0cb212f
Kazunari Kaizu <kwaizu@gmail.com>
2015-08-20 15:44:51 +0900

    Enable volume rendering with VTK

--- python/lib/ecell4/util/vtkview.py
+++ python/lib/ecell4/util/vtkview.py
@@ -3,40 +3,43 @@ import ecell4
 import os.path
 import argparse
 import sys
+import numpy
 
 colors = [
-    ('Red', [0.8, 0.1, 0.1]), 
-    ('Green', [0.27, 0.8, 0.21]), 
-    ('Blue', [0.24, 0.41, 0.7]), 
-    ('Yellow', [1.0, 0.5, 0.0]), 
-    ('Orange', [1.0, 0.37, 0.05]), 
-    ('White', [1, 1, 1]), 
-    ('Magenta', [0.72, 0.29, 1.0]), 
-    ('Cyan', [0.1, 1.0, 0.6]), 
-    ('Black', [0.1, 0.1, 0.1]), 
-    ('Grey', [0.46, 0.46, 0.46]), 
-    ('LightBlue', [0.32, 0.42, 1]), 
-    ('LightGrey', [0.6, 0.6, 0.6]), 
-    ('BrightGreen', [0.4, 1.0, 0.14]), 
-    ('BrightYellowGreen', [0.64, 1.0, 0.05]), 
-    ('BrightYellow', [1.0, 0.67, 0.0]), 
-    ('WhiteGray', [0.9, 0.9, 0.9]), 
-    ('WhiteMagenta', [0.8, 0.48, 1.0]), 
-    ('WhiteYellow', [1.0, 0.75, 0.17]), 
-    ('WhitePurple', [0.67, 0.6, 1.0]), 
-    ('DarkRed', [0.46, 0.1, 0.1]), 
-    ('DarkGreen', [0.1, 0.5, 0.1]), 
-    ('DarkBlue', [0.1, 0.2, 0.5]), 
+    ('Red', [0.8, 0.1, 0.1]),
+    ('Green', [0.27, 0.8, 0.21]),
+    ('Blue', [0.24, 0.41, 0.7]),
+    ('Yellow', [1.0, 0.5, 0.0]),
+    ('Orange', [1.0, 0.37, 0.05]),
+    ('White', [1, 1, 1]),
+    ('Magenta', [0.72, 0.29, 1.0]),
+    ('Cyan', [0.1, 1.0, 0.6]),
+    ('Black', [0.1, 0.1, 0.1]),
+    ('Grey', [0.46, 0.46, 0.46]),
+    ('LightBlue', [0.32, 0.42, 1]),
+    ('LightGrey', [0.6, 0.6, 0.6]),
+    ('BrightGreen', [0.4, 1.0, 0.14]),
+    ('BrightYellowGreen', [0.64, 1.0, 0.05]),
+    ('BrightYellow', [1.0, 0.67, 0.0]),
+    ('WhiteGray', [0.9, 0.9, 0.9]),
+    ('WhiteMagenta', [0.8, 0.48, 1.0]),
+    ('WhiteYellow', [1.0, 0.75, 0.17]),
+    ('WhitePurple', [0.67, 0.6, 1.0]),
+    ('DarkRed', [0.46, 0.1, 0.1]),
+    ('DarkGreen', [0.1, 0.5, 0.1]),
+    ('DarkBlue', [0.1, 0.2, 0.5]),
     ('DarkOrange', [0.845, 0.179, 0.102])]
 
 class vtkTimerCallback():
 
-    def __init__(self, filenames, saveimage=False, txt=None):
+    def __init__(self, filenames, saveimage=False, volume=False, txt=None):
          self.timer_count = 0
          self.filenames = filenames
          self.timer_id = None
          self.txt = txt
          self.saveimage = saveimage
+         self.volume = volume
+         self.running = True
 
     def filename(self):
         return self.filenames[min(self.timer_count, len(self.filenames) - 1)]
@@ -53,9 +56,12 @@ class vtkTimerCallback():
 
         renWin.SetWindowName(filename)
         ren = renWin.GetRenderers().GetFirstRenderer()
-        remove_all_actors(ren)
+        remove_all_actors_and_volumes(ren)
         w = load_world(filename)
-        add_actors(w, ren, source)
+        if self.volume:
+            add_volume(w, ren)
+        else:
+            add_actors(w, ren, source)
         if self.txt is not None:
             self.txt.SetInput("t={0:g}".format(w.t()))
 
@@ -73,17 +79,28 @@ class vtkTimerCallback():
         return True
 
     def execute(self, iren, event=None):
+        if not self.running:
+            return
+
         renWin = iren.GetRenderWindow()
         if not self.update(renWin):
             self.stop(iren)
             return
 
-def remove_all_actors(ren):
+def remove_all_actors_and_volumes(ren):
     actors = ren.GetActors()
     while actors.GetNumberOfItems() > 0:
         actor = actors.GetLastActor()
         ren.RemoveActor(actor)
         actors.RemoveItem(actor)
+
+    volumes = ren.GetVolumes()
+    volumes.InitTraversal()
+    while True:
+        volume = volumes.GetNextVolume()
+        if volume is None:
+            break
+        ren.RemoveVolume(volume)
 
 def load_world(filename):
     return ecell4.meso.MesoscopicWorld(filename)
@@ -117,21 +134,86 @@ def add_actors(w, ren, source):
             mapper.SetInput(atoms.GetOutput())
         else:
             mapper.SetInputConnection(atoms.GetOutputPort())
-         
+
         # actor
         actor = vtk.vtkActor()
         actor.GetProperty().SetColor(*colors[cnt % len(colors)][1])
         actor.SetMapper(mapper)
-         
+
         # assign actor to the renderer
         ren.AddActor(actor)
+
+def add_volume(w, ren, N=50, c=(0, 1, 0)):
+    L = max(w.edge_lengths())
+    shift = w.edge_lengths() / L * 0.5
+
+    tmp = numpy.zeros([N, N, N], dtype=float)
+    for sp in w.list_species():
+    # for serial in ("MinD_M", "MinDE"):
+    #     sp = ecell4.core.Species(serial)
+        for pid, p in w.list_particles(sp):
+            pos = p.position() / L - shift
+            pos = (pos + ecell4.core.Real3(0.5, 0.5, 0.5)) * N
+            tmp[int(pos[2])][int(pos[1])][int(pos[0])] += 1
+
+    data_matrix = numpy.zeros([N, N, N], dtype=numpy.uint8)
+    if tmp.max() > 0:
+        norm = 255 / tmp.max()
+        tmp *= norm
+        for x in range(N):
+            for y in range(N):
+                for z in range(N):
+                    data_matrix[x][y][z] = numpy.uint8(tmp[x][y][z])
+    del tmp
+
+    dataImporter = vtk.vtkImageImport()
+    data_string = data_matrix.tostring()
+    dataImporter.CopyImportVoidPointer(data_string, len(data_string))
+    dataImporter.SetDataScalarTypeToUnsignedChar()
+    dataImporter.SetNumberOfScalarComponents(1)
+    (l, m, n) = data_matrix.shape
+    dataImporter.SetDataExtent(0, l - 1, 0, m - 1, 0, n - 1)
+    dataImporter.SetWholeExtent(0, l - 1, 0, m - 1, 0, n - 1)
+
+    alphaChannelFunc = vtk.vtkPiecewiseFunction()
+    # alphaChannelFunc.AddPoint(0, 0.0)
+    # alphaChannelFunc.AddPoint(50, 0.05)
+    # alphaChannelFunc.AddPoint(100, 0.1)
+    # alphaChannelFunc.AddPoint(150, 0.2)
+    # alphaChannelFunc.AddPoint(0, 0.0)
+    # alphaChannelFunc.AddPoint(1, 0.05)
+    # alphaChannelFunc.AddPoint(data_matrix.max(), 0.05)
+    alphaChannelFunc.AddPoint(0, 0.0)
+    alphaChannelFunc.AddPoint(1, 0.255 / 255)
+    alphaChannelFunc.AddPoint(255, 0.255)
+
+    colorFunc = vtk.vtkColorTransferFunction()
+    # colorFunc.AddRGBPoint(50, 1.0, 0.0, 0.0)
+    # colorFunc.AddRGBPoint(100, 0.0, 1.0, 0.0)
+    # colorFunc.AddRGBPoint(150, 0.0, 0.0, 1.0)
+    if data_matrix.max() > 0:
+        colorFunc.AddRGBPoint(data_matrix.max(), *c)
+
+    volumeProperty = vtk.vtkVolumeProperty()
+    volumeProperty.SetColor(colorFunc)
+    volumeProperty.SetScalarOpacity(alphaChannelFunc)
+
+    compositeFunction = vtk.vtkVolumeRayCastCompositeFunction()
+    volumeMapper = vtk.vtkVolumeRayCastMapper()
+    volumeMapper.SetVolumeRayCastFunction(compositeFunction)
+    volumeMapper.SetInputConnection(dataImporter.GetOutputPort())
+
+    volume = vtk.vtkVolume()
+    volume.SetMapper(volumeMapper)
+    volume.SetProperty(volumeProperty)
+    ren.AddVolume(volume)
 
 def screenshot(renWin, filename):
     # ffmpeg -r 15 -i input%03d.png -qscale 0 output.mp4
     w2if = vtk.vtkWindowToImageFilter()
     w2if.SetInput(renWin)
     w2if.Update()
- 
+
     writer = vtk.vtkPNGWriter()
     writer.SetFileName(filename)
     writer.SetInput(w2if.GetOutput())
@@ -157,6 +239,9 @@ if __name__ == "__main__":
         '--save-image', action='store_true', dest='saveimage',
         help='whether saving a screenshot for each view')
     parser.add_argument(
+        '--volume', '-V', action='store_true', dest='volume',
+        help='enable volume rendering')
+    parser.add_argument(
         '--offscreen', action='store_true', dest='offscreen',
         help='enable offscreen rendering')
     parser.add_argument(
@@ -174,13 +259,13 @@ if __name__ == "__main__":
     offscreen = args.offscreen
     if offscreen:
         saveimage = True
+    volume = args.volume
 
     # create a rendering window and renderer
     renWin = vtk.vtkRenderWindow()
     renWin.SetSize(768, 432)
     if offscreen:
         renWin.SetOffScreenRendering(True)
-     
     ren = vtk.vtkRenderer()
     renWin.AddRenderer(ren)
 
@@ -208,15 +293,27 @@ if __name__ == "__main__":
         # create a renderwindowinteractor
         iren = vtk.vtkRenderWindowInteractor()
         iren.SetRenderWindow(renWin)
-         
         # enable user interface interactor
         iren.Initialize()
 
+
     # Sign up to receive TimerEvent
-    cb = vtkTimerCallback(filenames, saveimage, None if nolabel else txt)
+    cb = vtkTimerCallback(filenames, saveimage, volume, None if nolabel else txt)
+
+    def keypress_callback_func(obj, event):
+        global cb
+        key = obj.GetKeySym()
+        if key == 'space':
+            cb.running = not cb.running
+
+    # add keyboard interface, initialize, and start the interactor
+    iren.AddObserver("KeyPressEvent", keypress_callback_func)
 
     if not offscreen:
         cb.execute(iren)
+        cb.timer_count = 0
+        cb.running = False
+
         if len(filenames) > 1:
             iren.AddObserver('TimerEvent', cb.execute)
             cb.timer_id = iren.CreateRepeatingTimer(dt)
