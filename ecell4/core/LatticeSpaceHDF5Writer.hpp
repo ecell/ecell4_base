33a56742e3350612d4d0674bd254f6c93324c7eb
Suguru Kato <salmon.bioinformatics@gmail.com>
2015-10-22 15:11:41 +0900

    Use macro in LatticeSpaceHDF5Writer.hpp

--- ecell4/core/LatticeSpaceHDF5Writer.hpp
+++ ecell4/core/LatticeSpaceHDF5Writer.hpp
@@ -21,59 +21,47 @@ namespace ecell4
 
 struct LatticeSpaceHDF5Traits
 {
-    typedef struct h5_species_struct {
+    struct h5_species_struct {
         uint32_t id;
         char serial[32]; // species' serial may exceed the limit
         double radius;
         double D;
         uint32_t location;
-    } h5_species_struct;
+    };
 
-    typedef struct h5_voxel_struct {
+    struct h5_voxel_struct {
         int lot;
         int serial;
         uint32_t sid;
-        int64_t coord;
-    } h5_voxel_struct;
+        int64_t coordinate;
+    };
+
+#define INSERT_MEMBER(str, member, type) \
+    h5_comp_type.insertMember(std::string(#member),\
+        HOFFSET(str, member), type)
 
     static H5::CompType get_species_comp_type()
     {
-        H5::CompType h5_species_comp_type(sizeof(h5_species_struct));
-        h5_species_comp_type.insertMember(
-            std::string("id"), HOFFSET(h5_species_struct, id),
-            H5::PredType::STD_I32LE);
-        h5_species_comp_type.insertMember(
-            std::string("serial"), HOFFSET(h5_species_struct, serial),
-            H5::StrType(H5::PredType::C_S1, 32));
-        h5_species_comp_type.insertMember(
-            std::string("radius"), HOFFSET(h5_species_struct, radius),
-            H5::PredType::NATIVE_DOUBLE);
-        h5_species_comp_type.insertMember(
-            std::string("D"), HOFFSET(h5_species_struct, D),
-            H5::PredType::NATIVE_DOUBLE);
-        h5_species_comp_type.insertMember(
-            std::string("location"), HOFFSET(h5_species_struct, location),
-            H5::PredType::STD_I32LE);
-        return h5_species_comp_type;
+        H5::CompType h5_comp_type(sizeof(h5_species_struct));
+        INSERT_MEMBER(h5_species_struct, id, H5::PredType::STD_I32LE);
+        INSERT_MEMBER(h5_species_struct, serial, H5::StrType(H5::PredType::C_S1, 32));
+        INSERT_MEMBER(h5_species_struct, radius, H5::PredType::NATIVE_DOUBLE);
+        INSERT_MEMBER(h5_species_struct, D, H5::PredType::NATIVE_DOUBLE);
+        INSERT_MEMBER(h5_species_struct, location, H5::PredType::STD_I32LE);
+        return h5_comp_type;
     }
 
     static H5::CompType get_voxel_comp_type()
     {
-        H5::CompType h5_voxel_comp_type(sizeof(h5_voxel_struct));
-        h5_voxel_comp_type.insertMember(
-            std::string("lot"), HOFFSET(h5_voxel_struct, lot),
-            H5::PredType::NATIVE_INT);
-        h5_voxel_comp_type.insertMember(
-            std::string("serial"), HOFFSET(h5_voxel_struct, serial),
-            H5::PredType::NATIVE_INT);
-        h5_voxel_comp_type.insertMember(
-            std::string("sid"), HOFFSET(h5_voxel_struct, sid),
-            H5::PredType::STD_I32LE);
-        h5_voxel_comp_type.insertMember(
-            std::string("coordinate"), HOFFSET(h5_voxel_struct, coord),
-            H5::PredType::STD_I64LE);
-        return h5_voxel_comp_type;
+        H5::CompType h5_comp_type(sizeof(h5_voxel_struct));
+        INSERT_MEMBER(h5_voxel_struct, lot, H5::PredType::NATIVE_INT);
+        INSERT_MEMBER(h5_voxel_struct, serial, H5::PredType::NATIVE_INT);
+        INSERT_MEMBER(h5_voxel_struct, sid, H5::PredType::STD_I32LE);
+        INSERT_MEMBER(h5_voxel_struct, coordinate, H5::PredType::STD_I64LE);
+        return h5_comp_type;
     }
+
+#undef INSERT_MEMBER
 
     static void update_h5_species(h5_species_struct& h5_species,
         unsigned int sid, const Species& species)
@@ -91,7 +79,7 @@ struct LatticeSpaceHDF5Traits
         h5_voxel.lot = id_voxel_pair.first.lot();
         h5_voxel.serial = id_voxel_pair.first.serial();
         h5_voxel.sid = sid;
-        h5_voxel.coord = id_voxel_pair.second.coordinate();
+        h5_voxel.coordinate = id_voxel_pair.second.coordinate();
     }
 
 };
@@ -100,8 +88,8 @@ template<typename Tspace_>
 void save_lattice_space(const Tspace_& space, H5::Group* root)
 {
     typedef LatticeSpaceHDF5Traits traits_type;
-    typedef typename traits_type::h5_species_struct h5_species_struct;
-    typedef typename traits_type::h5_voxel_struct h5_voxel_struct;
+    typedef traits_type::h5_species_struct h5_species_struct;
+    typedef traits_type::h5_voxel_struct h5_voxel_struct;
 
     typedef std::vector<std::pair<ParticleID, Voxel> >
         voxel_container_type;
@@ -144,54 +132,45 @@ void save_lattice_space(const Tspace_& space, H5::Group* root)
     }
 
     const int RANK = 1;
-    hsize_t dim1[] = {num_voxels};
-    H5::DataSpace dataspace1(RANK, dim1);
-    boost::scoped_ptr<H5::DataSet> dataset1(new H5::DataSet(
+    hsize_t voxels_dimension[] = {num_voxels};
+    H5::DataSpace dataspace_for_voxels(RANK, voxels_dimension);
+    boost::scoped_ptr<H5::DataSet> voxel_dataset(new H5::DataSet(
         root->createDataSet(
-            "voxels", traits_type::get_voxel_comp_type(), dataspace1)));
+            "voxels", traits_type::get_voxel_comp_type(), dataspace_for_voxels)));
 
-    hsize_t dim2[] = {species.size()};
-    H5::DataSpace dataspace2(RANK, dim2);
-    boost::scoped_ptr<H5::DataSet> dataset2(new H5::DataSet(
+    hsize_t species_dimension[] = {species.size()};
+    H5::DataSpace dataspace_for_species(RANK, species_dimension);
+    boost::scoped_ptr<H5::DataSet> species_dataset(new H5::DataSet(
         root->createDataSet(
-            "species", traits_type::get_species_comp_type(), dataspace2)));
+            "species", traits_type::get_species_comp_type(), dataspace_for_species)));
 
-    dataset1->write(h5_voxel_table.get(), dataset1->getDataType());
-    dataset2->write(h5_species_table.get(), dataset2->getDataType());
+    voxel_dataset->write(h5_voxel_table.get(), voxel_dataset->getDataType());
+    species_dataset->write(h5_species_table.get(), species_dataset->getDataType());
+
+#define CREATE_ATTRIBUTE(attribute, type) \
+    root->createAttribute(#attribute, type,\
+        H5::DataSpace(H5S_SCALAR)).write(type, &attribute)
 
     const uint32_t space_type(static_cast<uint32_t>(Space::LATTICE));
-    H5::Attribute attr_space_type(
-        root->createAttribute(
-            "type", H5::PredType::STD_I32LE, H5::DataSpace(H5S_SCALAR)));
-    attr_space_type.write(H5::PredType::STD_I32LE, &space_type);
+    CREATE_ATTRIBUTE(space_type, H5::PredType::STD_I32LE);
 
     const double t(space.t());
-    H5::Attribute attr_t(
-        root->createAttribute(
-            "t", H5::PredType::IEEE_F64LE, H5::DataSpace(H5S_SCALAR)));
-    attr_t.write(H5::PredType::IEEE_F64LE, &t);
+    CREATE_ATTRIBUTE(t, H5::PredType::IEEE_F64LE);
 
     const double voxel_radius(space.voxel_radius());
-    H5::Attribute attr_voxel_radius(
-        root->createAttribute(
-            "voxel_radius", H5::PredType::IEEE_F64LE, H5::DataSpace(H5S_SCALAR)));
-    attr_voxel_radius.write(H5::PredType::IEEE_F64LE, &voxel_radius);
+    CREATE_ATTRIBUTE(voxel_radius, H5::PredType::IEEE_F64LE);
 
     const uint32_t is_periodic(space.is_periodic()? 1 : 0);
-    H5::Attribute attr_is_periodic(
-        root->createAttribute(
-            "is_periodic", H5::PredType::STD_I32LE, H5::DataSpace(H5S_SCALAR)));
-    attr_is_periodic.write(H5::PredType::STD_I32LE, &is_periodic);
+    CREATE_ATTRIBUTE(is_periodic, H5::PredType::STD_I32LE);
 
-
-    const Real3 edge_lengths(space.edge_lengths());
+    const Real3 lengths(space.edge_lengths());
+    double edge_lengths[] = {lengths[0], lengths[1], lengths[2]};
     const hsize_t dims[] = {3};
     const H5::ArrayType lengths_type(H5::PredType::NATIVE_DOUBLE, 1, dims);
-    H5::Attribute attr_lengths(
-        root->createAttribute(
-            "edge_lengths", lengths_type, H5::DataSpace(H5S_SCALAR)));
-    double lengths[] = {edge_lengths[0], edge_lengths[1], edge_lengths[2]};
-    attr_lengths.write(lengths_type, lengths);
+    CREATE_ATTRIBUTE(edge_lengths, lengths_type);
+
+#undef CREATE_ATTRIBUTE
+
 }
 
 template<typename Tspace_>
@@ -266,7 +245,7 @@ void load_lattice_space(const H5::Group& root, Tspace_* space)
             const Species& species(species_map[h5_voxel_table[i].sid]);
             space->update_voxel(
                 ParticleID(std::make_pair(h5_voxel_table[i].lot, h5_voxel_table[i].serial)),
-                Voxel(species, h5_voxel_table[i].coord,
+                Voxel(species, h5_voxel_table[i].coordinate,
                     atof(species.get_attribute("radius").c_str()),
                     atof(species.get_attribute("D").c_str()),
                     species.get_attribute("location")));
