eddcfd938fae2b4985d0d6f5d2ba461d6458a7cb
Kozo Nishida <knishida@riken.jp>
2016-01-02 12:44:37 +0900

    add "-DWITH_HDF5" to extra_compile_args (for win32)

--- python/setup.py
+++ python/setup.py
@@ -49,7 +49,7 @@ if sys.platform == "win32":
         dependent_libs = ['gsl', 'cblas', 'hdf5_cpp', 'hdf5']
     elif sys.version_info.major == 3:
         dependent_libs = ['gsl', 'gslcblas', 'hdf5_cpp', 'hdf5']
-    extra_compile_args = ["/EHsc", "/w", "-DHAVE_CONFIG_H", "-DHAVE_INLINE"]
+    extra_compile_args = ["/EHsc", "/w", "-DHAVE_CONFIG_H", "-DHAVE_INLINE", "-DWITH_HDF5"]
 elif sys.platform == "darwin":
     dependent_libs = ['gsl', 'gslcblas', 'm', 'hdf5_cpp', 'hdf5']
     extra_compile_args = ["-DWITH_HDF5"]
