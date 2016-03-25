f17c3ffe2e3557b17cc0502bcb96a97f4571c7f2
Kozo Nishida <kozo.nishida@gmail.com>
2016-03-26 05:00:15 +0900

    replace libsbml to bs4 and requests

--- python/lib/ecell4/util/datasource.py
+++ python/lib/ecell4/util/datasource.py
@@ -1,11 +1,16 @@
-from libsbml import *
+from bs4 import BeautifulSoup
+import requests
 
-reader = SBMLReader()
-
-class BioModels(object):
-    """Class to represent a BioModels model."""
-    def __init__(self, modelid):
-        self.modelid = modelid
-        document = reader.readSBML(modelid)
-        self.model = document.getModel()
-        self.reactions = [self.model.getReaction(x) for x in range(0, self.model.getNumReactions())]
+def biomodels(biomodels_id):
+    xml_soup = BeautifulSoup(requests.get("https://www.ebi.ac.uk/biomodels-main/download?mid=" + biomodels_id).content, 'xml')
+    reactions = xml_soup.listOfReactions.find_all('reaction')
+    rpks = []
+    for r in reactions:
+        rpk = []
+        reactant = r.listOfReactants.find('speciesReference')['species']
+        product = r.listOfProducts.find('speciesReference')['species']
+        kinetics = {}
+        for k in r.listOfParameters.find_all('parameter'):
+            kinetics[k['id']] = k['value']
+        rpks.append([reactant, product, kinetics])
+    return rpks
