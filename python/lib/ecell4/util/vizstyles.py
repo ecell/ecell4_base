6c92a81ed865d051cea42b36a736d8cf6979934c
Kazunari Kaizu <kwaizu@gmail.com>
2016-04-11 17:31:45 +0900

    #79 Restore a buffer after refleshing

--- python/lib/ecell4/util/vizstyles.py
+++ python/lib/ecell4/util/vizstyles.py
@@ -1,3 +1,6 @@
+import copy
+
+
 def periodic_color_scale(color_list):
     class PeriodicColorScale:
         """
@@ -18,11 +21,13 @@ def periodic_color_scale(color_list):
 
             """
             self.__config = config
-            self.__buffer = self.COLORS[:]
+            self.__buffer = copy.copy(self.COLORS)
 
             for color in self.__config.values():
                 if color in self.__buffer:
                     self.__buffer.remove(color)
+            if len(self.__buffer) == 0:
+                self.__buffer = copy.copy(self.COLORS)
 
         def get_color(self, name):
             """
@@ -37,14 +42,13 @@ def periodic_color_scale(color_list):
             if self.__config.get(name) is None:
                 self.__config[name] = self.__buffer.pop(0)
                 if len(self.__buffer) == 0:
-                    self.__buffer = self.COLORS[:]
+                    self.__buffer = copy.copy(self.COLORS)
 
             return self.__config[name]
 
         def get_config(self):
             """Get an instance of dic as the config of colors."""
             return self.__config
-
     return PeriodicColorScale
 
 elegans_color_scale = periodic_color_scale(
@@ -57,4 +61,4 @@ matplotlib_color_scale = periodic_color_scale(
 default_color_scale = elegans_color_scale
 
 attractive_mpl_color_scale = periodic_color_scale(
-    [(0.2980392156862745, 0.4470588235294118, 0.6901960784313725), (0.3333333333333333, 0.6588235294117647, 0.40784313725490196), (0.7686274509803922, 0.3058823529411765, 0.3215686274509804), (0.5058823529411764, 0.4470588235294118, 0.6980392156862745), (0.8, 0.7254901960784313, 0.4549019607843137), (0.39215686274509803, 0.7098039215686275, 0.803921568627451)])  # originally from the seaborn color palette
+        [(0.2980392156862745, 0.4470588235294118, 0.6901960784313725), (0.3333333333333333, 0.6588235294117647, 0.40784313725490196), (0.7686274509803922, 0.3058823529411765, 0.3215686274509804), (0.5058823529411764, 0.4470588235294118, 0.6980392156862745), (0.8, 0.7254901960784313, 0.4549019607843137), (0.39215686274509803, 0.7098039215686275, 0.803921568627451)])  # originally from the seaborn color palette
