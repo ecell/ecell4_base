fae519a4192f1cd33c086cf53f704bbd3a3e4160
Kozo Nishida <kozo.nishida@gmail.com>
2016-06-09 03:17:02 +0900

    loop through all pathways

--- python/lib/ecell4/extra/ecocyc.py
+++ python/lib/ecell4/extra/ecocyc.py
@@ -8,12 +8,13 @@ class EcocycDataSource(object):
 if __name__ == '__main__':
     from ecell4 import *
     pgdb = EcocycDataSource().pgdb
-#    for p in pgdb.all_pathways():
-    p = pgdb.all_pathways()[3]
+
     with reaction_rules():
-        for g in pgdb.genes_of_pathway(p):
-            for p in pgdb.all_products_of_gene(g):
-                ~_eval(g) > _eval(p) | 3
+        for p in pgdb.all_pathways():
+            for g in pgdb.genes_of_pathway(p):
+                for p in pgdb.all_products_of_gene(g):
+                    ~_eval(g) > _eval(p) | 3
+
     m = get_model()
     for s in m.list_species():
         print(s.serial())
