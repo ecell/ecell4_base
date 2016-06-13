a23643fd3dab5909825cf6c0e9a7a3d5da30dd1d
Kozo Nishida <knishida@riken.jp>
2016-06-13 23:19:49 +0900

    #87 add Uniprot RDF DataSource

---
+++ python/lib/ecell4/extra/uniprot.py
@@ -0,0 +1,38 @@
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+class UniprotDataSource(object):
+    def __init__(self):
+        self.sparql = SPARQLWrapper("http://sparql.uniprot.org/sparql/")
+
+    def create_species(self, taxon, model):
+        self.sparql.setQuery("""
+
+        PREFIX up:<http://purl.uniprot.org/core/> 
+        PREFIX taxon:<http://purl.uniprot.org/taxonomy/> 
+        PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
+        PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#> 
+        SELECT ?protein
+        WHERE
+        {
+        	?protein a up:Protein .
+        	?protein up:organism ?organism . 
+        	{
+        		?protein up:organism taxon:""" + taxon + """.
+        	}
+        }
+
+        """)
+        self.sparql.setReturnFormat(JSON)
+        results = self.sparql.query().convert()
+        
+        for s in results['results']['bindings']:
+            uniprot_id = s['protein']['value']
+            print(uniprot_id)
+            #model.add_species_attribute(Species(uniprot_id))
+
+if __name__ == '__main__':
+    from ecell4 import *
+    m1 = NetworkModel()
+    ds = UniprotDataSource()
+    ds.create_species('83333', m1)
+
