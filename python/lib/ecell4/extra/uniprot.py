953ac023bdbab47a02bce65dce833fd7f41c1f90
Kozo Nishida <knishida@riken.jp>
2016-06-15 03:30:09 +0900

    replace target taxon from ecoli to yeast, and add genename attribute to Species

--- python/lib/ecell4/extra/uniprot.py
+++ python/lib/ecell4/extra/uniprot.py
@@ -7,19 +7,20 @@ class UniprotDataSource(object):
     def create_species(self, taxon, model):
         self.sparql.setQuery("""
 
-        PREFIX up:<http://purl.uniprot.org/core/> 
-        PREFIX taxon:<http://purl.uniprot.org/taxonomy/> 
-        PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
-        PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#> 
-        SELECT ?protein
-        WHERE
-        {
-        	?protein a up:Protein .
-        	?protein up:organism ?organism . 
-        	{
-        		?protein up:organism taxon:""" + taxon + """.
-        	}
-        }
+PREFIX up:<http://purl.uniprot.org/core/>
+PREFIX taxon:<http://purl.uniprot.org/taxonomy/>
+PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#>
+PREFIX skos:<http://www.w3.org/2004/02/skos/core#>
+SELECT ?protein ?genename
+WHERE
+{
+    ?protein a up:Protein .
+    ?protein up:organism ?organism .
+    ?protein up:organism taxon:""" + taxon + """ .
+    ?protein up:encodedBy ?gene .
+    ?gene skos:prefLabel ?genename .
+}
 
         """)
         self.sparql.setReturnFormat(JSON)
@@ -28,14 +29,18 @@ class UniprotDataSource(object):
         for s in results['results']['bindings']:
             uniprot_uri = s['protein']['value']
             uniprot_id = uniprot_uri.split("/")[-1]
-            #print(uniprot_id)
+            uniprot_genename = s['genename']['value']
             s = Species(uniprot_id)
+#            if s.serial() == uniprot_id:
+#                print(uniprot_id)
+#            else:
             s.set_attribute("identifiers.org", "uniprot")
+            s.set_attribute("genename", uniprot_genename)
             model.add_species_attribute(s)
 
 if __name__ == '__main__':
     from ecell4 import *
     m1 = NetworkModel()
     ds = UniprotDataSource()
-    ds.create_species('83333', m1)
+    ds.create_species('4932', m1)
 
