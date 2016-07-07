a7c1185b7f116292970c6a6c1b4a9c8913deef95
Kazunari Kaizu <kwaizu@gmail.com>
2016-07-07 19:29:00 +0900

    #83 Add a new ensemble_simulations tentatively

--- python/lib/ecell4/extra/ensemble.py
+++ python/lib/ecell4/extra/ensemble.py
@@ -96,21 +96,90 @@ def run_sge(target, jobs, n=1, path='.', delete=True, environ={}):
 
     return retval
 
+#XXX:
+#XXX:
+#XXX:
+
+def singlerun(job, job_id, task_id):
+    import ecell4
+    import ecell4.util
+    import ecell4.util.simulation
+    job.update({'return_type': 'array'})
+    solver = job.pop('solver')
+    rng = ecell4.GSLRandomNumberGenerator()
+    rng.seed(task_id)
+    solver = ecell4.util.simulation.get_factory(solver, rng)
+    data = ecell4.util.run_simulation(**job)
+    # data = ecell4.util.run_simulation(return_type='array', **job)
+    return data
+
+import ecell4.util.decorator
+import ecell4.ode
+
+def ensemble_simulations(
+    t, y0={}, volume=1.0, model=None, solver='ode', species_list=None, structures={},
+    is_netfree=False, without_reset=False,
+    n=1, nproc=1, env=None):
+    """
+    """
+
+    if not isinstance(solver, str):
+        raise ValueError('Argument "solver" must be a string.')
+
+    if model is None:
+        model = ecell4.util.decorator.get_model(is_netfree, without_reset)
+
+    if isinstance(model, ecell4.ode.ODENetworkModel):
+        raise ValueError('A model with ratelaws is not supported yet.')
+
+    if species_list is None:
+        raise ValueError('Argument "species_list" must be given.')
+
+    jobs = [{'t': t, 'y0': y0, 'volume': volume, 'model': model, 'solver': solver, 'species_list': species_list, 'structures': structures}]
+
+    if env is None or env.lower() == "serial":
+        retval = run_serial(singlerun, jobs, n=n)
+    elif env.lower() == "sge":
+        retval = run_sge(singlerun, jobs, n=n)
+    elif env.lower() == "multiprocessing":
+        environ = {'LD_LIBRARY_PATH': '/home/kaizu/lily_kaizu/src/ecell4/local/lib', 'PYTHONPATH': '/home/kaizu/lily_kaizu/src/ecell4/local/lib/python3.4/site-packages'}
+        retval = run_multiprocessing(singlerun, jobs, n=n, environ=environ)
+    else:
+        raise ValueError(
+            'Argument "env" must be one of "serial", "multiprocessing" and "sge"')
+
+    return retval
 
 if __name__ == "__main__":
-    def singlerun(job, job_id=0, task_id=0):
-        import ecell4
-        print("Hi, I'm in local!")
-        print("My job id is {:d}, and my task id is {:d}.".format(job_id, task_id))
-        print("My job is {:s}.".format(str(job)))
-        return job['x'] + job['y']
+    # def myrun(job, job_id=0, task_id=0):
+    #     import ecell4
+    #     print("Hi, I'm in local!")
+    #     print("My job id is {:d}, and my task id is {:d}.".format(job_id, task_id))
+    #     print("My job is {:s}.".format(str(job)))
+    #     return job['x'] + job['y']
 
-    jobs = [{'x': i, 'y': i ** 2} for i in range(1, 4)]
-    print(run_serial(singlerun, jobs, n=2))
-    print(run_multiprocessing(singlerun, jobs, n=2))
+    # jobs = [{'x': i, 'y': i ** 2} for i in range(1, 4)]
+    # print(run_serial(myrun, jobs, n=2))
+    # print(run_multiprocessing(myrun, jobs, n=2))
 
-    # environ = {'LD_LIBRARY_PATH': '/home/kaizu/lily_kaizu/src/ecell4/local/lib', 'PYTHONPATH': '/home/kaizu/lily_kaizu/src/ecell4/local/lib/python3.4/site-packages'}
-    environ = {}
-    # print(run_sge(singlerun, jobs, n=2, delete=False, environ=environ))
-    print(run_sge(singlerun, jobs, n=2, environ=environ))
+    # # environ = {'LD_LIBRARY_PATH': '/home/kaizu/lily_kaizu/src/ecell4/local/lib', 'PYTHONPATH': '/home/kaizu/lily_kaizu/src/ecell4/local/lib/python3.4/site-packages'}
+    # environ = {}
+    # # print(run_sge(myrun, jobs, n=2, delete=False, environ=environ))
+    # print(run_sge(myrun, jobs, n=2, environ=environ))
 
+    from ecell4 import *
+    from ecell4.extra import ensemble
+
+    with reaction_rules():
+        A + B == C | (0.01, 0.3)
+
+    retval = ensemble.ensemble_simulations(10.0, {'C': 60}, species_list=['A', 'B', 'C'], solver='gillespie', n=10, env='sge')
+
+    import numpy
+
+    def concatenate(results):
+        return sum([numpy.array(data) for data in results]) / len(results)
+    retval = [concatenate(results) for results in retval]
+    print(retval)
+
+    numpy.savetxt('ens.dat', retval[0])
