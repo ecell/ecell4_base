ae20282d889369f8e1022f5692728c7fae766554
Kazunari Kaizu <kwaizu@gmail.com>
2016-07-07 16:34:03 +0900

    #83 Delete jobs before exiting

--- python/lib/ecell4/extra/sge.py
+++ python/lib/ecell4/extra/sge.py
@@ -10,6 +10,7 @@ import collections
 PREFIX = '/usr/bin'
 QSUB_CMD = os.path.join(PREFIX, 'qsub')
 QSTAT_CMD = os.path.join(PREFIX, 'qstat')
+QDEL_CMD = os.path.join(PREFIX, 'qdel')
 
 def get_logger():
     return logging.getLogger('sge')
@@ -83,38 +84,43 @@ def wait(jobids, interval=10):
         jobidstrs = [str(jobids)]
 
     dowait = True
-    while dowait:
-        output = subprocess.check_output([QSTAT_CMD])
-        output = output.decode('utf-8')
-        for line in output.split('\n'):
-            get_logger().debug(line)
+    try:
+        while dowait:
+            output = subprocess.check_output([QSTAT_CMD])
+            output = output.decode('utf-8')
+            for line in output.split('\n'):
+                get_logger().debug(line)
 
-        dowait = False
-        for line in output.split('\n'):
-            state = line.split()
-            if len(state) < 5 or state[0] not in jobidstrs:
-                continue
+            dowait = False
+            for line in output.split('\n'):
+                state = line.split()
+                if len(state) < 5 or state[0] not in jobidstrs:
+                    continue
 
-            #XXX: job-ID prior   name       user         state submit/start at     queue                          slots ja-task-ID
-            jobid = int(state[0])
-            if re.search(state[4], 'qwrt'):
+                #XXX: job-ID prior   name       user         state submit/start at     queue                          slots ja-task-ID
+                jobid = int(state[0])
+                if re.search(state[4], 'qwrt'):
+                    get_logger().info(
+                        'Job {:d} must be queued, running or being transferred'.format(jobid))
+                    dowait = True
+                    break
+                elif re.search(state[4], 'acuE'):
+                    get_logger().error('Job {:d} in error state'.format(jobid))
+                else:
+                    get_logger().error('Unknown state {:s}'.format(state[4]))
+
+            if dowait:
+                time.sleep(interval)
                 get_logger().info(
-                    'Job {:d} must be queued, running or being transferred'.format(jobid))
-                dowait = True
-                break
-            elif re.search(state[4], 'acuE'):
-                get_logger().error('Job {:d} in error state'.format(jobid))
-            else:
-                get_logger().error('Unknown state {:s}'.format(state[4]))
-
+                    "Waiting for jobids {:s} to finish".format(str(jobids)))
+    finally:
         if dowait:
-            time.sleep(interval)
-            get_logger().info(
-                "Waiting for jobids {:s} to finish".format(str(jobids)))
+            output = subprocess.check_output([QDEL_CMD] + jobidstrs)
+            get_logger().debug(output.strip())
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     n = 3
-    (jobid, name, filename) = singlerun("#!/bin/bash\ndate\nsleep 5\npwd\npuke", n)
+    (jobid, name, filename) = singlerun("#!/bin/bash\ndate\nsleep 5\npwd\necho \"puke\"", n)
     print(collect(jobid, name, n))
