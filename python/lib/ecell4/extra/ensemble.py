0e26920298c5147be756099a48f34543494cff1a
Kazunari Kaizu <kwaizu@gmail.com>
2016-07-13 17:01:03 +0900

    #83 Enable return_type

--- python/lib/ecell4/extra/ensemble.py
+++ python/lib/ecell4/extra/ensemble.py
@@ -9,11 +9,12 @@ import types
 import itertools
 import binascii
 import multiprocessing
+import copy
 
 import ecell4.extra.sge as sge
 
 def run_serial(target, jobs, n=1):
-    return [[target(job, i + 1, j + 1) for j in range(n)] for i, job in enumerate(jobs)]
+    return [[target(copy.copy(job), i + 1, j + 1) for j in range(n)] for i, job in enumerate(jobs)]
 
 def run_multiprocessing(target, jobs, n=1):
     def target_wrapper(f, end_send):
@@ -111,13 +112,16 @@ def singlerun(job, job_id, task_id):
 
 import ecell4.util.decorator
 import ecell4.util.simulation
+import ecell4.util.viz
 import ecell4.ode
 
 def ensemble_simulations(
     t, y0={}, volume=1.0, model=None, solver='ode', species_list=None, structures={},
     is_netfree=False, without_reset=False,
-    n=1, nproc=1, env=None):
+    return_type='matplotlib', opt_args=(), opt_kwargs={},
+    n=1, nproc=1, method=None, environ={}):
     """
+    observers=(), progressbar=0, rndseed=None,
     """
     if not isinstance(solver, str):
         raise ValueError('Argument "solver" must be a string.')
@@ -135,18 +139,94 @@ def ensemble_simulations(
 
     jobs = [{'t': t, 'y0': y0, 'volume': volume, 'model': model, 'solver': solver, 'species_list': species_list, 'structures': structures, 'myseed': myseed}]
 
-    if env is None or env.lower() == "serial":
+    if method is None or method.lower() == "serial":
         retval = run_serial(singlerun, jobs, n=n)
-    elif env.lower() == "sge":
-        environ = {'LD_LIBRARY_PATH': '/home/kaizu/lily_kaizu/src/ecell4/local/lib', 'PYTHONPATH': '/home/kaizu/lily_kaizu/src/ecell4/local/lib/python3.4/site-packages'}
+    elif method.lower() == "sge":
         retval = run_sge(singlerun, jobs, n=n, environ=environ)
-    elif env.lower() == "multiprocessing":
+    elif method.lower() == "multiprocessing":
         retval = run_multiprocessing(singlerun, jobs, n=n)
     else:
         raise ValueError(
-            'Argument "env" must be one of "serial", "multiprocessing" and "sge"')
+            'Argument "method" must be one of "serial", "multiprocessing" and "sge".')
 
-    return retval
+    if return_type == "array":
+        return retval
+
+    import numpy
+
+    class DummyObserver:
+
+        def __init__(self, inputs, species_list):
+            if len(inputs) == 0:
+                raise ValueError("No input was given.")
+
+            t = numpy.array(inputs[0], numpy.float64).T[0]
+            mean = sum([numpy.array(data, numpy.float64).T[1: ] for data in inputs])
+            mean /= len(inputs)
+            std = sum([(numpy.array(data, numpy.float64).T[1: ] - mean) ** 2
+                       for data in inputs])
+            std /= len(inputs)
+            std = numpy.sqrt(std)
+            # std = numpy.zeros(mean.shape)
+
+            self.__data = numpy.vstack([t, mean]).T
+            self.__error = numpy.vstack([t, std]).T
+            self.__species_list = [ecell4.Species(serial) for serial in species_list]
+
+        def targets(self):
+            return self.__species_list
+
+        def data(self):
+            return self.__data
+
+        def t(self):
+            return self.__data.T[0]
+
+        def error(self):
+            return self.__error
+
+    if return_type == "matplotlib":
+        if isinstance(opt_args, (list, tuple)):
+            ecell4.util.viz.plot_number_observer_with_matplotlib(
+                *itertools.chain([DummyObserver(inputs, species_list) for inputs in retval],
+                                 opt_args),
+                **opt_kwargs)
+        elif isinstance(opt_args, dict):
+            # opt_kwargs is ignored
+            ecell4.util.viz.plot_number_observer_with_matplotlib(
+                *[DummyObserver(inputs, species_list) for inputs in retval],
+                **opt_args)
+        else:
+            raise ValueError('opt_args [{}] must be list or dict.'.format(
+                repr(opt_args)))
+    elif return_type == "nyaplot":
+        if isinstance(opt_args, (list, tuple)):
+            ecell4.util.viz.plot_number_observer_with_nya(
+                *itertools.chain([DummyObserver(inputs, species_list) for inputs in retval],
+                                 opt_args),
+                **opt_kwargs)
+        elif isinstance(opt_args, dict):
+            # opt_kwargs is ignored
+            ecell4.util.viz.plot_number_observer_with_nya(
+                *[DummyObserver(inputs, species_list) for inputs in retval],
+                **opt_args)
+        else:
+            raise ValueError('opt_args [{}] must be list or dict.'.format(
+                repr(opt_args)))
+    elif return_type == "observer":
+        return [DummyObserver(inputs, species_list) for inputs in retval]
+    elif return_type == "dataframe":
+        import pandas
+        return [[
+            pandas.concat([
+                pandas.DataFrame(dict(Time=numpy.array(data).T[0],
+                                      Value=numpy.array(data).T[i + 1],
+                                      Species=serial))
+                for i, serial in enumerate(species_list)])
+            for data in inputs] for inputs in retval]
+    else:
+        raise ValueError(
+            'Invalid Argument "return_type" was given [{}].'.format(str(return_type)))
 
 
 if __name__ == "__main__":
@@ -172,15 +252,17 @@ if __name__ == "__main__":
     with reaction_rules():
         A + B == C | (0.01, 0.3)
 
+    environ = {'LD_LIBRARY_PATH': '/home/kaizu/lily_kaizu/src/ecell4/local/lib',
+               'PYTHONPATH': '/home/kaizu/lily_kaizu/src/ecell4/local/lib/python3.4/site-packages'}
     retval = ensemble.ensemble_simulations(
-        10.0, {'C': 60}, solver='gillespie',
-        n=20, env='sge')
+        10.0, {'C': 60}, solver='gillespie', return_type='matplotlib',
+        n=5, method='multiprocessing', environ=environ)
 
-    import numpy
+    # import numpy
 
-    def concatenate(results):
-        return sum([numpy.array(data) for data in results]) / len(results)
-    retval = [concatenate(results) for results in retval]
-    print(retval)
+    # def concatenate(results):
+    #     return sum([numpy.array(data) for data in results]) / len(results)
+    # retval = [concatenate(results) for results in retval]
+    # print(retval)
 
-    numpy.savetxt('ens.dat', retval[0])
+    # numpy.savetxt('ens.dat', retval[0])
