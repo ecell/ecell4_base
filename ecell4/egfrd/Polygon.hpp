8cd318af912e3b21c2dbab6d5f98e7773d809dd2
ToruNiina <niina.toru.68u@gmail.com>
2016-08-31 22:16:13 +0900

    add Polygon::emplace

--- ecell4/egfrd/Polygon.hpp
+++ ecell4/egfrd/Polygon.hpp
@@ -7,6 +7,9 @@
 
 struct Polygon : public ecell4::Shape
 {
+    typedef Real3 coordinate_type;
+    typedef FaceTrialgne<coordinate_type> face_type;
+
     Polygon(){}
     ~Polygon(){}
 
@@ -15,9 +18,14 @@ struct Polygon : public ecell4::Shape
     std::pair<std::vector<std::size_t>, std::pair<std::size_t, std::pair<Real, Real> > >
     get_intruder_faces(const Real3& pos, const Real range) const;
 
-    std::vector<FaceTrialgne<Real3> > faces;
+    void emplace(const boost::array<Real3>& vertices)
+    {
+        this->faces.push_back(FaceTriangle(vertices));
+    }
 
-// shape
+    std::vector<face_type> faces;
+
+// for shapes (not implemented yet)
     dimensiton_kind dimension() const {return THREE;}
     Real  is_inside(const Real3& coord) const;
     Real3 draw_position(boost::shared_ptr<RandomNumberGenerator>& rng) const;
