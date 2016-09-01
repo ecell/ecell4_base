636db77d9eae8d8249b0c7d5e8fabc6dcc0d0757
ToruNiina <niina.toru.68u@gmail.com>
2016-09-01 13:02:06 +0900

    add Polygon::apply_reflection

--- ecell4/egfrd/Polygon.cpp
+++ ecell4/egfrd/Polygon.cpp
@@ -3,13 +3,13 @@
 #include <limits>
 
 std::pair<std::vector<std::size_t>, std::pair<std::size_t, std::pair<Real, Real> > >
-Polygon::get_intruder_faces(const Real3& pos, const Real range) const
+Polygon::get_faces_within_radius(const coordinate_type& pos, const Real range) const
 {
     std::vector<std::size_t> intruders;
     std::size_t nearest_idx = std::numeric_limits<std::size_t>::max();
     std::pair<Real, Real> min_dist(std::numeric_limits<Real>::max(), 0.);
     std::size_t idx = 0;
-    for(typename std::vector<FaceTrialgne<Real3> >::const_iterator
+    for(std::vector<FaceTriangle<coordinate_type> >::const_iterator
             iter = faces.begin(); iter != faces.end(); ++iter)
     {
         std::pair<Real, Real> dist = distance(pos, *iter);
@@ -26,17 +26,85 @@ Polygon::get_intruder_faces(const Real3& pos, const Real range) const
     return std::make_pair(intruders, std::make_pair(nearest_idx, min_dist));
 }
 
-Real Polygon::is_inside(const Real3& coord) const
+Polygon::coordinate_type
+Polygon::apply_reflection(const coordinate_type& pos, const coordinate_type& displacement,
+                          const std::vector<std::size_t>& intruder_idxs) const
 {
-    throw ecell::NotImplemented("polygon::is_inside");
+    if(intruder_idxs.empty())
+    {
+        return pos + displacement;
+    }
+    else
+    {
+        coordinate_type begin = pos;
+        coordinate_type end = pos + displacement;
+        std::vector<std::pair<std::size_t, Real> > pierce;
+        pierce.reserve(intruder_idxs.size());
+        while(true)
+        {
+            pierce.clear();
+            for(std::vector<std::size_t>::const_iterator
+                iter = intruder_idxs.begin(); iter != intruder_idxs.end(); ++iter)
+            {
+                const std::pair<bool, coordinate_type> dist =
+                    is_pierce(begin, end, faces.at(*iter));
+                if(dist.first)
+                    pierce.push_back(std::make_pair(*iter, length(dist.second)));
+            }
+            if(pierce.empty()) break; // particle goes through no faces. return.
+
+            // check which face the particle collides first.
+            std::vector<std::size_t> first_collision_index_list;
+            Real minimum_distance = std::numeric_limits<Real>::max();
+            std::size_t multiplicity = 0;
+            for(std::vector<std::pair<std::size_t, Real> >::const_iterator
+                iter = pierce.begin(); iter != pierce.end(); ++iter)
+            {
+                if(iter->second == minimum_distance)
+                {
+                    first_collision_index_list.push_back(iter->first);
+                }
+                else if(iter->second < minimum_distance)
+                {
+                    first_collision_index_list.clear();
+                    first_collision_index_list.push_back(iter->first);
+                }
+            }
+
+            // update begin and end
+            if(first_collision_index_list.size() == 1)
+            {
+                coordinate_type tmp_begin = is_pierce(begin, end,
+                        faces.at(first_collision_index_list.first())).second;
+                end = reflect_plane(begin, end,
+                        faces.at(first_collision_index_list.first()));
+                begin = tmp_begin;
+            }
+            else if(first_collision_index_list.size() > 1)
+            {
+                throw ecell4::NotImplemented("particle collides several faces at once");
+            }
+            else
+            {
+                throw std::logic_error("app_reflect: never reach here");
+            }
+        }
+        return end;
+    }
 }
 
-Real3 Polygon::draw_position(boost::shared_ptr<RandomNumberGenerator>& rng) const
+Real Polygon::is_inside(const coordinate_type& coord) const
 {
-    throw ecell::NotImplemented("polygon::draw_position");
+    throw ecell4::NotImplemented("polygon::is_inside");
 }
 
-bool Polygon::test_AABB(const Real3& l, const Real3& u) const
+Polygon::coordinate_type
+Polygon::draw_position(boost::shared_ptr<ecell4::RandomNumberGenerator>& rng) const
 {
-    throw ecell::NotImplemented("polygon::test_AABB");
+    throw ecell4::NotImplemented("polygon::draw_position");
+}
+
+bool Polygon::test_AABB(const coordinate_type& l, const coordinate_type& u) const
+{
+    throw ecell4::NotImplemented("polygon::test_AABB");
 }
--- ecell4/egfrd/Polygon.hpp
+++ ecell4/egfrd/Polygon.hpp
@@ -2,13 +2,14 @@
 #define EGFRD_POLYGON
 
 #include <ecell4/core/Shape.hpp>
+#include <ecell4/core/Real3.hpp>
 #include "exceptions.hpp"
 #include "FaceTriangle.hpp"
 
 struct Polygon : public ecell4::Shape
 {
-    typedef Real3 coordinate_type;
-    typedef FaceTrialgne<coordinate_type> face_type;
+    typedef ecell4::Real3 coordinate_type;
+    typedef FaceTriangle<coordinate_type> face_type;
 
     Polygon(){}
     ~Polygon(){}
@@ -16,20 +17,26 @@ struct Polygon : public ecell4::Shape
     // nearest
     // (intruders, (idx, (distance, max_radius)))
     std::pair<std::vector<std::size_t>, std::pair<std::size_t, std::pair<Real, Real> > >
-    get_intruder_faces(const Real3& pos, const Real range) const;
+    get_faces_within_radius(const coordinate_type& pos, const Real range) const;
 
-    void emplace(const boost::array<Real3>& vertices)
+    void emplace(const boost::array<coordinate_type, 3>& vertices)
     {
-        this->faces.push_back(FaceTriangle(vertices));
+        this->faces.push_back(face_type(vertices));
     }
 
+    coordinate_type
+    apply_reflection(const coordinate_type& pos,
+                     const coordinate_type& displacement,
+                     const std::vector<std::size_t>& intruder_idxs) const;
+
+// data member
     std::vector<face_type> faces;
 
 // for shapes (not implemented yet)
-    dimensiton_kind dimension() const {return THREE;}
-    Real  is_inside(const Real3& coord) const;
-    Real3 draw_position(boost::shared_ptr<RandomNumberGenerator>& rng) const;
-    bool  test_AABB(const Real3& l, const Real3& u) const;
+    dimension_kind dimension() const {return THREE;}
+    Real  is_inside(const coordinate_type& coord) const;
+    coordinate_type draw_position(boost::shared_ptr<ecell4::RandomNumberGenerator>& rng) const;
+    bool  test_AABB(const coordinate_type& l, const coordinate_type& u) const;
 };
 
 #endif //EGFRD_POLYGON
