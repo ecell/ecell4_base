c31bc67ca816d3a0da92b6a119f0bac0ce8b85b8
Kazunari Kaizu <kwaizu@gmail.com>
2016-09-14 14:33:59 +0900

    Add the DataSource for BioGrid originally written by KeitaS

---
+++ python/lib/ecell4/extra/biogrid.py
@@ -0,0 +1,107 @@
+# coding: utf-8
+__author__ = "Keita Sasaki"
+
+import json
+try:
+    from urllib2 import Request, urlopen
+except ImportError:
+    from urllib.request import Request, urlopen
+
+
+class biogridDataSource(object):
+
+    histry = {}
+
+    def __init__(self, ACCESS_KEY):
+        self.ak = ACCESS_KEY
+
+
+    def query(self, ACCESS_POINT, geneList=[], organisms=[], cache=True):
+        options = ""
+        if geneList:
+            options += "&geneList={}".format("|".join(geneList))
+        if organisms:
+            options += "&taxid={}".format("|".join(organisms))
+
+        url = "http://webservice.thebiogrid.org/{AP}/?format=json&searchNames=true{OPTIONS}&accesskey={AK}".format(AP=ACCESS_POINT, OPTIONS=options, AK=self.ak)
+
+        if not cache or url not in self.histry.keys():
+            req = Request(url)
+            response = urlopen(req)
+            data = response.read().decode("utf-8")
+
+            if cache:
+                self.histry[url] = data
+
+        else:
+            data = self.histry[url]
+
+        return json.loads(data)
+
+
+    def organisms(self):
+        return self.query("organisms").values()
+
+
+    def orgmaker(self, org=[]):
+        organisms_reverse = dict([(value, key) for key, value in self.query("organisms").items()])
+        if org:
+            for i in range(len(org)):
+                if isinstance(org[i], int):
+                    org[i] = str(org[i])
+                elif isinstance(org[i], str):
+                    try:
+                        org[i] = organisms_reverse[org[i]]
+                    except KeyError:
+                        pass
+        return org
+
+
+    def interactions(self, geneList=[], org=[]):
+        organisms = self.query("organisms")
+        org = self.orgmaker(org)
+
+        querydata = self.query("interactions", geneList, org).values()
+        returnData = []
+        for i in querydata:
+            if i["OFFICIAL_SYMBOL_A"] in geneList:
+                dataDict = {"symA": {"name": i["OFFICIAL_SYMBOL_A"],
+                                     "biogridID": i["BIOGRID_ID_A"],
+                                     "organism": {"name": organisms[str(i["ORGANISM_A"])], "id": i["ORGANISM_A"]}},
+                            "symB": {"name": i["OFFICIAL_SYMBOL_B"],
+                                     "biogridID": i["BIOGRID_ID_B"],
+                                     "organism": {"name": organisms[str(i["ORGANISM_B"])], "id": i["ORGANISM_B"]}},
+                            "pubmedID": i["PUBMED_ID"]
+                            }
+            else:
+                dataDict = {"symA": {"name": i["OFFICIAL_SYMBOL_B"],
+                                     "biogridID": i["BIOGRID_ID_B"],
+                                     "organism": {"name": organisms[str(i["ORGANISM_B"])], "id": i["ORGANISM_B"]}},
+                            "symB": {"name": i["OFFICIAL_SYMBOL_A"],
+                                     "biogridID": i["BIOGRID_ID_A"],
+                                     "organism": {"name": organisms[str(i["ORGANISM_A"])], "id": i["ORGANISM_A"]}},
+                            "pubmedID": i["PUBMED_ID"]
+                            }
+            returnData.append(dataDict)
+
+        return returnData
+
+
+    def interactor(self, geneList=[], org=[]):
+        """
+        geneListは1つの要素であることを想定。
+        """
+        querydata = self.interactions(geneList, org)
+        returnData = {}
+        for i in querydata:
+            if not returnData.get(i["symB"]["name"]):
+                returnData[i["symB"]["name"]] = {"interactions": []}
+            returnData[i["symB"]["name"]]["interactions"].append(i)
+        return returnData
+
+
+if __name__ == "__main__":
+    ACCESSKEY = "528f82a103f7785ace2300dd1e2e161c"
+    interactions = biogridDataSource(ACCESSKEY).interactions(["MDM2"], [9606])
+    interactor = biogridDataSource(ACCESSKEY).interactor(["MDM2"])
+    print interactor.keys()
