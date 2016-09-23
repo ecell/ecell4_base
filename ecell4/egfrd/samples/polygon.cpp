9f3ba42bf2ae3612f30a356e54602cb6d3b5e5eb
ToruNiina <niina.toru.68u@gmail.com>
2016-09-23 14:43:36 +0900

    add outside-particle

--- ecell4/egfrd/samples/polygon.cpp
+++ ecell4/egfrd/samples/polygon.cpp
@@ -58,7 +58,7 @@ int main(int argc, char **argv)
     const ecell4::Real3 edge_lengths(L, L, L);
     const ecell4::Integer3 matrix_sizes(3, 3, 3);
     const ecell4::Real volume(L * L * L);
-    const ecell4::Integer N(32);
+    const ecell4::Integer N(60);
     const ecell4::Real kd(0.1), U(0.5);
     const ecell4::Real ka(kd * volume * (1 - U) / (U * U * N));
     const ecell4::Real k2(ka), k1(kd);
@@ -72,6 +72,10 @@ int main(int argc, char **argv)
     ecell4::Species sp1(
         std::string("C"), std::string("2.5"), std::string("1e3"));
     model->add_species_attribute(sp1);
+    ecell4::Species sp2(
+        std::string("N"), std::string("2.5"), std::string("1e3"));
+    model->add_species_attribute(sp2);
+ 
     // }}}
 
     // ReactionRules
@@ -102,17 +106,23 @@ int main(int argc, char **argv)
     {
         while(true)
         {
-            const ecell4::Real radius = rng->uniform(0, 15.0);
-            const ecell4::Real theta = rng->uniform(0, 2.0 * 3.14159265358979);
-            const ecell4::Real phi = rng->uniform(0, 2.0 * 3.14159265358979);
+//             const ecell4::Real radius = rng->uniform(0, 15.0);
+//             const ecell4::Real theta = rng->uniform(0, 2.0 * 3.14159265358979);
+//             const ecell4::Real phi = rng->uniform(0, 2.0 * 3.14159265358979);
             const ecell4::Real3 sphere_center(24.0, 24.0, 24.0);
-            const ecell4::Real3 dist(
-                    radius * sin(theta) * cos(phi),
-                    radius * sin(theta) * sin(phi),
-                    radius * cos(theta));
-            assert(length(dist) < 24.);
-
-            const ecell4::Particle newpart(ecell4::Species("C"), sphere_center + dist, 2.5, 1e3);
+//             const ecell4::Real3 dist(
+//                     radius * sin(theta) * cos(phi),
+//                     radius * sin(theta) * sin(phi),
+//                     radius * cos(theta));
+//             assert(length(dist) < 24.);
+            const ecell4::Real3 position(rng->uniform(0.0, 5e1), rng->uniform(0.0, 5e1), rng->uniform(0.0, 5e1));
+            ecell4::Particle newpart;
+            if(length(position - sphere_center) < 20.0)
+                newpart = ecell4::Particle(ecell4::Species("C"), position, 2.5, 1e3);
+            else if(length(position - sphere_center) > 24.5)
+                newpart = ecell4::Particle(ecell4::Species("N"), position, 2.5, 1e3);
+            else
+                continue;
             if(world->new_particle(newpart).second) break;
         }
     }
@@ -141,7 +151,7 @@ int main(int argc, char **argv)
     // EGFRDSimulator instance generated
     // {{{
     boost::shared_ptr<simulator_type> sim(
-        new simulator_type(world, model, 0.1));
+        new simulator_type(world, model, 1.0));
     // sim->paranoiac() = true;
     sim->initialize();
     // }}}
@@ -153,7 +163,8 @@ int main(int argc, char **argv)
     for (int i(0); i < 100; i++)
     {
         ecell4::Integer num_sp1 = world->num_molecules_exact(sp1);
-        std::cout << num_sp1 << std::endl;
+        ecell4::Integer num_sp2 = world->num_molecules_exact(sp2);
+        std::cout << num_sp1 + num_sp2 << std::endl;
         std::cout << "now t = " << sim->t() << std::endl;
         const std::vector<
             std::pair<world_type::particle_id_type, world_type::particle_type>
@@ -161,11 +172,28 @@ int main(int argc, char **argv)
         for(std::size_t i=0; i< particles.size(); ++i)
         {
             const ecell4::Real3 center(24.0, 24.0, 24.0);
-            assert(length(particles.at(i).second.position() - center) < 24.);
-            if(length(particles.at(i).second.position() - center) > 24.)
+            if(particles.at(i).second.sid() == "C")
             {
-                std::cerr << "particle goes out!" << std::endl;
+                if(length(particles.at(i).second.position() - center) > 24.)
+                {
+                    std::cerr << "radius = " << length(particles.at(i).second.position() - center) << std::endl;
+                    std::cerr << "it should be inside" << std::endl;
+//                     assert(false);
+                }
             }
+            else if(particles.at(i).second.sid() == "N")
+            {
+                if(length(particles.at(i).second.position() - center) < 24.)
+                {
+                    std::cerr << "radius = " << length(particles.at(i).second.position() - center) << std::endl;
+                    std::cerr << "it should be outside" << std::endl;
+//                     assert(false);
+                }
+            }
+//             if(length(particles.at(i).second.position() - center) > 24.)
+//             {
+//                 std::cerr << "particle goes out!" << std::endl;
+//             }
             std::cout << particles.at(i).second.sid() << " "
                 << particles.at(i).second.position()[0] << " "
                 << particles.at(i).second.position()[1] << " "
