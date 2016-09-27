4d731742ea6ccd10bf647ed777d74f9062fb7159
Toru Niina <niina.toru.68u@gmail.com>
2016-09-27 17:57:14 +0900

    add const to closest_point

--- ecell4/egfrd/TriangleOperation.hpp
+++ ecell4/egfrd/TriangleOperation.hpp
@@ -1,10 +1,13 @@
 #ifndef GFRD_POLYGON_TRIANGLE_OPERATION
 #define GFRD_POLYGON_TRIANGLE_OPERATION
 #include "Vector3Operation.hpp"
+#include "BarycentricCoordinate.hpp"
 #include "circular_iteration.hpp"
 #include <boost/array.hpp>
 #include <algorithm>
 #include <cassert>
+
+namespace ecell4{
 
 template<typename coordT>
 inline coordT centroid(const boost::array<coordT, 3>& vertices)
@@ -68,66 +71,71 @@ project_to_plane(const coordT& pos, const boost::array<coordT, 3>& vertices,
 }
 
 template<typename coordT>
-std::pair<typename scalar_type_helper<coordT>::type, // distance
-          typename scalar_type_helper<coordT>::type> // r of circle in triangle
-distance(const coordT& pos, const boost::array<coordT, 3>& vertices)
+coordT
+closest_point(const coordT& pos, const boost::array<coordT, 3>& vertices)
 {
     typedef typename scalar_type_helper<coordT>::type valueT;
     // this implementation is from Real-Time Collision Detection by Christer Ericson,
     // published by Morgan Kaufmann Publishers, (c) 2005 Elsevier Inc.
     // pp.141-142
 
-    coordT const& a = vertices[0];
-    coordT const& b = vertices[1];
-    coordT const& c = vertices[2];
+    const coordT a = vertices[0];
+    const coordT b = vertices[1];
+    const coordT c = vertices[2];
 
-    // Check if P in vertex region outside A
-    coordT ab = b - a;
-    coordT ac = c - a;
-    coordT ap = pos - a;
-    valueT d1 = dot_product(ab, ap);
-    valueT d2 = dot_product(ac, ap);
+    const coordT ab = b - a;
+    const coordT ac = c - a;
+    const coordT ap = pos - a;
+    const valueT d1 = dot_product(ab, ap);
+    const valueT d2 = dot_product(ac, ap);
     if (d1 <= 0.0 && d2 <= 0.0)
-        return std::make_pair(length(pos - a), 0.0); // barycentric coordinates (1,0,0)
-    // Check if P in vertex region outside B
-    coordT bp = pos - b;
-    valueT d3 = dot_product(ab, bp);
-    valueT d4 = dot_product(ac, bp);
+        return a;
+
+    const coordT bp = pos - b;
+    const valueT d3 = dot_product(ab, bp);
+    const valueT d4 = dot_product(ac, bp);
     if (d3 >= 0.0 && d4 <= d3)
-        return std::make_pair(length(pos - b), 0.0); // barycentric coordinates (0,1,0)
-    // Check if P in edge region of AB, if so return projection of P onto AB
-    valueT vc = d1*d4 - d3*d2;
+        return b;
+
+    const valueT vc = d1*d4 - d3*d2;
     if (vc <= 0.0 && d1 >= 0.0 && d3 <= 0.0)
     {
         valueT v = d1 / (d1 - d3);
-        return std::make_pair(length(a + ab * v- pos), 0.0); // barycentric coordinates (1-v,v,0)
+        return a + ab * v;
     }
-    // Check if P in vertex region outside C
-    coordT cp = pos - c;
-    valueT d5 = dot_product(ab, cp);
-    valueT d6 = dot_product(ac, cp);
-    if (d6 >= 0.0 && d5 <= d6)
-        return std::make_pair(length(c - pos), 0.0); // barycentric coordinates (0,0,1)
 
-    //Check if P in edge region of AC, if so return projection of P onto AC
-    valueT vb = d5*d2 - d1*d6;
+    const coordT cp = pos - c;
+    const valueT d5 = dot_product(ab, cp);
+    const valueT d6 = dot_product(ac, cp);
+    if (d6 >= 0.0 && d5 <= d6)
+        return c;
+
+    const valueT vb = d5*d2 - d1*d6;
     if (vb <= 0.0 && d2 >= 0.0 && d6 <= 0.0)
     {
-        valueT w = d2 / (d2 - d6);
-        return std::make_pair(length(a + ac * w - pos), 0.0); // barycentric coordinates (1-w,0,w)
+        const valueT w = d2 / (d2 - d6);
+        return a + ac * w;
     }
-    // Check if P in edge region of BC, if so return projection of P onto BC
-    valueT va = d3*d6 - d5*d4;
+
+    const valueT va = d3*d6 - d5*d4;
     if (va <= 0.0 && (d4 - d3) >= 0.0 && (d5 - d6) >= 0.0)
     {
-        valueT w = (d4 - d3) / ((d4 - d3) + (d5 - d6));
-        return std::make_pair(length(b + (c - b) * w - pos), 0.0); // barycentric coordinates (0,1-w,w)
+        const valueT w = (d4 - d3) / ((d4 - d3) + (d5 - d6));
+        return b + (c - b) * w;
     }
-    // P inside face region. Compute Q through its barycentric coordinates (u,v,w)
-    valueT denom = 1.0 / (va + vb + vc);
-    valueT v = vb * denom;
-    valueT w = vc * denom;
-    return std::make_pair(length(a + ab * v + ac * w - pos), 0.0); // = u*a + v*b + w*c, u = va * denom = 1.0f-v-w
+
+    const valueT denom = 1.0 / (va + vb + vc);
+    const valueT v = vb * denom;
+    const valueT w = vc * denom;
+    return a + ab * v + ac * w;
+}
+
+template<typename coordT>
+std::pair<typename scalar_type_helper<coordT>::type, // distance
+          typename scalar_type_helper<coordT>::type> // r of circle in triangle
+distance(const coordT& pos, const boost::array<coordT, 3>& vertices)
+{
+    return std::make_pair(length(closest_point(pos - vertices) - pos), 0.);
 }
 
 template<typename coordT>
@@ -170,9 +178,11 @@ is_pierce(const coordT& begin, const coordT& end,
     bary[0] = u;
     bary[1] = v;
     bary[2] = w;
-    const coordT intersect = barycentric_to_absolute(bary, vertices);
+    const coordT intersect = make_absolute(bary, FaceTriangle<coordT>(vertices));
 
     return std::make_pair(true, intersect);
 }
 
+
+}
 #endif /* GFRD_POLYGON_TRIANGLE */
