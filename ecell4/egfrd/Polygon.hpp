ffa98f5ec3c8695c16709b01332d13fd56e486a9
Toru Niina <niina.toru.68u@gmail.com>
2016-10-11 14:12:23 +0900

    change definition of Polygon::apply_reflection

--- ecell4/egfrd/Polygon.hpp
+++ ecell4/egfrd/Polygon.hpp
@@ -14,6 +14,7 @@ struct Polygon : public ecell4::Shape
 {
     typedef coordT coordinate_type;
     typedef FaceTriangle<coordinate_type> face_type;
+    typedef std::size_t face_id_type;
 
     Polygon(){}
     ~Polygon(){}
@@ -28,10 +29,14 @@ struct Polygon : public ecell4::Shape
         this->faces.push_back(face_type(vertices));
     }
 
-    coordinate_type
+    // assume the segment collides a face at first and
+    // return pairof(new begin, new end) and id of collided face
+    std::pair<std::pair<coordinate_type, coordinate_type>, face_id_type>
     apply_reflection(const coordinate_type& pos, const coordinate_type& displacement,
-                     const std::vector<std::size_t>& intruder_idxs,
-                     const coordinate_type& edge_lengths) const;
+                     const std::vector<face_id_type>& intruder_faces,
+                     const face_id_type ignore_face) const;
+
+    static face_id_type make_nonsence_id(){return std::numeric_limits<std::size_t>::max();}
 
 // data member
     std::vector<face_type> faces;
@@ -69,118 +74,56 @@ Polygon<coordT>::get_faces_within_radius(const coordinate_type& pos, const Real 
 }
 
 template<typename coordT>
-typename Polygon<coordT>::coordinate_type
+std::pair<std::pair<typename Polygon<coordT>::coordinate_type,
+                    typename Polygon<coordT>::coordinate_type>,
+          typename Polygon<coordT>::face_id_type>
 Polygon<coordT>::apply_reflection(
         const coordinate_type& pos, const coordinate_type& displacement,
-        const std::vector<std::size_t>& intruder_idxs,
-        const coordinate_type& edge_lengths/* = unit cell of periodic boundary*/) const
+        const std::vector<face_id_type>& intruder_faces,
+        const face_id_type ignore_face) const
 {
-    const ecell4::AABB unitcell(coordinate_type(0.,0.,0.), edge_lengths);
-    coordinate_type begin = pos;
-    coordinate_type end   = pos + displacement;
-    std::pair<bool, Real> collide_world = unitcell.intersect_ray(pos, displacement);
-    if(collide_world.first)
-    {// second is just a parameter
-        collide_world.second = collide_world.second * length(displacement);
-    }
-    else
+    const coordinate_type end = pos + displacement;
+    if(intruder_idxs.empty())
+        return std::make_pair(std::make_pair(end, end), make_nonsence_id());
+
+    bool collide_face = false;
+    coordinate_type next_begin = end;
+    face_id_type first_collide_face_idx = make_nonsence_id;
+    Real         first_collide_distance = length(displacement);
+
+    for(typename std::vector<face_id_type>::const_iterator
+        iter = intruder_faces.begin(); iter != intruder_faces.end(); ++iter)
     {
-        collide_world.second = std::numeric_limits<Real>::infinity();
-    }
+        if(*iter == ignore_face) continue;
 
-    if(intruder_idxs.empty() && not collide_world.first)
-        return end;
+        const std::pair<bool, coordinate_type> test_result =
+            is_pierce(pos, end, faces.at(*iter));
 
-    std::vector<std::size_t> intruder_faces = intruder_idxs;
-    std::size_t ignore_idx = std::numeric_limits<std::size_t>::max();
-
-    while(true)
-    {
-        bool        collide_face = false;
-        std::size_t first_collide_face_idx = std::numeric_limits<std::size_t>::max();
-        Real        first_collide_distance = collide_world.second;// inf or dist to unit cell
-
-        for(typename std::vector<std::size_t>::const_iterator
-            iter = intruder_faces.begin(); iter != intruder_faces.end(); ++iter)
+        if(test_result.first)
         {
-            if(*iter == ignore_idx) continue;
+            const Real dist_to_face = length(test_result.second - pos);
 
-            const std::pair<bool, coordinate_type> dist =
-                is_pierce(begin, end, faces.at(*iter));
-            if(dist.first)
+            if(dist_to_face < first_collide_distance)
             {
-                const Real dist_to_face = length(dist.second - begin);
-                if(dist_to_face == collide_world.second)
-                {
-                    throw ecell4::NotImplemented("collide 2 object at the same time");
-                }
-                else if(dist_to_face < first_collide_distance)
-                {
-                    collide_face = true;
-                    first_collide_face_idx = *iter;
-                    first_collide_distance = dist_to_face;
-                }
+                collide_face = true;
+                first_collide_face_idx = *iter;
+                first_collide_distance = dist_to_face;
+                next_begin = test_result.second;
+            }
+            else if(dist_to_face == collide_world.second)
+            {
+                throw ecell4::NotImplemented("collide 2 object at the same time");
             }
         }
-        if(not collide_face && not collide_world.first) return end;
-
-        if(not collide_face)
-        {// collide unit cell first
-
-            const coordinate_type upto_collision =
-                (end - begin) * (collide_world.second / length(end - begin));
-            coordinate_type move_into_unit_cell;
-            coordinate_type tmp_begin = begin + upto_collision;
-
-            // XXX omg!!!
-            if(std::abs(tmp_begin[0]) < 1e-12)
-                move_into_unit_cell = coordinate_type(edge_lengths[0], 0., 0.);
-            else if(std::abs(tmp_begin[0] - edge_lengths[0]) < 1e-12)
-                move_into_unit_cell = coordinate_type(-1. * edge_lengths[0], 0., 0.);
-            else if(std::abs(tmp_begin[1]) < 1e-12)
-                move_into_unit_cell = coordinate_type(0., edge_lengths[1], 0.);
-            else if(std::abs(tmp_begin[1] - edge_lengths[1]) < 1e-12)
-                move_into_unit_cell = coordinate_type(0., -1. * edge_lengths[1], 0.);
-            else if(std::abs(tmp_begin[2]) < 1e-12)
-                move_into_unit_cell = coordinate_type(0., 0., edge_lengths[2]);
-            else if(std::abs(tmp_begin[2] - edge_lengths[2]) < 1e-12)
-                move_into_unit_cell = coordinate_type(0., 0., -1. * edge_lengths[2]);
-            else throw std::logic_error("never reach here");
-
-            begin = (tmp_begin + move_into_unit_cell);
-            end   = (end       + move_into_unit_cell);
-
-            const coordinate_type new_displ = end - begin;
-            const Real length_new_displ = length(new_displ);
-            collide_world = unitcell.intersect_ray(begin, new_displ);
-            if(collide_world.first)
-                collide_world.second = collide_world.second * length_new_displ;
-            else
-                collide_world.second = std::numeric_limits<Real>::infinity();
-
-            intruder_faces = (this->get_faces_within_radius(begin, length_new_displ)).first;
-            ignore_idx = std::numeric_limits<std::size_t>::max();
-        }
-        else
-        {// collide certain face first
-
-            const coordinate_type tmp_begin = is_pierce(begin, end,
-                    faces.at(first_collide_face_idx)).second;
-            const coordinate_type tmp_end = reflect_plane(begin, end,
-                    faces.at(first_collide_face_idx));
-
-            begin = tmp_begin;
-            end   = tmp_end;
-            ignore_idx = first_collide_face_idx;
-
-            const coordinate_type new_displ = end - begin;
-            collide_world = unitcell.intersect_ray(begin, new_displ);
-            if(collide_world.first)
-                collide_world.second = collide_world.second * length(new_displ);
-            else
-                collide_world.second = std::numeric_limits<Real>::infinity();
-        }
     }
+    if(not collide_face)
+        return std::make_pair(std::make_pair(end, end), make_nonsence_id());
+
+    const coordinate_type next_end =
+        reflect_plane(pos, end, faces.at(first_collide_face_idx));
+
+    return std::make_pair(
+            std::make_pair(next_begin, next_end), first_collide_face_idx);
 }
 
 template<typename coordT>
