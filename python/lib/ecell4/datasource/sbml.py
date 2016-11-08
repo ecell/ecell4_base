4260296517625a7c45109b5b6864580435294ce2
Kazunari Kaizu <kwaizu@gmail.com>
2016-11-08 18:56:21 +0900

    refactor: Remove old lines

--- python/lib/ecell4/datasource/sbml.py
+++ python/lib/ecell4/datasource/sbml.py
@@ -114,6 +114,23 @@ class BioModelsDataSource(SBMLDataSource):
         #import pdb; pdb.set_trace()
         self.read_from_string(xml)
 
+# from bs4 import BeautifulSoup
+# import requests
+# 
+# def biomodels(biomodels_id):
+#     xml_soup = BeautifulSoup(requests.get("https://www.ebi.ac.uk/biomodels-main/download?mid=" + biomodels_id).content, 'xml')
+#     reactions = xml_soup.listOfReactions.find_all('reaction')
+#     rpks = []
+#     for r in reactions:
+#         rpk = []
+#         reactant = r.listOfReactants.find('speciesReference')['species']
+#         product = r.listOfProducts.find('speciesReference')['species']
+#         kinetics = {}
+#         for k in r.listOfParameters.find_all('parameter'):
+#             kinetics[k['id']] = k['value']
+#         rpks.append([reactant, product, kinetics])
+#     return rpks
+
 
 if __name__ == '__main__':
     import sys
