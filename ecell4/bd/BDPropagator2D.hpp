c0b6379dd4fb702100c70640a2a2cb0c52841cd7
ToruNiina <niina.toru.68u@gmail.com>
2016-12-27 19:01:20 +0900

    add impl of BDPropagator2D

--- ecell4/bd/BDPropagator2D.hpp
+++ ecell4/bd/BDPropagator2D.hpp
@@ -1,12 +1,12 @@
-#ifndef __ECELL4_BD_BD_PROPAGATOR_HPP
-#define __ECELL4_BD_BD_PROPAGATOR_HPP
+#ifndef __ECELL4_BD_BD_PROPAGATOR_2D_HPP
+#define __ECELL4_BD_BD_PROPAGATOR_2D_HPP
 
 #include <ecell4/core/RandomNumberGenerator.hpp>
 #include <ecell4/core/Model.hpp>
 
-#include "functions3d.hpp"
 #include "BDPropagator.hpp"
-
+#include "BDPolygon.hpp"
+#include "rotate_vector.hpp"
 
 namespace ecell4
 {
@@ -19,16 +19,18 @@ class BDPropagator2D
 public:
 
     typedef ReactionInfo reaction_info_type;
+    typedef BDPolygon polygon_type;
+    typedef typename BDPolygon::face_type face_type;
 
 public:
 
-    BDPropagator(
-        Model& model, BDWorld& world, BDPolygon& poly, RandomNumberGenerator& rng, const Real& dt,
+    BDPropagator2D(
+        Model& model, BDWorld& world, RandomNumberGenerator& rng, const Real& dt,
         std::vector<std::pair<ReactionRule, reaction_info_type> >& last_reactions)
-        : model_(model), world_(world), poly_(poly), rng_(rng), dt_(dt),
+        : model_(model), world_(world), poly_(world.polygon()), rng_(rng), dt_(dt),
         last_reactions_(last_reactions), max_retry_count_(1)
     {
-        queue_ = poly_.list_particles();
+        queue_ = world.list_2D_particles();
         shuffle(rng_, queue_);
     }
 
@@ -72,9 +74,15 @@ public:
 
     void remove_particle(const ParticleID& pid);
 
-    inline Real3 draw_displacement(const Particle& particle)
-    {// XXX!
-        return random_displacement_2d(rng(), dt(), particle.D());
+    inline Real3 draw_displacement(const Particle& particle, const Real3& normal)
+    {
+//      return random_displacement_3d(rng(), dt(), particle.D());
+        const Real sigma = std::sqrt(2 * particle.D() * dt());
+        const Real3 dr(rng_.gaussian(sigma), rng_.gaussian(sigma), 0);
+
+        const Real theta = normal[2];
+        const Real3 axis(normal[1], -normal[0], 0.);
+        return rotate(theta, axis, dr);
     }
 
     inline Real3 draw_ipv(const Real& sigma, const Real& t, const Real& D)
@@ -86,7 +94,7 @@ protected:
 
     Model& model_;
     BDWorld& world_;
-    BDPolygon& poly_;
+    BDPolygon& poly_; // XXX additional
     RandomNumberGenerator& rng_;
     Real dt_;
     std::vector<std::pair<ReactionRule, reaction_info_type> >& last_reactions_;
