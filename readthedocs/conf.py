9e61eca1d24dbbb806259314e1cac006e73adee0
Kozo Nishida <knishida@riken.jp>
2017-01-04 18:23:16 +0900

    chore: update version

--- readthedocs/conf.py
+++ readthedocs/conf.py
@@ -11,8 +11,8 @@ project = u'E-Cell'
 copyright = u'2015-, E-Cell project'
 author = u'Kazunari Kaizu'
 
-version = '4.0.7'
-release = '4.0.7'
+version = '4.0.8'
+release = '4.0.8'
 
 language = None
 
