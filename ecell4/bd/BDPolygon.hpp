bbcc95e7e905212616cd000b0c294b3685de58d7
ToruNiina <niina.toru.68u@gmail.com>
2017-01-17 13:45:28 +0900

    add barycentric and polygon_traits

--- ecell4/bd/BDPolygon.hpp
+++ ecell4/bd/BDPolygon.hpp
@@ -1,14 +1,60 @@
-#ifndef BD_POLYGON
-#define BD_POLYGON
+#ifndef ECELL_BD_POLYGON
+#define ECELL_BD_POLYGON
+#include "Barycentric.hpp"
 #include <ecell4/core/Triangle.hpp>
 #include <ecell4/core/get_mapper_mf.hpp>
 #include <ecell4/core/hash.hpp>
 #include <utility>
 #include <functional>
 #include <vector>
+#include <stdint.h>
 
+namespace ecell4
+{
+namespace bd
+{
+
+struct polygon_traits
+{
+    typedef face_type         Triangle;
+    typedef face_id_type      std::size_t;
+    typedef vertex_index_type uint32_t;
+    typedef edge_index_type   uint32_t;
+    typedef vertex_id_type    std::pair<face_id_type, vertex_index_type>;
+    typedef edge_id_type      std::pair<face_id_type, edge_index_type>;
+
+    static vertex_id_type
+    make_vertex_id(face_id_type fid, vertex_index_type vidx)
+    {
+        return std::make_pair(fid, vidx);
+    }
+
+    static edge_id_type
+    make_edge_id(face_id_type fid, edge_index_type eidx)
+    {
+        return std::make_pair(fid, eidx);
+    }
+
+    static edge_id_type vtoe(const vertex_id_type vid){return vid;}
+    static vertex_id_type etov(const edge_id_type eid){return eid;}
+
+    static face_id_type get_face_id(const vertex_id_type vid){return vid.first;}
+//    static face_id_type get_face_id(const edge_id_type eid){return eid.first;}
+    static vertex_id_type get_vertex_index(const vertex_id_type& vid)
+    {
+        return vid.second;
+    }
+    static edge_id_type get_edge_index(const edge_id_type& eid)
+    {
+        return eid.second;
+    }
+};
+
+} // bd
+} // ecell4
+
+// define hash for vertex/edge_id_type
 ECELL4_DEFINE_HASH_BEGIN()
-
 template<>
 struct hash<std::pair<std::size_t, uint32_t> >
 {
@@ -20,7 +66,6 @@ struct hash<std::pair<std::size_t, uint32_t> >
                hash<uint32_t>()(val.second);
     }
 };
-
 ECELL4_DEFINE_HASH_END()
 
 namespace ecell4
@@ -28,59 +73,61 @@ namespace ecell4
 namespace bd
 {
 
-struct BDPolygon
+// template<typename T_traits>
+class BDPolygon
 {
   public:
-    typedef Triangle face_type;
-    typedef std::vector<face_type> face_container_type;
-    typedef std::size_t face_id_type;
-    typedef std::pair<face_id_type, uint32_t> vertex_id_type;
-    typedef std::pair<face_id_type, uint32_t> edge_id_type;
+    typedef polygon_traits traits_type; // hard-coded
+    typedef typename traits_type::face_type         face_type;
+    typedef typename traits_type::face_id_type      face_id_type;
+    typedef typename traits_type::vertex_index_type vertex_id_type;
+    typedef typename traits_type::edge_index_type   edge_id_type;
+    typedef typename traits_type::vertex_id_type    vertex_id_type;
+    typedef typename traits_type::edge_id_type      edge_id_type;
+
+    typedef std::vector<face_type>      face_container_type;
     typedef std::vector<vertex_id_type> vertex_id_list;
     typedef typename utils::get_mapper_mf<edge_id_type, edge_id_type>::type
         edge_pair_type;
     typedef typename utils::get_mapper_mf<vertex_id_type, vertex_id_list>::type
         vertex_group_type;
 
+    typedef std::pair<Real3, face_id_type> surface_position_type;
+    typedef Barycentric<Real> barycentric_type;
+
   public:
     BDPolygon(){}
     ~BDPolygon(){}
 
-    void add_face(const face_type& face)
+    face_id_type add_face(const face_type& face)
     {
+        const face_id_type id = faces_.size();
         faces_.push_back(face);
+        return id;
     }
 
     void detect_connectivity();
 
-    std::size_t num_faces() const
-    {
-        return faces_.size();
-    }
+    edge_id_type const&
+    connecting_edge(const edge_id_type& eid){return edge_pairs_[eid];}
 
-    edge_id_type const& connecting_edge(const edge_id_type& eid)
-    {
-        return edge_pairs_[eid];
-    }
-
-    vertex_id_list const& connecting_vertices(const vertex_id_type& vid)
-    {
-        return vertex_groups_[vid];
-    }
+    vertex_id_list const&
+    connecting_vertices(const vertex_id_type& vid){return vertex_groups_[vid];}
 
     std::pair<bool, uint32_t>
     is_connected(const face_id_type& lhs, const face_id_type& rhs) const;
+
     std::pair<bool, uint32_t>
     is_share_vertex(const face_id_type& lhs, const face_id_type& rhs) const;
 
     Real distance(const std::pair<Real3, face_id_type>& lhs,
                   const std::pair<Real3, face_id_type>& rhs) const;
 
-
     std::pair<std::pair<Real3, face_id_type>, Real3>
-    move_next_face(
-        const std::pair<Real3, face_id_type>& pos, const Real3& disp) const;
+    move_next_face(const std::pair<Real3, face_id_type>& pos,
+                   const Real3& disp) const;
 
+    std::size_t num_faces() const {return faces_.size();}
     bool empty() const {return faces_.empty();}
     void clear() {return faces_.clear();}
     face_type&       operator[](const std::size_t i)       {return faces_[i];}
@@ -88,10 +135,13 @@ struct BDPolygon
     face_type&       at(const std::size_t i)       {return faces_.at(i);}
     face_type const& at(const std::size_t i) const {return faces_.at(i);}
 
+  private:
+
     struct face_finder
         : public std::unary_function<vertex_id_type, bool>
     {
         face_finder(const face_id_type& fid): fid_(fid){}
+
         bool operator()(const vertex_id_type& vid) const
         {
             return vid.first == fid_;
@@ -100,11 +150,11 @@ struct BDPolygon
         face_id_type fid_;
     };
 
-
     struct edge_finder
         : public std::unary_function<edge_id_type, bool>
     {
         edge_finder(const edge_id_type& eid): eid_(eid){}
+
         bool operator()(const std::pair<edge_id_type, edge_id_type>& eid) const
         {
             return eid.first == eid_;
@@ -113,21 +163,22 @@ struct BDPolygon
         edge_id_type eid_;
     };
 
-  private:
-
     void detect_shared_vertices();
     void detect_shared_edges();
 
-    boost::array<Real, 3> to_barycentric(const Real3& pos, const face_type& face) const;
-    Real3 to_absolute(const boost::array<Real, 3>& pos, const face_type& face) const;
-    bool is_inside(const boost::array<Real, 3>& pos) const;
+    template<typename keyT, typename valT, typename allocT, typename compT,
+             template<typename T1, typename T2, typename T3, typename T4>
+             class mapT>
+    valT const& const_at(const mapT<keyT, valT, allocT, compT>& m const keyT& k)
+    {
+        return m.find(k)->second;
+    }
+
     std::pair<uint32_t, Real>
-        crossed_edge(const boost::array<Real, 3>& pos,
-                     const boost::array<Real, 3>& disp) const;
-    Real cross_section(const boost::array<Real, 3>& pos,
-                       const boost::array<Real, 3>& disp, const uint32_t e) const;
-    Real triangle_area_2D(const Real& x1, const Real& y1,
-        const Real& x2, const Real& y2, const Real& x3, const Real& y3) const;
+    crossed_edge(const barycentric_type& pos, const barycentric_type& disp) const;
+    Real
+    cross_section(const barycentric_type& pos, const barycentric_type& disp,
+                  const edge_index_type eidx) const;
 
   private:
 
@@ -136,28 +187,7 @@ struct BDPolygon
     vertex_group_type   vertex_groups_;
 };
 
-
-inline Real3 BDPolygon::to_absolute(
-        const boost::array<Real, 3>& bary, const face_type& f) const
-{
-    return f.vertex_at(0) * bary[0] + f.vertex_at(1) * bary[1] +
-           f.vertex_at(2) * bary[2];
-}
-
-inline bool BDPolygon::is_inside(const boost::array<Real, 3>& barycentric) const
-{
-    return (0. <= barycentric[0] && barycentric[0] <= 1.0) &&
-           (0. <= barycentric[1] && barycentric[1] <= 1.0) &&
-           (0. <= barycentric[2] && barycentric[2] <= 1.0);
-}
-
-inline Real BDPolygon::triangle_area_2D(const Real& x1, const Real& y1,
-    const Real& x2, const Real& y2, const Real& x3, const Real& y3) const
-{
-    return (x1 - x2) * (y2 - y3) - (x2 - x3) * (y1 - y2);
-}
-
 }// bd
 
 }// ecell
-#endif //EGFRD_POLYGON
+#endif //ECELL_BD_POLYGON
---
+++ ecell4/bd/Barycentric.hpp
@@ -0,0 +1,146 @@
+#ifndef ECELL_BARYCENTRIC
+#define ECELL_BARYCENTRIC
+#include <boost/array.hpp>
+#include <ecell4/core/Real3.hpp>
+#include <ecell4/core/Triangle.hpp>
+
+namespace ecell4
+{
+namespace bd
+{
+
+template<typename realT>
+struct Barycentric
+{
+    typedef realT real_type;
+    typedef real_type value_type;
+    typedef std::size_t size_type;
+    typedef size_type index_type;
+
+    Barycentric(){}
+    ~Barycentric(){}
+
+    Barycentric(const real_type a, const real_type b, const real_type c);
+    Barycentric(const Barycentric& b);
+    Barycentric& operator=(const Barycentric& b);
+
+    real_type  operator[](const index_type i) const {return val[i];}
+    real_type& operator[](const index_type i)       {return val[i];}
+    real_type  at(const index_type i) const {return val.at(i);}
+    real_type& at(const index_type i)       {return val.at(i);}
+
+  private:
+    boost::array<realT, 3> val;
+};
+
+template<typename realT>
+Barycentric<realT>::Barycentric<realT>(
+        const real_type a, const real_type b, const real_type c)
+//     : val{{a, b, c}}
+{
+    val[0] = a;
+    val[1] = b;
+    val[2] = c;
+}
+
+template<typename realT>
+Barycentric<realT>::Barycentric<realT>(const Barycentric<realT>& b)
+{
+    val[0] = b.val[0];
+    val[1] = b.val[1];
+    val[2] = b.val[2];
+}
+
+template<typename realT>
+Barycentric<realT>& Barycentric<realT>::operator=(const Barycentric<realT>& b)
+{
+    val[0] = b.val[0];
+    val[1] = b.val[1];
+    val[2] = b.val[2];
+    return *this;
+}
+
+template<typename realT>
+Barycentric<realT>
+operator+(const Barycentric<realT>& lhs, const Barycentric<realT>& rhs)
+{
+    return Barycentric<realT>(lhs[0]+rhs[0], lhs[1]+rhs[1], lhs[2]+rhs[2]);
+}
+
+
+template<typename realT>
+Barycentric<realT>
+operator-(const Barycentric<realT>& lhs, const Barycentric<realT>& rhs)
+{
+    return Barycentric<realT>(lhs[0]-rhs[0], lhs[1]-rhs[1], lhs[2]-rhs[2]);
+}
+
+template<typename realT>
+inline bool is_inside(const Barycentric<realT>& bary) const
+{
+    return (0. <= bary[0] && bary[0] <= 1.0) &&
+           (0. <= bary[1] && bary[1] <= 1.0) &&
+           (0. <= bary[2] && bary[2] <= 1.0);
+}
+
+template<typename realT>
+inline Real3 to_absolute(const Barycentric<realT>& bary, const Triangle& tri)
+{
+    return tri.vertex_at(0) * bary[0] +
+           tri.vertex_at(1) * bary[1] +
+           tri.vertex_at(2) * bary[2];
+}
+
+namespace detail
+{
+
+template<typename realT>
+inline realT BDPolygon::triangle_area_2D(const realT x1, const realT y1,
+        const realT x2, const realT y2, const realT x3, const realT y3) const
+{
+    return (x1 - x2) * (y2 - y3) - (x2 - x3) * (y1 - y2);
+}
+
+} // detail
+
+//XXX hard-coded realT
+inline Barycentric<Real>
+to_barycentric(const Real3& pos, const Triangle& face) const
+{
+    const Real3& a = face.vertex_at(0);
+    const Real3& b = face.vertex_at(1);
+    const Real3& c = face.vertex_at(2);
+    const Real3  m = cross_product(face.edge_at(0), face.edge_at(2)) * (-1.);
+    const Real x = std::abs(m[0]);
+    const Real y = std::abs(m[1]);
+    const Real z = std::abs(m[2]);
+
+    Real nu, nv, ood;
+    if(x >= y && x >= z)
+    {
+        nu = detail::triangle_area_2D(pos[1], pos[2], b[1], b[2], c[1], c[2]);
+        nv = detail::triangle_area_2D(pos[1], pos[2], c[1], c[2], a[1], a[2]);
+        ood = 1.0 / m[0];
+    }
+    else if(y >= x && y >= z)
+    {
+        nu = detail::triangle_area_2D(pos[0], pos[2], b[0], b[2], c[0], c[2]);
+        nv = detail::triangle_area_2D(pos[0], pos[2], c[0], c[2], a[0], a[2]);
+        ood = 1.0 / -m[1];
+    }
+    else
+    {
+        nu = detail::triangle_area_2D(pos[0], pos[1], b[0], b[1], c[0], c[1]);
+        nv = detail::triangle_area_2D(pos[0], pos[1], c[0], c[1], a[0], a[1]);
+        ood = 1.0 / m[2];
+    }
+    Barycentric<Real> bary;
+    bary[0] = nu * ood;
+    bary[1] = nv * ood;
+    bary[2] = 1.0 - bary[0] - bary[1];
+    return bary;
+}
+
+} // bd
+} // ecell4
+#endif /* ECELL_BARYCENTRIC */
