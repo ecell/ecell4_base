3b99f403b7ec03785b1bf5609199bf284ae7a231
ToruNiina <niina.toru.68u@gmail.com>
2017-01-17 13:46:06 +0900

    fix cross_edge and cross_section in polygon

--- ecell4/bd/BDPolygon.cpp
+++ ecell4/bd/BDPolygon.cpp
@@ -16,24 +16,26 @@ void BDPolygon::detect_connectivity()
     return;
 }
 
-std::pair<bool, uint32_t>
+std::pair<bool, edge_index_type>
 BDPolygon::is_connected(const face_id_type& lhs, const face_id_type& rhs) const
 {
-    if(edge_pairs_.find(std::make_pair(lhs, 0))->second.first == rhs)
+    if(edge_pairs_.find(traits::make_pair(lhs, 0))->second.first == rhs)
         return std::make_pair(true, 0);
-    if(edge_pairs_.find(std::make_pair(lhs, 1))->second.first == rhs)
+    if(edge_pairs_.find(traits::make_pair(lhs, 1))->second.first == rhs)
         return std::make_pair(true, 1);
-    if(edge_pairs_.find(std::make_pair(lhs, 2))->second.first == rhs)
+    if(edge_pairs_.find(traits::make_pair(lhs, 2))->second.first == rhs)
         return std::make_pair(true, 2);
     return std::make_pair(false, 3);
 }
 
-std::pair<bool, uint32_t>
+std::pair<bool, vertex_index_type>
 BDPolygon::is_share_vertex(const face_id_type& lhs, const face_id_type& rhs) const
 {
-    for(uint32_t i=0; i<3; ++i)
+    for(vertex_index_type i=0; i<3; ++i)
     {
-        vertex_id_list list = vertex_groups_.find(std::make_pair(lhs, 0))->second;
+        vertex_id_list const& list =
+            vertex_groups_.find(std::make_pair(lhs, i));
+
         const face_finder cmp(rhs);
         if(std::find_if(list.begin(), list.end(), cmp) != list.end())
             return std::make_pair(true, i);
@@ -44,50 +46,60 @@ BDPolygon::is_share_vertex(const face_id_type& lhs, const face_id_type& rhs) con
 Real BDPolygon::distance(const std::pair<Real3, face_id_type>& lhs,
                          const std::pair<Real3, face_id_type>& rhs) const
 {
+    // CASE 0: on the same face
     if(lhs.second == rhs.second)
         return length(lhs.first - rhs.first);
 
-    const std::pair<bool, uint32_t> edg =
+    // CASE 1: lhs and rhs share an edge
+    { 
+    const std::pair<bool, edge_index_type> edg =
         this->is_connected(lhs.second, rhs.second);
-    if(edg.first)
+
+    if(edg.first) // connected
     {
+        // make two faces being same plane and then calculate distance
         const Triangle& lhs_t = faces_[lhs.second];
         const Triangle& rhs_t = faces_[rhs.second];
         const Real ang = angle(lhs_t.normal(), rhs_t.normal());
 
-//         const edge_id_type redg = edge_pairs_.find(edg)->second;
         const Real3 developped = lhs_t.vertex_at(edg.second) + 
             rotate(-ang,
                    rhs.first - lhs_t.vertex_at(edg.second),
                    lhs_t.edge_at(edg.second));
         return length(lhs.first - developped);
     }
+    }// end CASE 1
 
-    const std::pair<bool, uint32_t> vtx =
-        this->is_connected(lhs.second, rhs.second);
-    if(vtx.first)
+    // CASE 2: lhs and rhs share a vertex
     {
-        Real whole_angle = 0.;
-        Real angle_sum = 0.;
+    const std::pair<bool, vertex_index_type> vtx =
+        this->is_share_vertex(lhs.second, rhs.second);
+
+    if(vtx.first) // XXX!
+    {
+        Real whole_angle = 0.; // whole angle around the vertex
+        Real inter_angle = 0.; // angle lhs--vtx--rhs
+
+        Real r0 = 0.; // distance from the shared vertex to lhs.position
+        Real r1 = 0.; // ...                                rhs.position
+
+        vertex_id_list const& list = const_at(vertex_groups_,
+                traits::make_vertex_id(lhs.second, vtx.second));
 
         bool inter = false;
-        Real r0 = 0.;
-        Real r1 = 0.;
-
-        vertex_id_list const& list =
-            vertex_groups_.find(std::make_pair(lhs.second, vtx.second))->second;
-        for(vertex_id_list::const_iterator iter = list.begin();
-                iter != list.end(); ++iter) // ccw
+        for(vertex_id_list::const_iterator
+                iter = list.begin(); iter != list.end(); ++iter) // ccw
         {
-            const Triangle& f = this->faces_.at(iter->first);
-            whole_angle += f.angle_at(iter->second);
-            if(!inter && iter->first == lhs.second)
+            const Triangle& f = this->faces_.at(traits::get_face_id(*iter));
+            whole_angle += f.angle_at(traits::get_vertex_index(*iter));
+
+            if(!inter && traits::get_face_id(*iter) == lhs.second)
             {
                 inter = true;
                 angle_sum += angle(f.vertex_at(iter->second) - lhs.first,
                                    f.edge_at(iter->second==0?2:iter->second-1));
             }
-            else if(inter && iter->first == rhs.second)
+            else if(inter && traits::get_face_id(*iter) == rhs.second)
             {
                 inter = false;
                 angle_sum += angle(rhs.first - f.vertex_at(iter->second),
@@ -114,10 +126,11 @@ Real BDPolygon::distance(const std::pair<Real3, face_id_type>& lhs,
             }
         }
         const Real min_angle = std::min(angle_sum, whole_angle - angle_sum);
-        return r0 * r0 + r1 * r1 - 2. * r0 * r1 * cos(min_angle);
+        return r0 * r0 + r1 * r1 - 2. * r0 * r1 * std::cos(min_angle);
     }
+    }// end CASE 2
 
-//     throw std::logic_error("cannot determine the distance");
+    // lhs and rhs don't share edge nor vertex
     return std::numeric_limits<Real>::infinity();
 }
 
@@ -126,6 +139,7 @@ BDPolygon::move_next_face(const std::pair<Real3, face_id_type>& pos,
                           const Real3& disp) const
 {
     const face_type& f = faces_[pos.second];
+
     const boost::array<Real, 3> newpos =
         this->to_barycentric(pos.first + disp, f);
 
@@ -136,107 +150,105 @@ BDPolygon::move_next_face(const std::pair<Real3, face_id_type>& pos,
                 Real3(0.,0.,0.));
     }
 
+#ifdef ECELL4_TWOD_BD_DUMP
+    const boost::array<Real, 3> curpos =
+        this->to_barycentric(pos.first, f);
+    std::cerr << "curpos       = " << curpos[0] << ", " << curpos[1] << ", " << curpos[2] << std::endl;
+    std::cerr << "newpos       = " << newpos[0] << ", " << newpos[1] << ", " << newpos[2] << std::endl;
+    std::cerr << "length(disp) = " << length(disp) << std::endl;
+    std::cerr << "dot(norm, nd)= " << dot_product(disp, f.normal()) << std::endl;
+#endif
+
     const boost::array<Real, 3> bpos = to_barycentric(pos.first, f);
     boost::array<Real, 3> bdis;
     bdis[0] = newpos[0] - bpos[0];
     bdis[1] = newpos[1] - bpos[1];
     bdis[2] = newpos[2] - bpos[2];
 
+#ifdef ECELL4_TWOD_BD_DUMP
+    std::cerr << "pos(bary)    = " << bpos[0] << ", " << bpos[1] << ", " << bpos[2] << std::endl;
+    std::cerr << "disp(bary)   = " << bdis[0] << ", " << bdis[1] << ", " << bdis[2] << std::endl;
+#endif
+
     const std::pair<uint32_t, Real> cross = crossed_edge(bpos, bdis);
     const edge_finder cmp(std::make_pair(pos.second, cross.first));
-    const face_id_type next_face =
-        std::find_if(edge_pairs_.begin(), edge_pairs_.end(), cmp)->first.first;
+    const face_id_type next_face_id =
+        std::find_if(edge_pairs_.begin(), edge_pairs_.end(), cmp)->second.first;
+    const face_type& next_face = faces_[next_face_id];
 
-    return std::make_pair(std::make_pair(pos.first + disp * cross.second, next_face),
-            disp * (1. - cross.second));
+#ifdef ECELL4_TWOD_BD_DUMP
+    std::cerr << "cross ratio  = " << cross.second << std::endl;
+    std::cerr << "next_face id = " << next_face_id << std::endl;
+#endif
+
+    // turn displacement
+    const Real ang = angle(f.normal(), next_face.normal());
+    const Real3 axis = f.edge_at(cross.first) / length(f.edge_at(cross.first));
+    const Real3 next_disp = rotate(ang, axis, disp) * (1. - cross.second);
+
+#ifdef ECELL4_TWOD_BD_DUMP
+    std::cerr << "next disp    = " << next_disp << std::endl;
+    std::cerr << "next disp len= " << length(next_disp) << std::endl;
+    std::cerr << "dot(norm, nd)= " << dot_product(next_disp, next_face.normal()) << std::endl;
+    std::cerr << std::endl;
+#endif
+
+    return std::make_pair(std::make_pair(pos.first + disp * cross.second, next_face_id),
+            next_disp);
 }
 
-std::pair<uint32_t, Real>
-BDPolygon::crossed_edge(const boost::array<Real, 3>& pos, const boost::array<Real, 3>& disp) const
+//!
+//  find first-crossing edge and
+//  return its index and displacement ratio to the cross section
+std::pair<edge_index_type, Real>
+BDPolygon::crossed_edge(const barycentric_type& pos, const barycentric_type& disp) const
 {
-    boost::array<Real, 3> npos;
-    npos[0] = pos[0] + disp[0];
-    npos[1] = pos[1] + disp[1];
-    npos[2] = pos[2] + disp[2];
+    barycentric_type npos = pos + disp;
 
-    if(npos[0] * npos[1] * npos[2] < 0)
+    if(npos[0] * npos[1] * npos[2] < 0.) // (+, +, -) or one of its permutations
     {
-             if(npos[0] < 0) return std::make_pair(1, cross_section(pos, disp, 1));
-        else if(npos[1] < 0) return std::make_pair(2, cross_section(pos, disp, 2));
-        else if(npos[2] < 0) return std::make_pair(0, cross_section(pos, disp, 0));
+             if(npos[0] < 0.) return std::make_pair(1, cross_section(pos, disp, 1));
+        else if(npos[1] < 0.) return std::make_pair(2, cross_section(pos, disp, 2));
+        else if(npos[2] < 0.) return std::make_pair(0, cross_section(pos, disp, 0));
+        else throw std::logic_error("Polygon::crossed_edge: never reach here");
     }
-    else
+    else // (+, -, -) or one of its permutations
     {
-        assert(!is_inside(npos));
-        if(npos[0] > 0)
+        assert(!is_inside(npos)); // not (+, +, +) case
+//         assert(on_plane(npos));   // summation is 1
+        if(npos[0] > 0.)
         {
             const Real ab = cross_section(pos, disp, 0);
             const Real ca = cross_section(pos, disp, 2);
-            return (ca > ab) ? std::make_pair(0, ab) : std::make_pair(2, ca);
+            return (ab > ca) ? std::make_pair(2, ca) : std::make_pair(0, ab);
         }
-        else if(npos[1] > 0)
+        else if(npos[1] > 0.)
         {
             const Real ab = cross_section(pos, disp, 0);
             const Real bc = cross_section(pos, disp, 1);
-            return (ab > bc) ? std::make_pair(1, bc) : std::make_pair(0, ab);
+            return (bc > ab) ? std::make_pair(0, ab) : std::make_pair(1, bc);
         }
-        else if(npos[2] > 0)
+        else if(npos[2] > 0.)
         {
             const Real bc = cross_section(pos, disp, 1);
             const Real ca = cross_section(pos, disp, 2);
-            return (bc > ca) ? std::make_pair(2, ca) : std::make_pair(1, bc);
+            return (ca > bc) ? std::make_pair(1, bc) : std::make_pair(2, ca);
         }
     }
 }
 
-Real BDPolygon::cross_section(const boost::array<Real, 3>& pos,
-        const boost::array<Real, 3>& disp, const uint32_t e) const
+Real BDPolygon::cross_section(const barycentric_type& pos,
+        const barycentric_type& disp, const edge_index_type e) const
 {
     switch(e)
     {
-        case 0:  return -pos[1] / disp[1];
+        case 0:  return -pos[2] / disp[2];
         case 1:  return -pos[0] / disp[0];
-        case 2:  return -pos[2] / disp[2];
+        case 2:  return -pos[1] / disp[1];
         default: return std::numeric_limits<Real>::infinity();
     }
 }
 
-boost::array<Real, 3>
-BDPolygon::to_barycentric(const Real3& pos, const face_type& face) const
-{
-    const Real3& a = face.vertex_at(0);
-    const Real3& b = face.vertex_at(1);
-    const Real3& c = face.vertex_at(2);
-    const Real3  m = cross_product(face.edge_at(0), face.edge_at(2)) * (-1.);
-    const Real x = std::abs(m[0]);
-    const Real y = std::abs(m[1]);
-    const Real z = std::abs(m[2]);
-
-    Real nu, nv, ood;
-    if (x >= y && x >= z)
-    {
-        nu = triangle_area_2D(pos[1], pos[2], b[1], b[2], c[1], c[2]);
-        nv = triangle_area_2D(pos[1], pos[2], c[1], c[2], a[1], a[2]);
-        ood = 1.0 / m[0];
-    }
-    else if (y >= x && y >= z)
-    {
-        nu = triangle_area_2D(pos[0], pos[2], b[0], b[2], c[0], c[2]);
-        nv = triangle_area_2D(pos[0], pos[2], c[0], c[2], a[0], a[2]);
-        ood = 1.0 / -m[1];
-    }
-    else
-    {
-        nu = triangle_area_2D(pos[0], pos[1], b[0], b[1], c[0], c[1]);
-        nv = triangle_area_2D(pos[0], pos[1], c[0], c[1], a[0], a[1]);
-        ood = 1.0 / m[2];
-    }
-    boost::array<Real, 3> bary;
-    bary[0] = nu * ood;
-    bary[1] = nv * ood;
-    bary[2] = 1.0 - bary[0] - bary[1];
-    return bary;
-}
 
 void BDPolygon::detect_shared_vertices()
 {
