8486b0a02639d09eb97ffe7bc42dd681318c412a
Suguru Kato <salmon.bioinformatics@gmail.com>
2017-01-19 17:15:53 +0900

    style: Format the definitions of functions of OffLatticeSpace

--- ecell4/core/OffLatticeSpace.hpp
+++ ecell4/core/OffLatticeSpace.hpp
@@ -15,14 +15,14 @@ protected:
     typedef std::vector<std::vector<voxel_container::iterator> > adjoining_container;
 
 public:
+    typedef std::pair<coordinate_type, coordinate_type> coordinate_pair_type;
     typedef std::vector<Real3> position_container;
-    typedef std::vector<std::pair<coordinate_type, coordinate_type> > coordinate_pair_list_type;
+    typedef std::vector<coordinate_pair_type> coordinate_pair_list_type;
 
     OffLatticeSpace(const Real& voxel_radius);
-    OffLatticeSpace(
-            const Real& voxel_radius,
-            const position_container& positions,
-            const coordinate_pair_list_type& adjoining_pairs);
+    OffLatticeSpace(const Real& voxel_radius,
+                    const position_container& positions,
+                    const coordinate_pair_list_type& adjoining_pairs);
     virtual ~OffLatticeSpace();
 
     virtual std::pair<ParticleID, Voxel> get_voxel_at(const coordinate_type& coord) const;
@@ -33,13 +33,16 @@ public:
     virtual bool remove_voxel(const ParticleID& pid);
     virtual bool remove_voxel(const coordinate_type& coord);
 
-    virtual bool can_move(const coordinate_type& src, const coordinate_type& dest) const;
-    virtual bool move(
-        const coordinate_type& src, const coordinate_type& dest,
-        const std::size_t candidate=0);
+    virtual bool can_move(const coordinate_type& src,
+                          const coordinate_type& dest) const;
+    virtual bool move(const coordinate_type& src,
+                      const coordinate_type& dest,
+                      const std::size_t candidate=0);
     virtual std::pair<coordinate_type, bool> move_to_neighbor(
-        VoxelPool* const& from, VoxelPool* const& loc,
-        coordinate_id_pair_type& info, const Integer nrand);
+            VoxelPool* const& from,
+            VoxelPool* const& loc,
+            coordinate_id_pair_type& info,
+            const Integer nrand);
 
     virtual VoxelPool* get_voxel_pool_at(const coordinate_type& coord) const;
 
@@ -53,10 +56,10 @@ public:
     virtual Real3 coordinate2position(const coordinate_type& coord) const;
     virtual coordinate_type position2coordinate(const Real3& pos) const;
 
-    virtual coordinate_type get_neighbor(
-        const coordinate_type& coord, const Integer& nrand) const;
-    virtual coordinate_type get_neighbor_boundary(
-        const coordinate_type& coord, const Integer& nrand) const;
+    virtual coordinate_type get_neighbor(const coordinate_type& coord,
+                                         const Integer& nrand) const;
+    virtual coordinate_type get_neighbor_boundary(const coordinate_type& coord,
+                                                  const Integer& nrand) const;
 
     virtual Integer num_molecules(const Species& sp) const;
 
@@ -73,13 +76,15 @@ public:
 
 protected:
 
-    void reset(
-            const position_container& positions,
-            const coordinate_pair_list_type& adjoining_pairs);
+    void reset(const position_container& positions,
+               const coordinate_pair_list_type& adjoining_pairs);
     bool is_in_range(const coordinate_type& coord) const;
     VoxelPool* get_voxel_pool(const Voxel& v);
     coordinate_type get_coord(const ParticleID& pid) const;
-    bool make_molecular_pool(const Species& sp, Real radius, Real D, const std::string loc);
+    bool make_molecular_pool(const Species& sp,
+                             Real radius,
+                             Real D,
+                             const std::string loc);
     Integer count_voxels(const boost::shared_ptr<VoxelPool>& vp) const;
 
 protected:
