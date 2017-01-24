0e05bfac2267b06ea7225083b18ace855190e0bb
ToruNiina <niina.toru.68u@gmail.com>
2017-01-24 20:31:31 +0900

    follow changes in BD

--- ecell4/bd/samples/surface.cpp
+++ ecell4/bd/samples/surface.cpp
@@ -8,6 +8,7 @@
 #include <ecell4/core/Real3.hpp>
 #include <ecell4/core/NetworkModel.hpp>
 
+#include <ecell4/bd/BDPolygon.hpp>
 #include <ecell4/bd/BDSimulator.hpp>
 
 using namespace ecell4;
@@ -18,7 +19,7 @@ using namespace ecell4::bd;
  */
 void print_particle_position(const BDWorld& world, const ParticleID& pid)
 {
-    const Real3 pos(world.get_2D_particle(pid).second.position());
+    const Real3 pos(world.get_particle(pid).second.position());
     std::cout << std::setprecision(12) << world.t() << " "
         << pos[0] << " " << pos[1] << " " << pos[2] << std::endl;
 }
@@ -30,7 +31,7 @@ int main(int argc, char** argv)
 {
     /// simulation parameters
     const Real L(1e-6);
-    std::string D("5e-12"), radius("5e-9");
+    std::string D("5e0"), radius("5e-9");
     const Real3 edge_lengths(L, L, L);
     const Integer3 matrix_sizes(3, 3, 3);
 
@@ -57,6 +58,8 @@ int main(int argc, char** argv)
         return 1;
     }
 
+    std::cerr << "begin reading polygon.xyz";
+    BDPolygon polygon;
     std::size_t num_faces=0;
     while(!poly.eof())
     {
@@ -85,36 +88,44 @@ int main(int argc, char** argv)
         }
 
         Triangle f(v1, v2, v3);
-        world->new_face(f);
+        polygon.add_face(f);
         poly.peek();
     }
-    world->initialize_polygon();
-    std::cerr << "polygon initialized" << std::endl;
+    std::cerr << "... end!" << std::endl;
+
+    std::cerr << "begin polygon initialization";
+    world->set_polygon(polygon);
+    std::cerr << "... end!" << std::endl;
 
     const std::size_t fid=0;
-    Triangle f = world->face_at(fid);
+    Triangle f = world->get_face(fid);
     const Real3 initial = (f.vertex_at(0) + f.vertex_at(1) + f.vertex_at(2)) / 3.;
     std::cerr << "initial position = " << initial << std::endl;
 
     /// create a Particle, and inject it into BDWorld
+    std::cerr << "begin create/inject particle to BDWorld";
     BDWorld::molecule_info_type info1((*world).get_molecule_info(Species("A")));
     const Particle p1(sp1, initial, info1.radius, info1.D);
-    const ParticleID pid1((*world).new_2D_particle(p1, fid).first.first);
-    world->save("test_bd.h5");
+    const ParticleID pid1((*world).new_particle(p1, fid).first.first);
+//     world->save("test_bd.h5");
+    std::cerr << "... end!" << std::endl;
 
-
+    std::cerr << "begin simulator setup";
     /// instatiate BDSimulator
     BDSimulator sim(model, world);
     sim.set_dt(1e-6);
+    std::cerr << "... end!" << std::endl;
 
     /// run and log by the millisecond
+    std::cerr << "begin simulation..." << std::endl;
     for(unsigned int i(0); i <= 100; ++i)
     {
-        while(sim.step(1e-3*i))
+        while(sim.step(1.0*i))
         {
             // do nothing
         }
         print_particle_position(*world, pid1);
     }
+    std::cerr << "end!" << std::endl;
     return 0;
 }
