c00ce5833f7e55cde1337550a174c9b6b2353d4b
ToruNiina <niina.toru.68u@gmail.com>
2017-01-31 19:38:07 +0900

    add tolerance to angle(vec, vec) func

--- ecell4/bd/rotate_vector.hpp
+++ ecell4/bd/rotate_vector.hpp
@@ -1,6 +1,7 @@
 #ifndef ECELL4_VECTOR_ROTATION
 #define ECELL4_VECTOR_ROTATION
 #include <ecell4/core/Real3.hpp>
+#include <cmath>
 
 namespace ecell4
 {
@@ -8,7 +9,23 @@ namespace ecell4
 Real3 rotate(const Real angle, const Real3& axis, const Real3& target);
 inline Real angle(const Real3& lhs, const Real3& rhs)
 {
-    return acos(dot_product(lhs, rhs) / std::sqrt(length_sq(lhs) * length_sq(rhs)));
+    const Real costheta = dot_product(lhs, rhs) / std::sqrt(length_sq(lhs) * length_sq(rhs));
+    if(1.0 < costheta)
+    {
+        if(std::abs(costheta - 1.0) < 1e-8)
+            return std::acos(1.0);
+        else
+            throw std::out_of_range("acos");
+    }
+    else if(costheta < -1.0)
+    {
+        if(std::abs(costheta + 1.0) < 1e-8)
+            return std::acos(-1.0);
+        else
+            throw std::out_of_range("acos");
+    }
+    else
+        return std::acos(costheta);
 }
 
 
