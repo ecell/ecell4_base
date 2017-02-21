54c07ffe79d8ed5c5e3fde1990a8891d8ffd236e
ToruNiina <niina.toru.68u@gmail.com>
2017-02-21 18:56:19 +0900

    add force_put_inside(barycentric)

--- ecell4/bd/Barycentric.hpp
+++ ecell4/bd/Barycentric.hpp
@@ -92,6 +92,51 @@ inline bool is_inside(const Barycentric<realT>& bary)
 }
 
 template<typename realT>
+inline bool is_inside(const Barycentric<realT>& bary, const realT tolerance)
+{
+    return on_plane(bary) &&
+           (0.0 - tolerance <= bary[0] && bary[0] <= 1.0 + tolerance) &&
+           (0.0 - tolerance <= bary[1] && bary[1] <= 1.0 + tolerance) &&
+           (0.0 - tolerance <= bary[2] && bary[2] <= 1.0 + tolerance);
+}
+
+template<typename realT>
+Barycentric<realT> force_put_inside(const Barycentric<realT>& bary)
+{
+    if(!on_plane(bary))
+        throw std::invalid_argument("force_put_inside: outside of the plane");
+    if(is_inside(bary)) return bary;
+
+    Barycentric<realT> retval(bary);
+    boost::array<bool, 3> over;
+    over[0] = over[1] = over[2] = false;
+    for(std::size_t i=0; i<3; ++i)
+    {
+        if(retval[i] < 0.)
+        {
+            over[i] = true;
+            retval[i] = 0;
+        }
+        else if(retval[i] > 1.)
+        {
+            over[i] = true;
+            retval[i] = 1.;
+        }
+    }
+    if(!over[0])
+        retval[0] = 1. - retval[1] - retval[2];
+    else if(!over[1])
+        retval[1] = 1. - retval[0] - retval[2];
+    else if(!over[2])
+        retval[2] = 1. - retval[0] - retval[1];
+    else
+        throw std::invalid_argument("force_put_inside: too far");
+
+    return retval;
+}
+
+
+template<typename realT>
 inline Real3 to_absolute(const Barycentric<realT>& bary, const Triangle& tri)
 {
     return tri.vertex_at(0) * bary[0] +
