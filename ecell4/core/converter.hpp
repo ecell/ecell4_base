562aff4655190df6293c995db7e6b7181cd8fe00
ToruNiina <niina.toru.68u@gmail.com>
2017-04-18 12:52:52 +0900

    define core/converter::result_type

--- ecell4/core/converter.hpp
+++ ecell4/core/converter.hpp
@@ -10,24 +10,24 @@ namespace utils
 template<typename T_convert, typename T_first, typename T_second>
 struct pair_first_element_converter
 {
-    typedef T_convert converted_type;
+    typedef T_convert result_type;
     typedef std::pair<T_first, T_second> element_type;
 
-    converted_type operator()(const element_type& v)
+    result_type operator()(const element_type& v) const
     {
-        return converted_type(v.first);
+        return result_type(v.first);
     }
 };
 
 template<typename T_convert, typename T_first, typename T_second>
 struct pair_second_element_converter
 {
-    typedef T_convert converted_type;
+    typedef T_convert result_type;
     typedef std::pair<T_first, T_second> element_type;
 
-    converted_type operator()(const element_type& v)
+    result_type operator()(const element_type& v) const
     {
-        return converted_type(v.second);
+        return result_type(v.second);
     }
 };
 
