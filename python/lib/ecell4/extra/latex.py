6739490a07672b82ced7785c83d13154f2ee0520
Kazunari Kaizu <kwaizu@gmail.com>
2017-04-24 12:37:01 +0900

    #183 Add options to display equations and parameters inline

--- python/lib/ecell4/extra/latex.py
+++ python/lib/ecell4/extra/latex.py
@@ -14,7 +14,7 @@ def escape_serial(val):
         return escape_serial(val.serial())
     return "\\left[{}\\right]".format(val)
 
-def function_parseobj(obj):
+def function_parseobj(obj, params):
     reserved_vars = ['_t', 'pi']
     reserved_funcs = ['exp', 'log', 'sin', 'cos', 'tan', 'asin', 'acos', 'atan'] # , 'pow']
 
@@ -25,61 +25,73 @@ def function_parseobj(obj):
     if last.name not in reserved_funcs or last.args is None or len(last.args) != 1:
         return None
 
-    return "{{\\{}}} {}".format(last.name, wrap_parseobj(last.args[0], True))
+    return "{{\\{}}} {}".format(last.name, wrap_parseobj(last.args[0], params, True))
 
-def wrap_parseobj(obj, force=False):
-    res = function_parseobj(obj)
+def wrap_parseobj(obj, params, force=False):
+    res = function_parseobj(obj, params)
     if res is not None:
         return res
 
     if (isinstance(obj, (parseobj.AnyCallable, parseobj.ParseObj))
             or (not force and isinstance(obj, (parseobj.DivExp, parseobj.MulExp, parseobj.PowExp)))
-            or (isinstance(obj, numbers.Number) and obj >= 0)
+            or (isinstance(obj, numbers.Number) and (obj >= 0 or params is not None))
             or not isinstance(obj, (numbers.Number, parseobj.ExpBase))):
-        return convert_parseobj(obj)
-    return "\\left({}\\right)".format(convert_parseobj(obj))
+        return convert_parseobj(obj, params)
+    return "\\left({}\\right)".format(convert_parseobj(obj, params))
 
-def convert_parseobj(obj):
-    res = function_parseobj(obj)
+def convert_parseobj(obj, params):
+    res = function_parseobj(obj, params)
     if res is not None:
         return res
 
     if isinstance(obj, (parseobj.AnyCallable, parseobj.ParseObj)):
         return escape_serial(str(obj))
     elif isinstance(obj, parseobj.PosExp):
-        return "+{}".format(wrap_parseobj(obj._target))
+        return "+{}".format(wrap_parseobj(obj._target, params))
     elif isinstance(obj, parseobj.NegExp):
-        return "-{}".format(wrap_parseobj(obj._target))
+        return "-{}".format(wrap_parseobj(obj._target, params))
     elif isinstance(obj, parseobj.AddExp):
-        return "+".join([convert_parseobj(elem) for elem in obj._elements()])
+        return "+".join([convert_parseobj(elem, params) for elem in obj._elements()])
     elif isinstance(obj, parseobj.SubExp):
         elems = obj._elements()
-        res = convert_parseobj(elems[0])
-        res += "".join(["-{}".format(wrap_parseobj(elem)) for elem in elems[1: ]])
+        res = convert_parseobj(elems[0], params)
+        res += "".join(["-{}".format(wrap_parseobj(elem, params)) for elem in elems[1: ]])
         return res
     elif isinstance(obj, parseobj.DivExp):
         elems = obj._elements()
-        res = convert_parseobj(elems[0])
+        res = convert_parseobj(elems[0], params)
         for elem in elems[1: ]:
-            res = "\\frac{{{}}}{{{}}}".format(res, convert_parseobj(elem))
+            res = "\\frac{{{}}}{{{}}}".format(res, convert_parseobj(elem, params))
         return res
     elif isinstance(obj, parseobj.MulExp):
-        return "".join([wrap_parseobj(elem) for elem in obj._elements()])
+        return "".join([wrap_parseobj(elem, params) for elem in obj._elements()])
     elif isinstance(obj, parseobj.PowExp):
-        return "{{{}}}^{{{}}}".format(wrap_parseobj(obj._lhs, True), convert_parseobj(obj._rhs))
+        return "{{{}}}^{{{}}}".format(wrap_parseobj(obj._lhs, params, True), convert_parseobj(obj._rhs, params))
     elif isinstance(obj, parseobj.ExpBase):
         raise ValueError("The given type of ParseObj is not supported yet [{}].".format(repr(obj)))
 
+    if params is not None and isinstance(obj, numbers.Number):
+        k = "k_{{{}}}".format(len(params) + 1)
+        params[k] = str(obj)
+        return k
     return str(obj)
 
-def equations(m):
+def equations(m, inline=False, constants=True):
     derivatives = defaultdict(list)
     equations = {}
+    params = {} if constants else None
     for i, rr in enumerate(m.reaction_rules()):
         name = "v_{{{}}}".format(i + 1)
 
         if isinstance(rr, ReactionRule) or (isinstance(rr, ODEReactionRule) and rr.is_massaction()):
-            equations[name] = "{}{}".format(rr.k(), "".join([escape_serial(sp) for sp in rr.reactants()]))
+            if constants:
+                k = "k_{{{}}}".format(len(params) + 1)
+                params[k] = rr.k()
+            elif rr.k() != 1:
+                k = "{:g}".format(rr.k())
+            else:
+                k = ""
+            equations[name] = "{}{}".format(k, "".join([escape_serial(sp) for sp in rr.reactants()]))
         elif isinstance(rr, ODEReactionRule):
             #XXX: rr.is_massaction() is False
             rl = rr.get_ratelaw()
@@ -87,7 +99,17 @@ def equations(m):
             parser.set_callback()
             obj = parser.eval(rl.as_string(), params={'pow': pow})
             # obj = just_parse()._ParseDecorator__evaluate(rl.as_string())
-            equations[name] = convert_parseobj(obj)
+            if inline:
+                equations[name] = wrap_parseobj(obj, params)
+            else:
+                equations[name] = convert_parseobj(obj, params)
+
+        if inline:
+            if name not in equations:
+                raise ValueError("No equation is defined [{}]".format(rr.as_string()))
+            eq = equations[name]
+        else:
+            eq = name
 
         stoich = defaultdict(int)
 
@@ -108,11 +130,12 @@ def equations(m):
                     pref = "{:d} ".format(coef)
 
                 if len(derivatives[serial]) == 0 and coef > 0:
-                    derivatives[serial].append("{}{}".format(pref, name))
+                    derivatives[serial].append("{}{}".format(pref, eq))
                 elif coef > 0:
-                    derivatives[serial].append("+{}{}".format(pref, name))
+                    derivatives[serial].append("+{}{}".format(pref, eq))
                 else:
-                    derivatives[serial].append("{}{}".format(pref, name))
+                    # coef < 0
+                    derivatives[serial].append("{}{}".format(pref, eq))
         elif isinstance(rr, ODEReactionRule):
             for sp, coef in zip(rr.reactants(), rr.reactants_coefficients()):
                 stoich[escape_serial(sp)] -= coef
@@ -132,22 +155,35 @@ def equations(m):
                     pref = "{:g} ".format(coef)
 
                 if len(derivatives[serial]) == 0 and coef > 0:
-                    derivatives[serial].append("{}{}".format(pref, name))
+                    derivatives[serial].append("{}{}".format(pref, eq))
                 elif coef > 0:
-                    derivatives[serial].append("+{}{}".format(pref, name))
+                    derivatives[serial].append("+{}{}".format(pref, eq))
                 else:
-                    derivatives[serial].append("{}{}".format(pref, name))
+                    # coef < 0
+                    derivatives[serial].append("{}{}".format(pref, eq))
 
     res = []
     for serial in sorted(derivatives.keys()):
         res.append(
             "\\frac{{\\mathrm{{d}} {}}}{{\\mathrm{{d}} t}} &=& {}".format(serial, ''.join(derivatives[serial])))
-    for name in sorted(equations.keys()):
-        res.append(
-            "{} &=& {}".format(name, equations[name]))
+
+    if not inline:
+        for name in sorted(equations.keys()):
+            res.append(
+                "{} &=& {}".format(name, equations[name]))
+
+    if params:
+        for name in sorted(params.keys()):
+            res.append(
+                "{} &=& {}".format(name, params[name]))
 
     res = "\\begin{{eqnarray}}\n{}\n\\end{{eqnarray}}".format(',\\\\\n'.join(res))
     return res
+
+def display_equations(m, inline=False, constants=True):
+    from IPython.display import display, Math, Latex
+    fmt = equations(m, inline, constants)
+    display(Latex(fmt))
 
 
 if __name__ == "__main__":
