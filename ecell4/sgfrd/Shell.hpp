79aabae67d6b50bdcfa28ecbf2f4e48794f4e85c
ToruNiina <niina.toru.68u@gmail.com>
2017-04-25 17:00:23 +0900

    add structure id to shell

--- ecell4/sgfrd/Shell.hpp
+++ ecell4/sgfrd/Shell.hpp
@@ -9,19 +9,21 @@ namespace ecell4
 namespace sgfrd
 {
 
-template<typename T_shape>
+template<typename T_shape, typename T_structure_id>
 class Shell
 {
   public:
     typedef T_shape shape_type;
+    typedef T_structure_id structure_id_type;
     typedef DomainID domain_id_type;
 
   public:
     Shell(){}
     ~Shell(){}
 
-    Shell(const domain_id_type& domain_id, const shape_type& shape)
-        : domain_id_(domain_id), shape_(shape)
+    Shell(const domain_id_type& domain_id, const shape_type& shape,
+          const structure_id_type& sid)
+        : domain_id_(domain_id), structure_id_(sid), shape_(shape)
     {}
 
     Real3 const& position() const {return shape_.position();}
@@ -30,15 +32,23 @@ class Shell
     Real  size() const {return shape_.size();}
     Real& size()       {return shape_.size();}
 
-    DomainID      & domain_id()       {return domain_id_;}
-    DomainID const& domain_id() const {return domain_id_;}
+    domain_id_type      & domain_id()       {return domain_id_;}
+    domain_id_type const& domain_id() const {return domain_id_;}
 
+    structure_id_type      & structure_id()       {return structure_id_;}
+    structure_id_type const& structure_id() const {return structure_id_;}
     shape_type      & shape()       {return shape_;}
     shape_type const& shape() const {return shape_;}
 
+    std::pair<Real3, structure_id_type> get_surface_position() const
+    {
+        return std::make_pair(shape_.position(), structure_id_);
+    }
+
   private:
 
-    domain_id_type domain_id_;
+    domain_id_type    domain_id_;
+    structure_id_type structure_id_;
     shape_type     shape_;
 };
 
