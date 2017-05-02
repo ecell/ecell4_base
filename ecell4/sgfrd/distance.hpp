0c041582022ecf80953e58efd33265cb1418e015
ToruNiina <niina.toru.68u@gmail.com>
2017-05-02 13:37:45 +0900

    fix operator() declaration and partial specialization

--- ecell4/sgfrd/distance.hpp
+++ ecell4/sgfrd/distance.hpp
@@ -12,59 +12,64 @@ struct distance_sq
 {
     typedef Real result_type;
 
-    Real operator(const shape1& s1, const shape2& s2) const
+    Real operator()(const shape1& s1, const shape2& s2) const
     {
         throw ecell4::NotSupported(
                 "distance_sq between these shapes is not supported");
     }
 };
 
+template<>
 struct distance_sq<Real3, ecell4::Triangle>
 {
     typedef Real result_type;
 
-    Real operator(const Real3& p, const ecell4::Triangle& t) const
+    Real operator()(const Real3& p, const ecell4::Triangle& t) const
     {
         return ecell4::collision::distance_sq_point_triangle(p, t);
     }
 };
 
+template<>
 struct distance_sq<Real3, ecell4::Circle>
 {
     typedef Real result_type;
 
-    Real operator(const Real3& p, const ecell4::Circle& t) const
+    Real operator()(const Real3& p, const ecell4::Circle& t) const
     {
         return ecell4::collision::distance_sq_point_circle(p, t);
     }
 };
 
+template<>
 struct distance_sq<Real3, ecell4::Cone>
 {
     typedef Real result_type;
 
-    Real operator(const Real3& p, const ecell4::Cone& t) const
+    Real operator()(const Real3& p, const ecell4::Cone& t) const
     {
         return ecell4::collision::distance_sq_point_cone(p, t);
     }
 };
 
+template<>
 struct distance_sq<Real3, ecell4::Cylinder>
 {
     typedef Real result_type;
 
-    Real operator(const Real3& p, const ecell4::Cylinder& t) const
+    Real operator()(const Real3& p, const ecell4::Cylinder& t) const
     {
-        const Real d = ecell4::collision::distance_point_cone(p, t);
+        const Real d = ecell4::collision::distance_point_cylinder(p, t);
         return d*d;
     }
 };
 
+template<>
 struct distance_sq<Real3, ecell4::Sphere>
 {
     typedef Real result_type;
 
-    Real operator(const Real3& p, const ecell4::Sphere& s) const
+    Real operator()(const Real3& p, const ecell4::Sphere& s) const
     {
         const Real d = length(p - s.center()) - s.radius();
         return d*d;
@@ -78,58 +83,63 @@ struct distance
 {
     typedef Real result_type;
 
-    Real operator(shape1 s1, shape2 s2) const
+    Real operator()(shape1 s1, shape2 s2) const
     {
         throw ecell4::NotSupported(
                 "distance between these shapes is not supported");
     }
 };
 
+template<>
 struct distance<Real3, ecell4::Triangle>
 {
     typedef Real result_type;
 
-    Real operator(const Real3& p, const ecell4::Triangle& t) const
+    Real operator()(const Real3& p, const ecell4::Triangle& t) const
     {
         return ecell4::collision::distance_point_triangle(p, t);
     }
 };
 
+template<>
 struct distance<Real3, ecell4::Circle>
 {
     typedef Real result_type;
 
-    Real operator(const Real3& p, const ecell4::Circle& t) const
+    Real operator()(const Real3& p, const ecell4::Circle& t) const
     {
         return ecell4::collision::distance_point_circle(p, t);
     }
 };
 
+template<>
 struct distance<Real3, ecell4::Cone>
 {
     typedef Real result_type;
 
-    Real operator(const Real3& p, const ecell4::Cone& t) const
+    Real operator()(const Real3& p, const ecell4::Cone& t) const
     {
         return ecell4::collision::distance_point_cone(p, t);
     }
 };
 
+template<>
 struct distance<Real3, ecell4::Cylinder>
 {
     typedef Real result_type;
 
-    Real operator(const Real3& p, const ecell4::Cylinder& t) const
+    Real operator()(const Real3& p, const ecell4::Cylinder& t) const
     {
         return ecell4::collision::distance_point_cylinder(p, t);
     }
 };
 
+template<>
 struct distance<Real3, ecell4::Sphere>
 {
     typedef Real result_type;
 
-    Real operator(const Real3& p, const ecell4::Sphere& s) const
+    Real operator()(const Real3& p, const ecell4::Sphere& s) const
     {
         return length(p - s.center()) - s.radius();
     }
