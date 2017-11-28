d4a8919c7aa5c3167f4d7e2ccde6b995eabce693
ToruNiina <niina.toru.68u@gmail.com>
2017-11-28 18:27:58 +0900

    add R_(ipv|com) to Pair domain

--- ecell4/sgfrd/Pair.hpp
+++ ecell4/sgfrd/Pair.hpp
@@ -14,12 +14,13 @@ class Pair
 
     enum EventKind
     {
-        SINGLE_REACTION_0,
         SINGLE_REACTION_1,
+        SINGLE_REACTION_2,
         COM_ESCAPE,
         IV_ESCAPE,
         IV_REACTION,
         IV_UNDETERMINED,
+        UNDETERMINED
     };
 
     typedef ShellID    shell_id_type;
@@ -28,49 +29,71 @@ class Pair
     typedef std::pair<ParticleID, Particle>   particle_id_pair;
     typedef boost::array<particle_id_pair, 2> particle_array_type;
 
-  public:
-    Pair(): dt_(0.), begin_time_(0.){}
-    ~Pair(){}
-
-    Pair(const Real dt, const Real begin_time, shell_id_type const& sh)
-        : dt_(dt), begin_time_(begin_time), shell_id_(sh)
-    {}
-
-    Pair(shell_id_type const& sh,
-         particle_id_pair const& p0, particle_id_pair const& p1)
-        : shell_id_(sh)
+    static Real calc_D_ipv(const Real D1, const Real D2) throw()
     {
-        if(p0.second.D() < p1.second.D())
-        {
-            new(&particles_[0]) particle_id_pair(p0);
-            new(&particles_[1]) particle_id_pair(p1);
-        }
-        else
-        {
-            new(&particles_[0]) particle_id_pair(p1);
-            new(&particles_[1]) particle_id_pair(p0);
-        }
+        return D1 + D2;
     }
 
-    shell_id_type&       shell_id()       {return shell_id_;}
-    shell_id_type const& shell_id() const {return shell_id_;}
+    static Real calc_D_com(const Real D1, const Real D2) throw()
+    {
+        return D1 * D2 / (D1 + D2);
+    }
 
-    Real& dt()       {return dt_;}
-    Real  dt() const {return dt_;}
-    Real& begin_time()       {return begin_time_;}
-    Real  begin_time() const {return begin_time_;}
+    // TODO: calculate more efficient R_*
+    static Real calc_R_ipv(const Real r_shell,
+                           const Particle& p1, const Particle& p2) throw()
+    {
+        return r_shell / 2;
+    }
+    static Real calc_R_com(const Real r_shell,
+                           const Particle& p1, const Particle& p2) throw()
+    {
+        return r_shell / 2;
+    }
 
-    particle_array_type&       particles()       {return particles_;}
-    particle_array_type const& particles() const {return particles_;}
+  public:
 
-    std::size_t num_shells()   const {return 1;}
-    std::size_t multiplicity() const {return 2;}
+    Pair(): kind_(UNDETERMINED), dt_(0.), begin_time_(0.){}
+    ~Pair(){}
+
+    Pair(const EventKind kind, const Real dt, const Real begin_time,
+         const shell_id_type& sh, const Real shell_rad,
+         const particle_id_pair& p0, const particle_id_pair& p1)
+        : kind_(kind), dt_(dt), begin_time_(begin_time), shell_id_(sh)
+    {
+        particles_[0] = p0;
+        particles_[1] = p1;
+        this->r_ipv_ = Pair::calc_R_ipv(shell_rad, p0.second, p1.second);
+        this->r_com_ = Pair::calc_R_com(shell_rad, p0.second, p1.second);
+    }
+
+    shell_id_type&       shell_id()       throw() {return shell_id_;}
+    shell_id_type const& shell_id() const throw() {return shell_id_;}
+
+    Real& dt()               throw() {return dt_;}
+    Real  dt()         const throw() {return dt_;}
+    Real& begin_time()       throw() {return begin_time_;}
+    Real  begin_time() const throw() {return begin_time_;}
+
+    Real R_ipv() const throw() {return this->r_ipv_;}
+    Real R_com() const throw() {return this->r_com_;}
+
+    particle_id_pair&       operator[](std::size_t i)       throw()
+    {return particles_[i];}
+    particle_id_pair const& operator[](std::size_t i) const throw()
+    {return particles_[i];}
+
+    std::size_t num_shells()   const throw() {return 1;}
+    std::size_t multiplicity() const throw() {return 2;}
 
   private:
 
-    Real dt_;
-    Real begin_time_;
-    shell_id_type   shell_id_;
+    EventKind kind_;
+    Real      dt_;
+    Real      begin_time_;
+    Real      r_ipv_;
+    Real      r_com_;
+    shell_id_type       shell_id_;
     particle_array_type particles_;
 };
 
