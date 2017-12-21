d67ed6cb137c5e0c82c1ea4f12535e7ff659d243
Suguru Kato <salmon.bioinformatics@gmail.com>
2017-12-21 21:03:09 +0900

    style: Clean up sources of VoxelSpaceBase

--- ecell4/core/VoxelSpaceBase.hpp
+++ ecell4/core/VoxelSpaceBase.hpp
@@ -62,8 +62,70 @@ protected:
 
 public:
 
-    VoxelSpaceBase(const Real& voxel_radius);
-    virtual ~VoxelSpaceBase();
+    /*
+     * Constructor and Destructor
+     */
+    VoxelSpaceBase(const Real& voxel_radius) : t_(0.0), voxel_radius_(voxel_radius) {}
+    virtual ~VoxelSpaceBase() {}
+
+    /*
+     * Static Members
+     */
+
+    static inline
+    Real
+    calculate_voxel_volume(const Real r)
+    {
+        return 4.0 * sqrt(2.0) * r * r * r;
+    }
+
+    static inline
+    Real3
+    calculate_hcp_lengths(const Real voxel_radius)
+    {
+        return Real3(
+            voxel_radius / sqrt(3.0), // HCP_L
+            voxel_radius * sqrt(8.0 / 3.0), // HCP_X
+            voxel_radius * sqrt(3.0)); // HCP_Y
+    }
+
+    static inline
+    Integer3
+    calculate_shape(const Real3& edge_lengths, const Real& voxel_radius, const bool is_periodic)
+    {
+        const Real3 hcpLXY = calculate_hcp_lengths(voxel_radius);
+        const Real lengthX = edge_lengths[0];
+        const Real lengthY = edge_lengths[1];
+        const Real lengthZ = edge_lengths[2];
+
+        Integer col_size = (Integer)rint(lengthX / hcpLXY[1]) + 1;
+        Integer layer_size = (Integer)rint(lengthY / hcpLXY[2]) + 1;
+        Integer row_size = (Integer)rint((lengthZ / 2) / voxel_radius) + 1;
+
+        if (is_periodic)
+        {
+            // The number of voxels in each axis must be even for a periodic boundary.
+            col_size = (col_size % 2 == 0 ? col_size : col_size + 1);
+            layer_size = (layer_size % 2 == 0 ? layer_size : layer_size + 1);
+            row_size = (row_size % 2 == 0 ? row_size : row_size + 1);
+        }
+
+        return Integer3(col_size, row_size, layer_size);
+    }
+
+    static inline
+    Real
+    calculate_volume(const Real3& edge_lengths, const Real& voxel_radius, const bool is_periodic)
+    {
+        const Integer3 shape = calculate_shape(edge_lengths, voxel_radius, is_periodic);
+        return static_cast<Real>(shape[0] * shape[1] * shape[2])
+             * calculate_voxel_volume(voxel_radius);
+    }
+
+
+    /*
+     * Space Traits
+     */
 
     const Real t() const
     {
@@ -99,6 +161,25 @@ public:
     }
 #endif
 
+
+    /*
+     * CompartmentSpace Traits
+     */
+    std::vector<Species> list_species() const;
+
+    bool has_species(const Species& sp) const
+    {
+        return (voxel_pools_.find(sp) != voxel_pools_.end()
+                || molecule_pools_.find(sp) != molecule_pools_.end());
+    }
+
+    virtual Integer num_molecules(const Species& sp) const;
+
+    virtual Integer num_molecules_exact(const Species& sp) const
+    {
+        return num_voxels_exact(sp);
+    }
+
     Real get_value(const Species& sp) const
     {
         return static_cast<Real>(num_molecules(sp));
@@ -109,58 +190,61 @@ public:
         return static_cast<Real>(num_molecules_exact(sp));
     }
 
-    bool has_species(const Species& sp) const
-    {
-        return (voxel_pools_.find(sp) != voxel_pools_.end()
-                || molecule_pools_.find(sp) != molecule_pools_.end());
-    }
 
-
-    /**
-     * static members
+    /*
+     * ParticleSpace Traits
      */
-
-    static inline Real calculate_voxel_volume(const Real r)
+    Integer num_particles() const
     {
-        return 4.0 * sqrt(2.0) * r * r * r;
+        return num_voxels();
     }
 
-    static inline Real3 calculate_hcp_lengths(const Real voxel_radius)
+    Integer num_particles(const Species& sp) const
     {
-        return Real3(
-            voxel_radius / sqrt(3.0), // HCP_L
-            voxel_radius * sqrt(8.0 / 3.0), // HCP_X
-            voxel_radius * sqrt(3.0)); // HCP_Y
+        return num_voxels(sp);
     }
 
-    static inline Integer3 calculate_shape(const Real3& edge_lengths, const Real& voxel_radius, const bool is_periodic)
+    Integer num_particles_exact(const Species& sp) const
     {
-        const Real3 hcpLXY = calculate_hcp_lengths(voxel_radius);
-        const Real lengthX = edge_lengths[0];
-        const Real lengthY = edge_lengths[1];
-        const Real lengthZ = edge_lengths[2];
-
-        Integer col_size = (Integer)rint(lengthX / hcpLXY[1]) + 1;
-        Integer layer_size = (Integer)rint(lengthY / hcpLXY[2]) + 1;
-        Integer row_size = (Integer)rint((lengthZ / 2) / voxel_radius) + 1;
-
-        if (is_periodic)
-        {
-            // The number of voxels in each axis must be even for a periodic boundary.
-            col_size = (col_size % 2 == 0 ? col_size : col_size + 1);
-            layer_size = (layer_size % 2 == 0 ? layer_size : layer_size + 1);
-            row_size = (row_size % 2 == 0 ? row_size : row_size + 1);
-        }
-
-        return Integer3(col_size, row_size, layer_size);
+        return num_voxels_exact(sp);
     }
 
-    static inline Real calculate_volume(const Real3& edge_lengths, const Real& voxel_radius, const bool is_periodic)
+    bool has_particle(const ParticleID& pid) const
     {
-        const Integer3 shape = calculate_shape(edge_lengths, voxel_radius, is_periodic);
-        return static_cast<Real>(shape[0] * shape[1] * shape[2]) * calculate_voxel_volume(voxel_radius);
+        return has_voxel(pid);
     }
 
+    virtual
+    std::vector<std::pair<ParticleID, Particle> >
+    list_particles() const;
+
+    virtual
+    std::vector<std::pair<ParticleID, Particle> >
+    list_particles(const Species& sp) const;
+
+    virtual
+    std::vector<std::pair<ParticleID, Particle> >
+    list_particles_exact(const Species& sp) const;
+
+    virtual
+    std::pair<ParticleID, Particle>
+    get_particle(const ParticleID& pid) const
+    {
+        const Voxel v(get_voxel(pid).second);
+        return std::make_pair(pid, Particle(
+            v.species(), coordinate2position(v.coordinate()), v.radius(), v.D()));
+    }
+
+    virtual const Particle particle_at(const coordinate_type& coord) const = 0;
+
+    virtual bool remove_particle(const ParticleID& pid)
+    {
+        return remove_voxel(pid);
+    }
+
+    /*
+     * VoxelSpace Traits
+     */
     Real voxel_radius() const
     {
         return voxel_radius_;
@@ -188,194 +272,90 @@ public:
         return 2.0 * sqrt(3.0) * r * r;
     }
 
-    virtual Integer num_molecules(const Species& sp) const
-    {
-        Integer count(0);
-        SpeciesExpressionMatcher sexp(sp);
-
-        for (voxel_pool_map_type::const_iterator itr(voxel_pools_.begin());
-             itr != voxel_pools_.end(); ++itr)
-        {
-            const Integer cnt(sexp.count((*itr).first));
-            if (cnt > 0)
-            {
-                const boost::shared_ptr<VoxelPool>& vp((*itr).second);
-                count += count_voxels(vp) * cnt;
-            }
-        }
-
-        for (molecule_pool_map_type::const_iterator itr(molecule_pools_.begin());
-             itr != molecule_pools_.end(); ++itr)
-        {
-            const Integer cnt(sexp.count((*itr).first));
-            if (cnt > 0)
-            {
-                const boost::shared_ptr<MoleculePool>& vp((*itr).second);
-                count += vp->size() * cnt;
-            }
-        }
-        return count;
-    }
-
-    virtual Integer num_molecules_exact(const Species& sp) const
-    {
-        return num_voxels_exact(sp);
-    }
-
-    Integer num_particles() const
-    {
-        return num_voxels();
-    }
-
-    Integer num_particles(const Species& sp) const
-    {
-        return num_voxels(sp);
-    }
-
-    Integer num_particles_exact(const Species& sp) const
-    {
-        return num_voxels_exact(sp);
-    }
-
-    bool has_particle(const ParticleID& pid) const
-    {
-        return has_voxel(pid);
-    }
-
-    virtual bool remove_particle(const ParticleID& pid)
-    {
-        return remove_voxel(pid);
-    }
-
-    virtual std::pair<ParticleID, Particle> get_particle(const ParticleID& pid) const
-    {
-        const Voxel v(get_voxel(pid).second);
-        return std::make_pair(pid, Particle(
-            v.species(), coordinate2position(v.coordinate()), v.radius(), v.D()));
-    }
-
-    virtual std::vector<std::pair<ParticleID, Particle> > list_particles() const
-    {
-        const std::vector<std::pair<ParticleID, Voxel> > voxels(list_voxels());
-
-        std::vector<std::pair<ParticleID, Particle> > retval;
-        retval.reserve(voxels.size());
-        for (std::vector<std::pair<ParticleID, Voxel> >::const_iterator
-            i(voxels.begin()); i != voxels.end(); ++i)
-        {
-            const ParticleID& pid((*i).first);
-            const Particle p(particle_at((*i).second.coordinate()));
-            retval.push_back(std::make_pair(pid, p));
-        }
-        return retval;
-    }
-
-    virtual std::vector<std::pair<ParticleID, Particle> > list_particles(const Species& sp) const
-    {
-        const std::vector<std::pair<ParticleID, Voxel> > voxels(list_voxels(sp));
-
-        std::vector<std::pair<ParticleID, Particle> > retval;
-        retval.reserve(voxels.size());
-        for (std::vector<std::pair<ParticleID, Voxel> >::const_iterator
-            i(voxels.begin()); i != voxels.end(); ++i)
-        {
-            const ParticleID& pid((*i).first);
-            const Particle p(particle_at((*i).second.coordinate()));
-            retval.push_back(std::make_pair(pid, p));
-        }
-        return retval;
-    }
-
-    virtual std::vector<std::pair<ParticleID, Particle> > list_particles_exact(const Species& sp) const
-    {
-        const std::vector<std::pair<ParticleID, Voxel> >
-            voxels(list_voxels_exact(sp));
-
-        std::vector<std::pair<ParticleID, Particle> > retval;
-        retval.reserve(voxels.size());
-        for (std::vector<std::pair<ParticleID, Voxel> >::const_iterator
-            i(voxels.begin()); i != voxels.end(); ++i)
-        {
-            const ParticleID& pid((*i).first);
-            const Particle p(particle_at((*i).second.coordinate()));
-            retval.push_back(std::make_pair(pid, p));
-        }
-        return retval;
-    }
-
-    virtual Integer size() const = 0;
-    virtual Integer3 shape() const = 0;
-    virtual Integer inner_size() const = 0;
-
-    virtual bool on_structure(const Voxel& v) = 0;
-    virtual bool make_structure_type(const Species& sp, Shape::dimension_kind dimension, const std::string loc)
-    {
-        throw NotImplemented("make_structure_type is not implemented.");
-    }
-
-    virtual bool make_interface_type(const Species& sp, Shape::dimension_kind dimension, const std::string loc)
-    {
-        throw NotImplemented("make_interface_type is not implemented.");
-    }
-
-
-    std::vector<Species> list_species() const;
-
+    bool has_voxel(const ParticleID& pid) const;
     Integer num_voxels_exact(const Species& sp) const;
     Integer num_voxels(const Species& sp) const;
     Integer num_voxels() const;
-    bool has_voxel(const ParticleID& pid) const;
 
     virtual std::vector<std::pair<ParticleID, Voxel> > list_voxels() const;
     virtual std::vector<std::pair<ParticleID, Voxel> > list_voxels(const Species& sp) const;
     virtual std::vector<std::pair<ParticleID, Voxel> > list_voxels_exact(const Species& sp) const;
 
     virtual std::pair<ParticleID, Voxel> get_voxel(const ParticleID& pid) const;
+    virtual std::pair<ParticleID, Voxel> get_voxel_at(const coordinate_type& coord) const = 0;
+
     VoxelPool* find_voxel_pool(const Species& sp);
     const VoxelPool* find_voxel_pool(const Species& sp) const;
+
     bool has_molecule_pool(const Species& sp) const;
     MoleculePool* find_molecule_pool(const Species& sp);
     const MoleculePool* find_molecule_pool(const Species& sp) const;
 
+    virtual VoxelPool* get_voxel_pool_at(const coordinate_type& coord) const = 0;
+
+    /*
+     * Coordinate Transformation
+     */
     virtual coordinate_type inner2coordinate(const coordinate_type inner) const = 0;
 
     virtual Real3 coordinate2position(const coordinate_type& coord) const = 0;
     virtual coordinate_type position2coordinate(const Real3& pos) const = 0;
 
+    /*
+     * Neighbor
+     */
     virtual Integer num_neighbors(const coordinate_type& coord) const = 0;
-    virtual coordinate_type get_neighbor(
-        const coordinate_type& coord, const Integer& nrand) const = 0;
-    virtual coordinate_type get_neighbor_boundary(
-        const coordinate_type& coord, const Integer& nrand) const = 0;
+
+    virtual coordinate_type
+    get_neighbor(const coordinate_type& coord, const Integer& nrand) const = 0;
+
+    virtual coordinate_type
+    get_neighbor_boundary(const coordinate_type& coord, const Integer& nrand) const = 0;
 
     /*
-     * Virtual functions
+     * Voxel Manipulation
      */
-
-    virtual Real3 actual_lengths() const = 0;
-
-    virtual std::pair<ParticleID, Voxel> get_voxel_at(const coordinate_type& coord) const = 0;
-    virtual VoxelPool* get_voxel_pool_at(const coordinate_type& coord) const = 0;
-    virtual const Particle particle_at(const coordinate_type& coord) const = 0;
-
     virtual bool update_voxel(const ParticleID& pid, const Voxel& v) = 0;
     virtual bool remove_voxel(const ParticleID& pid) = 0;
     virtual bool remove_voxel(const coordinate_type& coord) = 0;
 
     virtual bool can_move(const coordinate_type& src, const coordinate_type& dest) const = 0;
-    virtual bool move(
-        const coordinate_type& src, const coordinate_type& dest,
-        const std::size_t candidate=0) = 0;
-    virtual std::pair<coordinate_type, bool> move_to_neighbor(
-        VoxelPool* const& from, VoxelPool* const& loc,
-        coordinate_id_pair_type& info, const Integer nrand) = 0;
+
+    virtual
+    bool
+    move(const coordinate_type& src, const coordinate_type& dest,
+         const std::size_t candidate=0)
+    = 0;
+
+    virtual
+    std::pair<coordinate_type, bool>
+    move_to_neighbor(VoxelPool* const& from, VoxelPool* const& loc,
+                     coordinate_id_pair_type& info, const Integer nrand)
+    = 0;
+
+    virtual Integer size() const = 0;
+    virtual Integer3 shape() const = 0;
+    virtual Integer inner_size() const = 0;
+
+    virtual bool on_structure(const Voxel& v) = 0;
+    virtual bool
+    make_structure_type(const Species& sp, Shape::dimension_kind dimension, const std::string loc)
+    {
+        throw NotImplemented("make_structure_type is not implemented.");
+    }
+
+    virtual bool
+    make_interface_type(const Species& sp, Shape::dimension_kind dimension, const std::string loc)
+    {
+        throw NotImplemented("make_interface_type is not implemented.");
+    }
 
 protected:
 
     virtual Integer count_voxels(const boost::shared_ptr<VoxelPool>& vp) const = 0;
     void push_voxels(std::vector<std::pair<ParticleID, Voxel> >& voxels,
-            const boost::shared_ptr<MoleculePool>& voxel_pool,
-            const Species& species) const;
+                     const boost::shared_ptr<MoleculePool>& voxel_pool,
+                     const Species& species) const;
 
 protected:
 
