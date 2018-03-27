a064021b1975d0e60b94c006779212a1cdb197b4
ToruNiina <niina.toru.68u@gmail.com>
2018-03-27 18:53:52 +0900

    add test cases

--- ecell4/core/tests/Halfedge_test.cpp
+++ ecell4/core/tests/Halfedge_test.cpp
@@ -228,6 +228,219 @@ const Real3 octahedron::p4 = Real3(0, 1, 1);
 const Real3 octahedron::p5 = Real3(1, 0, 1);
 const Real3 octahedron::p6 = Real3(1, 1, 0);
 
+BOOST_AUTO_TEST_CASE(Polygon_octahedron_construction_from_triangles)
+{
+    const Real pi = boost::math::constants::pi<Real>();
+    const Polygon poly = octahedron::make();
 
+    // check shape detection
+    BOOST_CHECK_EQUAL(poly.face_size(), 8);
+    BOOST_CHECK_EQUAL(poly.edge_size(), 8 * 3);
+    BOOST_CHECK_EQUAL(poly.vertex_size(), 6);
+    BOOST_CHECK_CLOSE(poly.total_area(), 8 * std::sqrt(3.0) / 2, 1e-8);
 
+    // check vertex position
+    BOOST_CHECK(static_cast<bool>(poly.find_vertex(octahedron::p1)));
+    BOOST_CHECK(static_cast<bool>(poly.find_vertex(octahedron::p2)));
+    BOOST_CHECK(static_cast<bool>(poly.find_vertex(octahedron::p3)));
+    BOOST_CHECK(static_cast<bool>(poly.find_vertex(octahedron::p4)));
+    BOOST_CHECK(static_cast<bool>(poly.find_vertex(octahedron::p5)));
+    BOOST_CHECK(static_cast<bool>(poly.find_vertex(octahedron::p6)));
 
+    typedef Polygon::vertex_id_type vertex_id_type;
+    const vertex_id_type v1 = *poly.find_vertex(octahedron::p1);
+    const vertex_id_type v2 = *poly.find_vertex(octahedron::p2);
+    const vertex_id_type v3 = *poly.find_vertex(octahedron::p3);
+    const vertex_id_type v4 = *poly.find_vertex(octahedron::p4);
+    const vertex_id_type v5 = *poly.find_vertex(octahedron::p5);
+    const vertex_id_type v6 = *poly.find_vertex(octahedron::p6);
+
+    BOOST_CHECK_SMALL(ecell4::length(
+            poly.periodic_transpose(poly.position_at(v1), octahedron::p1) -
+            octahedron::p1), 1e-8);
+    BOOST_CHECK_SMALL(ecell4::length(
+            poly.periodic_transpose(poly.position_at(v2), octahedron::p2) -
+            octahedron::p2), 1e-8);
+    BOOST_CHECK_SMALL(ecell4::length(
+            poly.periodic_transpose(poly.position_at(v3), octahedron::p3) -
+            octahedron::p3), 1e-8);
+    BOOST_CHECK_SMALL(ecell4::length(
+            poly.periodic_transpose(poly.position_at(v4), octahedron::p4) -
+            octahedron::p4), 1e-8);
+    BOOST_CHECK_SMALL(ecell4::length(
+            poly.periodic_transpose(poly.position_at(v5), octahedron::p5) -
+            octahedron::p5), 1e-8);
+    BOOST_CHECK_SMALL(ecell4::length(
+            poly.periodic_transpose(poly.position_at(v6), octahedron::p6) -
+            octahedron::p6), 1e-8);
+
+    // check apex angle
+    BOOST_CHECK_CLOSE(poly.apex_angle_at(v1), 4.0 / 3.0 * pi, 1e-8);
+    BOOST_CHECK_CLOSE(poly.apex_angle_at(v2), 4.0 / 3.0 * pi, 1e-8);
+    BOOST_CHECK_CLOSE(poly.apex_angle_at(v3), 4.0 / 3.0 * pi, 1e-8);
+    BOOST_CHECK_CLOSE(poly.apex_angle_at(v4), 4.0 / 3.0 * pi, 1e-8);
+    BOOST_CHECK_CLOSE(poly.apex_angle_at(v5), 4.0 / 3.0 * pi, 1e-8);
+    BOOST_CHECK_CLOSE(poly.apex_angle_at(v6), 4.0 / 3.0 * pi, 1e-8);
+
+    // check all the outgoing_edges are terminated at the correct vertex.
+    /* v1 */{
+        const std::vector<vertex_id_type> ans =
+            boost::assign::list_of(v2)(v3)(v4)(v5);
+
+        std::vector<vertex_id_type> result; result.reserve(4);
+        for(typename std::vector<vertex_id_type>::const_iterator
+            i(poly.outgoing_edges(v1).begin()), e(poly.outgoing_edges(v1).end());
+            i != e; ++i)
+        {
+            result.push_back(poly.target_of(*i));
+        }
+        BOOST_CHECK(ans.size() == result.size());
+        BOOST_CHECK(boost::algorithm::is_permutation(
+                    ans.begin(), ans.end(), result.begin()));
+    }
+    /* v2 */{
+        const std::vector<vertex_id_type> ans =
+            boost::assign::list_of(v1)(v3)(v5)(v6);
+
+        std::vector<vertex_id_type> result; result.reserve(4);
+        for(typename std::vector<vertex_id_type>::const_iterator
+            i(poly.outgoing_edges(v2).begin()), e(poly.outgoing_edges(v2).end());
+            i != e; ++i)
+        {
+            result.push_back(poly.target_of(*i));
+        }
+        BOOST_CHECK(ans.size() == result.size());
+        BOOST_CHECK(boost::algorithm::is_permutation(
+                    ans.begin(), ans.end(), result.begin()));
+    }
+    /* v3 */{
+        const std::vector<vertex_id_type> ans =
+            boost::assign::list_of(v1)(v2)(v4)(v6);
+
+        std::vector<vertex_id_type> result; result.reserve(4);
+        for(typename std::vector<vertex_id_type>::const_iterator
+            i(poly.outgoing_edges(v3).begin()), e(poly.outgoing_edges(v3).end());
+            i != e; ++i)
+        {
+            result.push_back(poly.target_of(*i));
+        }
+        BOOST_CHECK(ans.size() == result.size());
+        BOOST_CHECK(boost::algorithm::is_permutation(
+                    ans.begin(), ans.end(), result.begin()));
+    }
+    /* v4 */{
+        const std::vector<vertex_id_type> ans =
+            boost::assign::list_of(v1)(v3)(v5)(v6);
+
+        std::vector<vertex_id_type> result; result.reserve(4);
+        for(typename std::vector<vertex_id_type>::const_iterator
+            i(poly.outgoing_edges(v4).begin()), e(poly.outgoing_edges(v4).end());
+            i != e; ++i)
+        {
+            result.push_back(poly.target_of(*i));
+        }
+        BOOST_CHECK(ans.size() == result.size());
+        BOOST_CHECK(boost::algorithm::is_permutation(
+                    ans.begin(), ans.end(), result.begin()));
+    }
+    /* v5 */{
+        const std::vector<vertex_id_type> ans =
+            boost::assign::list_of(v1)(v2)(v4)(v6);
+
+        std::vector<vertex_id_type> result; result.reserve(4);
+        for(typename std::vector<vertex_id_type>::const_iterator
+            i(poly.outgoing_edges(v5).begin()), e(poly.outgoing_edges(v5).end());
+            i != e; ++i)
+        {
+            result.push_back(poly.target_of(*i));
+        }
+        BOOST_CHECK(ans.size() == result.size());
+        BOOST_CHECK(boost::algorithm::is_permutation(
+                    ans.begin(), ans.end(), result.begin()));
+    }
+    /* v6 */{
+        const std::vector<vertex_id_type> ans =
+            boost::assign::list_of(v2)(v3)(v4)(v5);
+
+        std::vector<vertex_id_type> result; result.reserve(4);
+        for(typename std::vector<vertex_id_type>::const_iterator
+            i(poly.outgoing_edges(v6).begin()), e(poly.outgoing_edges(v6).end());
+            i != e; ++i)
+        {
+            result.push_back(poly.target_of(*i));
+        }
+        BOOST_CHECK(ans.size() == result.size());
+        BOOST_CHECK(boost::algorithm::is_permutation(
+                    ans.begin(), ans.end(), result.begin()));
+    }
+}
+
+//! test data 3: plane
+// below, the normal vector towords the depth of your display.
+//
+// +--> x
+// |  1__2__3__4__5__6
+// |  |\ |\ |\ |\ |\ |
+// v 7|_\|_\|_\|_\|_\|12
+// y  |\ |\ |\ |\ |\ |
+//   .|_\|_\|_\|_\|_\| .
+//   .|\ |\ |\ |\ |\ | .
+//   .|_\|_\|_\|_\|_\| .
+//    |\ |\ |\ |\ |\ |
+//    |_\|_\|_\|_\|_\|
+//    |\ |\ |\ |\ |\ |
+//  31|_\|_\|_\|_\|_\|36
+struct plane
+{
+    const static Real3 edge_length;
+
+    static Polygon make()
+    {
+        std::vector<Triangle> triangles;
+        for(std::size_t j=0; j<5; ++j)
+        {
+            const Real y_up = 2.0 * (j+1);
+            const Real y_lw = 2.0 *  j;
+            for(std::size_t i=0; i<5; ++i)
+            {
+                const Real x_up = 2.0 * (i+1);
+                const Real x_lw = 2.0 *  i;
+
+                const Real3 uxuy = Real3(x_up, y_up, 5.0);
+                const Real3 uxly = Real3(x_up, y_lw, 5.0);
+                const Real3 lxuy = Real3(x_lw, y_up, 5.0);
+                const Real3 lxly = Real3(x_lw, y_lw, 5.0);
+
+                triangles.push_back(Triangle(lxly, uxly, uxuy));
+                triangles.push_back(Triangle(uxuy, lxuy, lxly));
+            }
+        }
+        return Polygon(edge_length, triangles);
+    }
+};
+const Real3 plane::edge_length = Real3(10, 10, 10);
+
+BOOST_AUTO_TEST_CASE(Polygon_plane_construction_from_triangles)
+{
+    typedef Polygon::vertex_id_type vertex_id_type;
+    const Real pi = boost::math::constants::pi<Real>();
+    const Polygon poly = plane::make();
+
+    // check shape detection
+    BOOST_CHECK_EQUAL(poly.face_size(),   50);
+    BOOST_CHECK_EQUAL(poly.edge_size(),   50 * 3);
+    BOOST_CHECK_EQUAL(poly.vertex_size(), 25);
+    BOOST_CHECK_CLOSE(poly.total_area(),  10 * 10, 1e-8);
+
+    // check apex angle
+    for(std::size_t j=0; j<5; ++j)
+    {
+        const Real y = 2.0 * j;
+        for(std::size_t i=0; i<5; ++i)
+        {
+            const Real x = 2.0 * i;
+            const vertex_id_type vid = *poly.find_vertex(Real3(x, y, 5.0));
+            BOOST_CHECK_CLOSE(poly.apex_angle_at(vid), 2.0 * pi, 1e-8);
+        }
+    }
+}
