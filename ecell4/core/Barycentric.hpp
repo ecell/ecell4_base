2046d41a549246e0cd61c26d631e6db8fb9d83e5
ToruNiina <niina.toru.68u@gmail.com>
2018-04-10 14:25:44 +0900

    nit: fix some indentation and styles

--- ecell4/core/Barycentric.hpp
+++ ecell4/core/Barycentric.hpp
@@ -15,9 +15,25 @@ struct Barycentric
     Barycentric(){}
     ~Barycentric(){}
 
-    Barycentric(const Real a, const Real b, const Real c) throw();
-    Barycentric(const Barycentric& b) throw();
-    Barycentric& operator=(const Barycentric& b) throw();
+    Barycentric(const Real a, const Real b, const Real c) throw()
+    {
+        val[0] = a;
+        val[1] = b;
+        val[2] = c;
+    }
+    Barycentric(const Barycentric& rhs) throw()
+    {
+        val[0] = rhs.val[0];
+        val[1] = rhs.val[1];
+        val[2] = rhs.val[2];
+    }
+    Barycentric& operator=(const Barycentric& b) throw()
+    {
+        val[0] = b.val[0];
+        val[1] = b.val[1];
+        val[2] = b.val[2];
+        return *this;
+    }
 
     Real  operator[](const index_type i) const throw() {return val[i];}
     Real& operator[](const index_type i)       throw() {return val[i];}
@@ -28,26 +44,12 @@ struct Barycentric
     boost::array<Real, 3> val;
 };
 
-inline Barycentric::Barycentric(const Real a, const Real b, const Real c) throw()
+template<typename charT, typename traitsT>
+inline std::basic_ostream<charT, traitsT>&
+operator<<(std::basic_ostream<charT, traitsT>& os, const Barycentric& b)
 {
-    val[0] = a;
-    val[1] = b;
-    val[2] = c;
-}
-
-inline Barycentric::Barycentric(const Barycentric& b) throw()
-{
-    val[0] = b.val[0];
-    val[1] = b.val[1];
-    val[2] = b.val[2];
-}
-
-inline Barycentric& Barycentric::operator=(const Barycentric& b) throw()
-{
-    val[0] = b.val[0];
-    val[1] = b.val[1];
-    val[2] = b.val[2];
-    return *this;
+    os << b[0] << ", " << b[1] << ", " << b[2];
+    return os;
 }
 
 inline Barycentric
@@ -62,6 +64,8 @@ operator-(const Barycentric& lhs, const Barycentric& rhs) throw()
     return Barycentric(lhs[0] - rhs[0], lhs[1] - rhs[1], lhs[2] - rhs[2]);
 }
 
+// utility functions...
+
 inline bool on_plane(const Barycentric& bary, const Real tol = 1e-10) throw()
 {
     return std::abs(bary[0] + bary[1] + bary[2] - 1.0) < tol;
@@ -69,10 +73,9 @@ inline bool on_plane(const Barycentric& bary, const Real tol = 1e-10) throw()
 
 inline bool is_inside(const Barycentric& bary) throw()
 {
-    return on_plane(bary) &&
-           (0. <= bary[0] && bary[0] <= 1.0) &&
-           (0. <= bary[1] && bary[1] <= 1.0) &&
-           (0. <= bary[2] && bary[2] <= 1.0);
+    return on_plane(bary) && (0. <= bary[0] && bary[0] <= 1.0) &&
+                             (0. <= bary[1] && bary[1] <= 1.0) &&
+                             (0. <= bary[2] && bary[2] <= 1.0);
 }
 
 inline bool is_inside(const Barycentric& bary, const Real tolerance) throw()
@@ -92,17 +95,8 @@ Barycentric to_barycentric(const Real3& pos, const Triangle& face);
 
 inline Real3 to_absolute(const Barycentric& bary, const Triangle& tri) throw()
 {
-    return tri.vertex_at(0) * bary[0] +
-           tri.vertex_at(1) * bary[1] +
+    return tri.vertex_at(0) * bary[0] + tri.vertex_at(1) * bary[1] +
            tri.vertex_at(2) * bary[2];
-}
-
-template<typename charT, typename traitsT>
-inline std::basic_ostream<charT, traitsT>&
-operator<<(std::basic_ostream<charT, traitsT>& os, const Barycentric& b)
-{
-    os << b[0] << ", " << b[1] << ", " << b[2];
-    return os;
 }
 
 }// ecell4
