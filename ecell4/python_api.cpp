9304daed6dd1b196958aeabaad026d9f816e5e73
Suguru Kato <salmon.bioinformatics@gmail.com>
2019-01-10 15:36:39 +0900

    feat: add a tiny pybind11 source

---
+++ ecell4/python_api.cpp
@@ -0,0 +1,33 @@
+#include <pybind11/pybind11.h>
+#include <ecell4/spatiocyte/SpatiocyteWorld.hpp>
+
+namespace py = pybind11;
+
+namespace spatiocyte
+{
+    using namespace ecell4::spatiocyte;
+
+    void setup_module(py::module& m)
+    {
+        py::class_<SpatiocyteWorld>(m, "SpatiocyteWorld")
+            .def(py::init<>())
+            .def("t", &SpatiocyteWorld::t)
+            .def("set_t", &SpatiocyteWorld::set_t)
+            .def("save", &SpatiocyteWorld::save)
+            .def("load", &SpatiocyteWorld::load)
+            .def("volume", &SpatiocyteWorld::volume)
+            .def("has_species", &SpatiocyteWorld::has_species);
+    }
+
+}
+
+PYBIND11_MODULE(ecell4, m) {
+    py::module m_bd         = m.def_submodule("bd",         "A submodule of ecell4");
+    py::module m_egfrd      = m.def_submodule("egfrd",      "A submodule of ecell4");
+    py::module m_gillespie  = m.def_submodule("gillespie",  "A submodule of ecell4");
+    py::module m_meso       = m.def_submodule("meso",       "A submodule of ecell4");
+    py::module m_ode        = m.def_submodule("ode",        "A submodule of ecell4");
+    py::module m_spatiocyte = m.def_submodule("spatiocyte", "A submodule of ecell4");
+
+    spatiocyte::setup_module(m_spatiocyte);
+}
