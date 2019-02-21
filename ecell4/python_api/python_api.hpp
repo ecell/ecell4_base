2be4a4fa2c645630818dac55505a21118fcafec7
Suguru Kato <salmon.bioinformatics@gmail.com>
2019-02-22 01:06:26 +0900

    :sparkles: Migrate the egfrd submodule to pybind11
    Migrate it from Cython to pybind11 #321

--- ecell4/python_api/python_api.hpp
+++ ecell4/python_api/python_api.hpp
@@ -13,6 +13,7 @@ namespace python_api
 
 void setup_module(pybind11::module& m);
 void setup_bd_module(pybind11::module& m);
+void setup_egfrd_module(pybind11::module& m);
 void setup_spatiocyte_module(pybind11::module& m);
 void setup_gillespie_module(pybind11::module& m);
 
