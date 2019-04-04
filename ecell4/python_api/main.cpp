474f2695d3c6ed4723528fbf06c60941a58da7c1
Suguru Kato <salmon.bioinformatics@gmail.com>
2019-04-04 15:07:45 +0900

    :building_construction: Add core submodule
     #321

--- ecell4/python_api/main.cpp
+++ ecell4/python_api/main.cpp
@@ -7,6 +7,7 @@ namespace py = pybind11;
 using namespace ecell4::python_api;
 
 PYBIND11_MODULE(ecell4_base, m) {
+    py::module m_core       = m.def_submodule("core",       "A submodule of ecell4_base");
     py::module m_bd         = m.def_submodule("bd",         "A submodule of ecell4_base");
     py::module m_egfrd      = m.def_submodule("egfrd",      "A submodule of ecell4_base");
     py::module m_gillespie  = m.def_submodule("gillespie",  "A submodule of ecell4_base");
@@ -14,7 +15,7 @@ PYBIND11_MODULE(ecell4_base, m) {
     py::module m_ode        = m.def_submodule("ode",        "A submodule of ecell4_base");
     py::module m_spatiocyte = m.def_submodule("spatiocyte", "A submodule of ecell4_base");
 
-    setup_module(m);
+    setup_module(m_core);
     setup_bd_module(m_bd);
     setup_egfrd_module(m_egfrd);
     setup_gillespie_module(m_gillespie);
