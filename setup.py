7764114babcec775aba2a43d7b13016aa05225ac
Kozo Nishida <kozo.nishida@gmail.com>
2019-05-21 20:13:23 +0900

    :bookmark: Update the version to 2.0.0b1

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ LONG_DESCRIPTION = open("README.md").read()
 
 setup(
     name='ecell4_base',
-    version = '1.0.0',
+    version = '2.0.0b1',
     license = "the GNU General Public License v2",
     author = "Kazunari Kaizu",
     author_email = "kaizu@riken.jp",
