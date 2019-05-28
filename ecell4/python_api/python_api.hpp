db3030642370b3ae66dd597b5a35fb7de10607fe
ToruNiina <niina.toru.68u@gmail.com>
2019-05-28 15:30:25 +0900

    :sparkles: add sgfrd module to python api

--- ecell4/python_api/python_api.hpp
+++ ecell4/python_api/python_api.hpp
@@ -17,6 +17,7 @@ void setup_egfrd_module(pybind11::module& m);
 void setup_gillespie_module(pybind11::module& m);
 void setup_meso_module(pybind11::module& m);
 void setup_ode_module(pybind11::module& m);
+void setup_sgfrd_module(pybind11::module& m);
 void setup_spatiocyte_module(pybind11::module& m);
 
 }
