a8aec397f47f5949dc379a8175e6436e8c651736
Kazunari Kaizu <kwaizu@gmail.com>
2019-06-27 12:28:29 +0900

    #384 Just use c++11 regex (#385)

--- ecell4/core/UnitSpecies.cpp
+++ ecell4/core/UnitSpecies.cpp
@@ -1,15 +1,8 @@
 #include <stdexcept>
+#include <regex>
 #include <boost/algorithm/string.hpp>
 
 #include "UnitSpecies.hpp"
-
-#if defined(HAVE_BOOST_REGEX)
-#include <boost/regex.hpp>
-#elif defined(WIN32_MSC)
-#include <regex>
-#else
-#include <regex.h>
-#endif /* HAVE_BOOST_REGEX */
 
 
 namespace ecell4
@@ -29,12 +22,8 @@ void UnitSpecies::deserialize(const UnitSpecies::serial_type& serial)
         return;
     }
 
-#if defined(HAVE_BOOST_REGEX) || defined(WIN32_MSC)
-#if defined(HAVE_BOOST_REGEX)
-    using namespace boost;
-#else /* WIN32_MSC */
-    using namespace std::tr1;
-#endif /* HAVE_BOOST_REGEX */
+    using namespace std;
+
     regex r1(
         "^\\s*(\\w+)\\s*(\\(\\s*([\\w\\s\\^=,]*)\\))?\\s*$");
     smatch results1;
@@ -85,102 +74,6 @@ void UnitSpecies::deserialize(const UnitSpecies::serial_type& serial)
         throw std::invalid_argument(
             "a wrong serial was given to UnitSpecies [" + serial + "]"); //XXX:
     }
-#else /* regex.h */
-    regex_t reg1;
-    int errcode = regcomp(&reg1,
-        "^[[:blank:]]*([[:alnum:]_]+)[[:blank:]]*"
-        "(\\([[:blank:]]*([^\\(\\)[:blank:]][^\\(\\)]*)?\\))?[[:blank:]]*$",
-        REG_EXTENDED);
-    if (errcode != 0)
-    {
-        char errbuf[100];
-        regerror(errcode, &reg1, errbuf, sizeof(errbuf));
-        std::cout << errbuf << std::endl; //XXX: never get here
-    }
-
-    regmatch_t match1[4];
-    errcode = regexec(&reg1, serial.c_str(), 4, match1, 0);
-    if (errcode != 0)
-    {
-        char errbuf[100];
-        regerror(errcode, &reg1, errbuf, sizeof(errbuf));
-        throw std::invalid_argument(
-            "a wrong serial was given to UnitSpecies [" + serial + "]: "
-            + std::string(errbuf)); //XXX:
-    }
-
-    name_ = serial.substr(match1[1].rm_so, match1[1].rm_eo - match1[1].rm_so);
-
-    if (match1[3].rm_eo - match1[3].rm_so > 0)
-    {
-        std::string tmp(
-            serial.substr(match1[3].rm_so, match1[3].rm_eo - match1[3].rm_so));
-
-        regex_t reg2;
-        errcode = regcomp(&reg2,
-            "[[:blank:]]*([[:alnum:]_]+)[[:blank:]]*"
-            "(=[[:blank:]]*([[:alnum:]_]+))?[[:blank:]]*"
-            "(\\^[[:blank:]]*([[:alnum:]_]+))?[[:blank:]]*(,|$)",
-            REG_EXTENDED);
-        if (errcode != 0)
-        {
-            char errbuf[100];
-            regerror(errcode, &reg2, errbuf, sizeof(errbuf));
-            std::cout << errbuf << std::endl; //XXX: never get here
-        }
-
-        regmatch_t match2[7];
-        bool order(false);
-
-        while (true)
-        {
-            errcode = regexec(&reg2, tmp.c_str(), 7, match2, 0);
-            if (errcode != 0)
-            {
-                char errbuf[100];
-                regerror(errcode, &reg2, errbuf, sizeof(errbuf));
-                throw std::invalid_argument(
-                    "wrong site specifiers are given to UnitSpecies ["
-                    + serial + "]: " + std::string(errbuf)); //XXX:
-            }
-
-            if (match2[3].rm_so != -1)
-            {
-                order = true;
-            }
-            else if (order)
-            {
-                throw std::invalid_argument(
-                    "non-keyword arg after keyword arg [" +
-                    serial + "]"); //XXX:
-            }
-
-            const std::string site_name(
-                tmp.substr(match2[1].rm_so, match2[1].rm_eo - match2[1].rm_so));
-            const std::string state((match2[3].rm_so != -1)?
-                tmp.substr(match2[3].rm_so, match2[3].rm_eo - match2[3].rm_so)
-                : "");
-            const std::string bond((match2[5].rm_so != -1)?
-                tmp.substr(match2[5].rm_so, match2[5].rm_eo - match2[5].rm_so)
-                : "");
-
-            add_site(site_name, state, bond);
-
-            if (static_cast<size_t>(match2[0].rm_eo) == tmp.length())
-            {
-                break;
-            }
-            else
-            {
-                tmp = tmp.substr(match2[0].rm_eo);
-            }
-        }
-
-        regfree(&reg2);
-    }
-
-    regfree(&reg1);
-#endif /* HAVE_BOOST_REGEX */
 }
 
 UnitSpecies::serial_type UnitSpecies::serial() const
