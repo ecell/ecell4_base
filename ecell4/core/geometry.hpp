b3b0b6b81768a4e9fb003235323cbe43908c614e
ToruNiina <niina.toru.68u@gmail.com>
2019-10-01 17:08:15 +0900

    :recycle: replace branches by boost:algo::clamp

--- ecell4/core/geometry.hpp
+++ ecell4/core/geometry.hpp
@@ -2,6 +2,7 @@
 #define ECELL4_GEOMETRY
 #include <ecell4/core/Real3.hpp>
 #include <boost/math/constants/constants.hpp>
+#include <boost/algorithm/clamp.hpp>
 #include <cmath>
 
 namespace ecell4
@@ -9,11 +10,9 @@ namespace ecell4
 
 inline Real calc_angle(const Real3& lhs, const Real3& rhs)
 {
-    const Real cosine  = dot_product(lhs, rhs) /
-                         std::sqrt(length_sq(lhs) * length_sq(rhs));
-         if(cosine < -1.){return boost::math::constants::pi<Real>();}
-    else if(cosine >  1.){return 0;}
-    else                 {return std::acos(cosine);}
+    const Real cosine = dot_product(lhs, rhs) /
+                        std::sqrt(length_sq(lhs) * length_sq(rhs));
+    return std::acos(boost::algorithm::clamp(cosine, -1.0, 1.0));
 }
 
 Real3 rotate(const Real angle, const Real3& axis, const Real3& target);
