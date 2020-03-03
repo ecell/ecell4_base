c2dc37b43e0e1c8493047641a40dad76b38e8466
ToruNiina <niina.toru.68u@gmail.com>
2020-03-03 12:54:49 +0900

    :fire: remove comments

--- ecell4/core/types.hpp
+++ ecell4/core/types.hpp
@@ -13,10 +13,5 @@ namespace ecell4
 typedef int64_t Integer;
 typedef double Real;
 
-// const double inf = HUGE_VAL; // infinity (double)
-// const Real N_A = 6.022140857e+23;
-// const double epsilon = DBL_EPSILON; // std::numeric_limits<Real>::epsilon();
-
 } // ecell4
-
 #endif /* ECELL4_TYPES_HPP */
