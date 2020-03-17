08c9f0e1c16c565210a6fe4d872253e13eca9f1c
ToruNiina <niina.toru.68u@gmail.com>
2020-03-17 13:28:52 +0900

    :recycle: manually expand BOOST_PP_ENUM to reduce code

--- ecell4/egfrd/utils/array_helper.hpp
+++ ecell4/egfrd/utils/array_helper.hpp
@@ -1,41 +1,33 @@
-#ifndef ARRAY_HELPER_HPP
-#define ARRAY_HELPER_HPP
-
+#ifndef ECELL4_UTIL_ARRAY_HELPER_HPP
+#define ECELL4_UTIL_ARRAY_HELPER_HPP
 #include <boost/array.hpp>
-#include <boost/preprocessor/config/limits.hpp>
-#include <boost/preprocessor/repetition/repeat.hpp>
-#include <boost/preprocessor/repetition/enum_params.hpp>
-#include <boost/preprocessor/cat.hpp>
-#include <boost/call_traits.hpp>
 
 namespace ecell4
 {
 namespace egfrd
 {
 
-#define ARRAY_HELPER_INNER_TPL(__z__, __n__, __d__) \
-    __d__[__n__] = BOOST_PP_CAT(p, __n__);
-
-#define ARRAY_HELPER_TPL(__z__, __n__, __d__) \
-template<typename T_> \
-inline ::boost::array<T_, __n__> array_gen(\
-        BOOST_PP_ENUM_PARAMS(__n__, T_ const& p)) \
-{ \
-    ::boost::array<T_, __n__> retval; \
-    BOOST_PP_REPEAT_ ## __z__(__n__, ARRAY_HELPER_INNER_TPL, retval) \
-    return retval; \
+template<typename T>
+inline boost::array<T, 0> array_gen()
+{
+    return boost::array<T, 0>{};
 }
-
-#ifndef WIN32_MSC
-BOOST_PP_REPEAT_FROM_TO(0, BOOST_PP_LIMIT_REPEAT, ARRAY_HELPER_TPL, )
-#else
-BOOST_PP_REPEAT_FROM_TO(0, 128, ARRAY_HELPER_TPL, )
-#endif
-
-#undef ARRAY_HELPER_TPL
-#undef ARRAY_HELPER_INNER_TPL
+template<typename T>
+inline boost::array<T, 1> array_gen(const T& v0)
+{
+    return boost::array<T, 1>{{v0}};
+}
+template<typename T>
+inline boost::array<T, 2> array_gen(const T& v0, const T& v1)
+{
+    return boost::array<T, 2>{{v0, v1}};
+}
+template<typename T>
+inline boost::array<T, 3> array_gen(const T& v0, const T& v1, const T& v2)
+{
+    return boost::array<T, 3>{{v0, v1, v2}};
+}
 
 } // egfrd
 } // ecell4
-
 #endif /* ARRAY_HELPER_HPP */
