6269c3657e297cadb040ba8d0dc2bda999f44570
ToruNiina <niina.toru.68u@gmail.com>
2020-04-21 15:27:29 +0900

    :fire: remove consoleappender.cpp

--- ecell4/egfrd/ConsoleAppender.hpp
+++ ecell4/egfrd/ConsoleAppender.hpp
@@ -1,9 +1,8 @@
-#ifndef CONSOLE_LOGGER_HPP
-#define CONSOLE_LOGGER_HPP
-
-#include <string>
-
+#ifndef ECELL4_EGFRD_CONSOLE_LOGGER_HPP
+#define ECELL4_EGFRD_CONSOLE_LOGGER_HPP
 #include "Logger.hpp"
+#include <cstdio>
+#include <cstring>
 
 namespace ecell4
 {
@@ -16,11 +15,22 @@ public:
     typedef LogAppender base_type;
 
 public:
-    virtual ~ConsoleAppender();
+    ~ConsoleAppender() override = default;
 
-    virtual void flush();
+    void flush() override
+    {
+        std::fflush(stderr);
+    }
 
-    virtual void operator()(enum Logger::level lv, char const* name, char const** chunks);
+    void operator()(enum Logger::level lv, char const* name, char const** chunks)
+    {
+        std::fprintf(stderr, "%s: %-8s ", name, Logger::stringize_error_level(lv));
+        for (char const** p = chunks; *p; ++p)
+        {
+            std::fwrite(*p, sizeof(char), std::strlen(*p), stderr);
+        }
+        std::fputc('\n', stderr);
+    }
 };
 
 } // egfrd
