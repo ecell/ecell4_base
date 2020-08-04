bae5063c010ca14c7f08518d82384110826853e5
ToruNiina <niina.toru.68u@gmail.com>
2020-08-04 17:32:24 +0900

    :sparkles: add list_particles_within_radius_XD

--- ecell4/ngfrd/NGFRDWorld.hpp
+++ ecell4/ngfrd/NGFRDWorld.hpp
@@ -23,6 +23,14 @@ struct MoleculeInfo
     const Real D;
 };
 
+//
+// Since most of the member methods are redirected to ParticleSpace, we store
+// both 3D and 2D particles to the same ParticleSpace, as spherical particles.
+// It causes redundant checking in some cases because 2D particles which have
+// circular shape are considered sphere in ParticleSpace class. We need to check
+// if the particle is on a face and the target collides with the face before
+// concluding particles collide with each other.
+//
 class NGFRDWorld final
     : public WorldInterface
 {
@@ -111,56 +119,80 @@ public:
 
     // ------------------------------------------------------------------------
     // list_particles_within_radius
+    //
+    // for compatibility. Currently, it overestimates the number of 2D particles
 
     std::vector<std::pair<std::pair<ParticleID, Particle>, Real>>
     list_particles_within_radius(const Real3& center, const Real radius) const
     {
-        return list_particles_within_radius_impl(center, radius,
-                [](const ParticleID&) noexcept -> bool {return false;});
+        return this->list_particles_within_radius_impl(center, radius);
     }
     std::vector<std::pair<std::pair<ParticleID, Particle>, Real>>
     list_particles_within_radius(const Real3& center, const Real radius,
             const ParticleID& ignore) const
     {
-        return list_particles_within_radius_impl(center, radius,
-                [&](const ParticleID& pid) noexcept -> bool {
-                    return pid == ignore;
-                });
+        return this->list_particles_within_radius_impl(center, radius, ignore);
     }
     std::vector<std::pair<std::pair<ParticleID, Particle>, Real>>
     list_particles_within_radius(const Real3& center, const Real radius,
             const ParticleID& ignore1, const ParticleID& ignore2) const
     {
-        return list_particles_within_radius_impl(center, radius,
-                [&](const ParticleID& pid) noexcept -> bool {
-                    return pid == ignore1 || pid == ignore2;
-                });
+        return this->list_particles_within_radius_impl(
+                center, radius, ignore1, ignore2);
     }
 
+    // ------------------------------------------------------------------------
+    // list_particles_within_radius_3D
+    // Note that it returns only 3D particles. No 2D-3D checking.
+
     std::vector<std::pair<std::pair<ParticleID, Particle>, Real>>
-    list_particles_within_radius(
-            const std::pair<Real3, FaceID>& center, const Real radius) const
+    list_particles_within_radius_3D(const Real3& center, const Real radius) const
     {
-        return list_particles_within_radius_impl(center, radius,
-                [](const ParticleID&) noexcept -> bool {return false;});
+        return this->list_particles_within_radius_3D_impl(center, radius);
     }
     std::vector<std::pair<std::pair<ParticleID, Particle>, Real>>
-    list_particles_within_radius(
+    list_particles_within_radius_3D(const Real3& center, const Real radius,
+            const ParticleID& ignore) const
+    {
+        return this->list_particles_within_radius_3D_impl(
+                center, radius, ignore);
+    }
+    std::vector<std::pair<std::pair<ParticleID, Particle>, Real>>
+    list_particles_within_radius_3D(const Real3& center, const Real radius,
+            const ParticleID& ignore1, const ParticleID& ignore2) const
+    {
+        return this->list_particles_within_radius_3D_impl(
+                center, radius, ignore1, ignore2);
+    }
+
+    // ------------------------------------------------------------------------
+    // list_particles_within_radius_3D
+    // Note that it returns only 3D particles. No 2D-3D checking.
+
+    std::vector<std::pair<std::pair<ParticleID, Particle>, Real>>
+    list_particles_within_radius_2D(
+            const std::pair<Real3, FaceID>& center, const Real radius) const
+    {
+        return this->list_particles_within_radius_2D_impl(center, radius,
+                [](const ParticleID& pid) noexcept -> bool {return false;});
+    }
+    std::vector<std::pair<std::pair<ParticleID, Particle>, Real>>
+    list_particles_within_radius_2D(
             const std::pair<Real3, FaceID>& center, const Real radius,
             const ParticleID& ignore) const
     {
-        return list_particles_within_radius_impl(center, radius,
-                [&](const ParticleID& pid) noexcept -> bool {
+        return this->list_particles_within_radius_2D_impl(center, radius,
+                [](const ParticleID& pid) noexcept -> bool {
                     return pid == ignore;
                 });
     }
     std::vector<std::pair<std::pair<ParticleID, Particle>, Real>>
-    list_particles_within_radius(
+    list_particles_within_radius_2D(
             const std::pair<Real3, FaceID>& center, const Real radius,
             const ParticleID& ignore1, const ParticleID& ignore2) const
     {
-        return list_particles_within_radius_impl(center, radius,
-                [&](const ParticleID& pid) noexcept -> bool {
+        return this->list_particles_within_radius_2D_impl(center, radius,
+                [](const ParticleID& pid) noexcept -> bool {
                     return pid == ignore1 || pid == ignore2;
                 });
     }
@@ -317,21 +349,123 @@ public:
 
 private:
 
-    template<typename Filter>
+    template<typename ... Ts>
     std::vector<std::pair<std::pair<ParticleID, Particle>, Real>>
-    list_particles_within_radius_impl(const Real3& center, const Real radius) const
+    list_particles_within_radius_impl(const Real3& center, const Real radius,
+                                      Ts&& ... ignores) const
     {
-        std::vector<std::pair<std::pair<ParticleID, Particle>, Real>> list;
-        // TODO
+        const auto faces = polygon_->list_faces_within_radius(center, radius);
+        auto list = this->ps_->list_particles_within_radius(
+                center, radius, std::forward<Ts>(ignores)...);
+
+        // FIXME
+        // In case of a particle straddles faces, the calculation becomes
+        // incorrect (overestimate the number of particles). We need to remove
+        // them to make it perfect.
+        //
+        // To correctly find the overlap, we first need to check if a particle
+        // overlaps with a face. If
+        // - the range overlaps with a face and
+        // - a particle overlaps with the face and
+        // - the distance between the center of the particle and the center of
+        //   the region is less than the radius and the range radius on the face
+        // then they overlaps each other.
+        //
+        // If particle does not straddle faces, it may not overlap with the
+        // region even if the distance between region and the particle is less
+        // than the radius.
+
+        // 3D particle collides with 2D particle only if it is within radius
+        // AND 3D particle collides with the face on which the 2D particle is.
+
+        const auto removed = std::remove_if(list.begin(), list.end(), [this, &faces]
+            (const std::pair<std::pair<ParticleID, Particle>, Real>& elem) -> bool
+            {
+                const auto pid = elem.first.first;
+                if(const auto fidopt = poly_con_.on_which_face(elem.first.first))
+                {
+                    // 2D particle! If the face is not found in the overlapping
+                    // face, remove it.
+                    const auto fid = *fidopt;
+
+                    return std::find_if(faces.begin(), faces.end(), [&fid]
+                        (const std::pair<std::pair<FaceID, Triangle>, Real>& f) {
+                            return fid == f.first.first;
+                        }) == faces.end();
+                }
+                // it is not on a face. it is 3D. do not remove this.
+                return false;
+            });
+
+        // erase all the 2D particles that do not collide actually.
+        list.erase(removed, list.end());
         return list;
     }
+
+
+    template<typename ... Ts>
+    std::vector<std::pair<std::pair<ParticleID, Particle>, Real>>
+    list_particles_within_radius_3D_impl(const Real3& center, const Real radius,
+                                         Ts&& ... ignores) const
+    {
+        auto list = this->ps_->list_particles_within_radius(
+                center, radius, std::forward<Ts>(ignores)...);
+
+        // It does not check collision with 2D particles.
+        // If the list contains 2D particle, remove it from the result.
+        const auto removed = std::remove_if(list.begin(), list.end(),
+            [this](const std::pair<std::pair<ParticleID, Particle>, Real>& elem)
+                noexcept -> bool {
+                return poly_con_.on_face(elem.first.first);
+            });
+
+        // erase all the 2D particles that do not collide actually.
+        list.erase(removed, list.end());
+        return list;
+    }
+
     template<typename Filter>
     std::vector<std::pair<std::pair<ParticleID, Particle>, Real>>
-    list_particles_within_radius_impl(const std::pair<Real3, FaceID>& center,
-            const Real radius) const
+    list_particles_within_radius_2D_impl(
+            const std::pair<Real3, FaceID>& center, const Real radius,
+            Filter filter) const
     {
         std::vector<std::pair<std::pair<ParticleID, Particle>, Real>> list;
-        // TODO
+
+        const auto& pos = center.first;
+        const auto& fid = center.second;
+
+        // check particles on the same face
+        for(const auto& pid : this->poly_con_.objects_on(fid))
+        {
+            auto pp = ps_->get_particle(pid);
+            // it is okay to use 3D distance because both are on the same face
+            const Real dist = length(pos - pp.second.position()) -
+                              pp.second.radius();
+            if(dist < radius)
+            {
+                retval.push_back(std::make_pair(std::move(pp), dist));
+            }
+        }
+
+        // check particles on the neighborling faces
+        for(const auto& fid : polygon_->neighbor_faces_of(fid))
+        {
+            for(const auto& pid : this->poly_con_.objects_on(fid))
+            {
+                auto pp = ps_->get_particle(pid);
+                const Real dist = ecell4::polygon::distance(*polygon_,
+                    pos, std::make_pair(pp.second.position(), fid)
+                    ) - pp.second.radius();
+
+                if(dist < radius)
+                {
+                    retval.push_back(std::make_pair(std::move(pp), dist));
+                }
+            }
+        }
+
+        // no 3D check (Since particle would never overlaps with any face).
         return list;
     }
 
