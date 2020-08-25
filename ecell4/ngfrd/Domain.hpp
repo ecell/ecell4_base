1bfeb0fdc7ab16c231076cff03cc1ae4e9cf3c1c
ToruNiina <niina.toru.68u@gmail.com>
2020-08-25 15:10:12 +0900

    :sparkles: add multiplicity to Domain

--- ecell4/ngfrd/Domain.hpp
+++ ecell4/ngfrd/Domain.hpp
@@ -17,6 +17,17 @@ public:
 
     using storage_type = boost::variant<MultiDomain>;
 
+private:
+
+    struct multiplicity_visitor : boost::static_visitor<std::size_t>
+    {
+        template<typename D>
+        std::size_t operator()(const D& dom) const noexcept
+        {
+            return dom.multiplicity();
+        }
+    }
+
 public:
     template<typename D>
     explicit Domain(D&& d): storage_(std::forward<D>(d)) {}
@@ -30,6 +41,11 @@ public:
 
     storage_type const& as_variant() const noexcept {return storage_;}
     storage_type&       as_variant()       noexcept {return storage_;}
+
+    std::size_t multiplicity() const noexcept
+    {
+        return boost::apply_visitor(multiplicity_visitor(), storage_);
+    }
 
 private:
 
