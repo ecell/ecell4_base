eb7005071351b92ccb329dbf991df1b5b7fa1ace
ToruNiina <niina.toru.68u@gmail.com>
2020-09-01 14:59:09 +0900

    :sparkles: use move passing in fire_multi

--- ecell4/ngfrd/NGFRDSimulator.cpp
+++ ecell4/ngfrd/NGFRDSimulator.cpp
@@ -20,7 +20,7 @@ void NGFRDSimulator::form_domain_3D(const ParticleID& pid, const Particle& p)
 }
 
 boost::container::small_vector<std::pair<ParticleID, Particle>, 4>
-NGFRDSimulator::fire_multi(const DomainID& did, const MultiDomain& dom)
+NGFRDSimulator::fire_multi(const DomainID& did, MultiDomain dom)
 {
     dom.step(*(this->model_), *this, *(this->world_));
 
@@ -31,7 +31,14 @@ NGFRDSimulator::fire_multi(const DomainID& did, const MultiDomain& dom)
     //      of this step.
     if(dom.eventkind() == MultiDomain::EventKind::None)
     {
-        this->add_event(did, dom);
+        // add event with the same domain ID
+        const auto evid = scheduler_.add(
+                std::make_shared<event_type>(this->t(), did));
+
+        // update begin_time and re-insert domain into domains_ container
+        dom.begin_time() = this->t();
+        domains_[did] = std::make_pair(evid, Domain(std::move(dom));
+
         return {/* All particles are re-inserted as Multi! */};
     }
     // something happens. remove multi and assign domains for each particles.
